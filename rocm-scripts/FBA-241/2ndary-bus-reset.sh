e9d3f82ed1476b8ee184de8347a9aa337a0ffc95
Your Name <you@example.com>
2018-06-22 07:33:28 -0400

    example.cfg: added

--- rocm-scripts/FBA-241/2ndary-bus-reset.sh
+++ rocm-scripts/FBA-241/2ndary-bus-reset.sh
@@ -6,9 +6,12 @@ mkdir -p $LOG_FOLDER
 IP_BMC=10.96.30.225
 TEST_NAME="LINK down"
 
-#pci_up_port=0000:2f:00.0
-#pci_up_port=0000:2e:10.0
-pci_up_port=0000:20:03.1
+#pci_up_port=0000:20:03.1
+pci_up_port=0000:a0:03.1
+
+#a0:03.1->ae:00.0
+#20:03.1->31:00.0
+#00:01.0->11:00.0
 #echo "Capturing rocm_techsupport before " $TEST_NAME
 #../rocm_techsupport.sh > $LOG_FOLDER/before_rocm_techsupport_$pci_bus_number.log
 
@@ -16,7 +19,7 @@ pci_up_port=0000:20:03.1
 #ipmitool -H $IP_BMC -U ADMIN -I lanplus -P ADMIN sel elist > after_SEL_logs_$pci_bus_number.log
 
 echo "Setting link down in pcie cap + link control Rx..."
-setpci -s $pci_up_port 3e.L=00000040
+setpci -s $pci_up_port 3e.W=5200
 lspci -s $pci_up_port  -vvvxxx
 echo ------------------------
 lspci -s $pci_up_port  -vvvxxx | grep 30:
@@ -29,4 +32,4 @@ lspci -s $pci_up_port  -vvvxxx | grep BridgeCtl
 #ipmitool -H $IP_BMC -U ADMIN -I lanplus -P ADMIN sel elist > after_SEL_logs_$pci_bus_number.log
 
 #lspci | grep Disp
-#USE_HIP_CALL=1 /root/rccl/tools/TransferBench/TransferBench example.cfg
+/root/rccl/tools/TransferBench/TransferBench example.cfg
