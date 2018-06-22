3dc1a7b0f91bb59be1e5f72a18976b3e3d04271d
Your Name <you@example.com>
2018-06-22 08:11:17 -0400

    example-bidir.cfg: added

--- rocm-scripts/FBA-241/jtest.sh
+++ rocm-scripts/FBA-241/jtest.sh
@@ -2,7 +2,10 @@
 
 modprobe amdgpu
 
-pci_up_port=0000:2f:00.0
+PCIE_DEVICE=0000:2f:00.0
+lspci -t $PCIE_DEVICE
+echo ------
+lspci -t $PCIE_DEVICE
 echo "Capturing rocm_techsupport before SERR injection"
 ../rocm_techsupport.sh > before_rocm_techsupport_$pci_bus_number.log
 
@@ -12,7 +15,7 @@ ipmitool -H 10.96.30.225 -U ADMIN -I lanplus -P ADMIN sel elist > after_SEL_logs
 
 echo "Injecting SERR on pci bus $pci_bus_number"
 #setpci -s $pci_bus_number 60.L=00092981f
-setpci -s $pci_up_port CAP_EXP+08.L=0009291f
+setpci -s $PCIE_DEVICE CAP_EXP+08.L=0009291f
 echo "Capturing rocm_techsupport after SERR injection on $pci_bus_number"
 ../rocm_techsupport.sh > after_rocm_techsupport_$pci_bus_number.log
 
