61ceee7b0de50938fe419432423cd94e29ffab35
Your Name <you@example.com>
2018-06-22 07:31:36 -0400

    set-pcie-link.sh: working

--- rocm-scripts/FBA-241/set-pcie-link.sh
+++ rocm-scripts/FBA-241/set-pcie-link.sh
@@ -1,9 +1,12 @@
 echo ------------------
-setpci -s 2f:00.0 CAP_EXP+88.L=00000001
+setpci -s 2f:00.0 CAP_EXP+30.L=00000001
 lspci -s 2f:00.0 -vv | grep Lnk
+lspci -s 2f:00.0 -xxx | grep 80:
 echo ------------------
-setpci -s 2f:00.0 CAP_EXP+88.L=00000002
+setpci -s 2f:00.0 CAP_EXP+30.L=00000002
 lspci -s 2f:00.0 -vv | grep Lnk
+lspci -s 2f:00.0 -xxx | grep 80:
 echo ------------------
-setpci -s 2f:00.0 CAP_EXP+88.L=00000004
+setpci -s 2f:00.0 CAP_EXP+30.L=00000004
 lspci -s 2f:00.0 -vv | grep Lnk
+lspci -s 2f:00.0 -xxx | grep 80:
