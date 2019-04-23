db90b3acce053a07c730af55c475b72f8359ea2c
Your Name <you@example.com>
2019-04-23 18:05:21 -0400

    yeti-stream-2-pc-config-client.sh: added

---
+++ yeti-stream-2-pc-config-client.sh
@@ -0,0 +1,10 @@
+clear
+echo setting up Yeti on client machine...
+
+export LD_LIBRARY_PATH=~/yeti-eng-bundle/lib
+cd ~/yeti-eng-bundle/bin
+echo "Type, but do not execute the following command:"
+echo "./game_client run-direct <IPv4 address of the Yeti computer>:44700"
+
+
+
