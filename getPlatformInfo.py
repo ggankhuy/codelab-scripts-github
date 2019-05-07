e23f5f88a35e6734afde3d53648aa1d0c33fc66f
Your Name <you@example.com>
2019-05-06 20:51:51 -0700

    getPlatformInfo.py: added

---
+++ getPlatformInfo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#	Python 3 based script to collect information.
+
+#	HOST SYSTEMS: 
+#	Host OS (if launched from HOS OS, ignored if launched from VM) 
+#		BIOS version
+#		CPU model
+#		memory information
+#		GPU information
+#		OS version
+#		OS kernel version
+#		
+#	Guest OS running 
+#		BIOS version
+#		CPU information (number of cores, model )
+#		Memory information		
+#		GPU information
+#		GPU driver information (driver name, version)
+#		OS version
+#		OS kernel version
+#		drop version (/drop/<dropname>)
+
