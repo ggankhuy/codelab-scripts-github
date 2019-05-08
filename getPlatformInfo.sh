34b4263f261919cd2329c2427ae5b8fe114bb201
Your Name <you@example.com>
2019-05-08 14:09:27 -0700

    getPlatformInfo.sh: improved formatting

--- getPlatformInfo.sh
+++ getPlatformInfo.sh
@@ -21,8 +21,14 @@
 
 source common.sh
 p1=$1
+SINGLE_BAR='---------------------------------------'
+DOUBLE_BAR='======================================='
+apt install virt-what sshpass -y 
 
-apt install virt-what -y
+if [[ $?  -ne 0 ]] ; then
+	echo "ERROR: Failed to install packages..."
+	exit 1
+fi
 
 function do_inside_vm () {
 	echo do_inside_vm
@@ -34,18 +40,23 @@ if [[ -z `which virt-what` ]] ; then
 fi
 
 clear
+echo $DOUBLE_BAR
 
 if [[ -z `virt-what` ]] ; then
         echo `hostname`
 	echo "HOST: "
-	echo " -----------------"
+	echo $SINGLE_BAR
 	uname -r
-	echo " -----------------"
+	echo $SINGLE_BAR
 	lsb_release --all
-	echo " -----------------"
+	echo $SINGLE_BAR
 	lsmod | egrep "^amdkfd|^amdgpu"
-	echo " -----------------"
+	echo $SINGLE_BAR
 	modinfo amdgpu | egrep "^filename|^version"
+	echo $SINGLE_BAR
+	dmidecode -t 0  | grep Version
+	echo $SINGLE_BAR
+        echo BIOS version: `dmidecode -t 0  | grep Version`
 
 	#  ssh to vm, vm number is specified in $1.
 	
@@ -53,8 +64,24 @@ if [[ -z `virt-what` ]] ; then
 		echo "ERROR: VM No. is not specified. Use virsh list to get index" 
 		exit 1
 	fi
-	vmIp=`virsh domifaddr $1 | egrep "[0-9]+\.[0-9]+\." | tr -s ' ' | cut -d ' ' -f5 | cut -d '/' -f1`
+	vmIp=`virsh domifaddr $p1 | egrep "[0-9]+\.[0-9]+\." | tr -s ' ' | cut -d ' ' -f5 | cut -d '/' -f1`
 	echo "VM IP: " $vmIp
 fi
+
+echo $DOUBLE_BAR
+echo  "VM: $p1"
+echo $SINGLE_BAR
+sshpass -p amd1234 ssh root@$vmIp 'uname -r && lsb_release --all'
+echo $SINGLE_BAR
+sshpass -p amd1234 ssh root@$vmIp 'uname -r'
+echo $SINGLE_BAR
+sshpass -p amd1234 ssh root@$vmIp 'hostname'
+echo $SINGLE_BAR
+#sshpass -p amd1234 scp root@$vmIp '/work/drop*/debug-tool/amdvbflash -ai'
+
+
+echo $DOUBLE_BAR
+
+
 
 
