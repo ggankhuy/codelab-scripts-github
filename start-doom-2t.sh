95e6e4918d5c3d200e36bbeb53652f7fd9fe5223
root <root@debian.debian>
2019-05-10 14:38:07 -0400

    start-doom-t1/t2.sh: added

--- start-doom-2t.sh
+++ start-doom-2t.sh
@@ -1,7 +1,5 @@
-#/bin/bash ./yeti-game-test.sh doom yeti 2 t1 > t1.log &
-/bin/bash ./yeti-game-test.sh doom yeti 2 t2 > t2.log &
-echo =======================
-cat t1.log | grep tail -n 2
-echo =======================
-cat t2.log | grep tail -n 2
-echo =======================
+# 	fixed everything.
+#	problem is at the end of each of t1 and t2 execution, different present directory is expected so 
+#	this script may be just toast. 
+/bin/bash ./yeti-game-test.sh doom yeti 2 t1 > t1.log && cat t1.log | tail -n 2 &
+/bin/bash ./yeti-game-test.sh doom yeti 2 t2 > t2.log && cat t2.log | tail -n 2 &
