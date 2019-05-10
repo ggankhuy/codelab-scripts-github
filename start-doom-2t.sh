4f5879ca32016a2572ca74a8f1010ca77c0ecbd7
root <root@debian.debian>
2019-05-10 14:56:59 -0400

    removed start-doom scripts

--- start-doom-2t.sh
+++
@@ -1,5 +0,0 @@
-# 	fixed everything.
-#	problem is at the end of each of t1 and t2 execution, different present directory is expected so 
-#	this script may be just toast. 
-/bin/bash ./yeti-game-test.sh doom yeti 2 t1 > t1.log && cat t1.log | tail -n 2 &
-/bin/bash ./yeti-game-test.sh doom yeti 2 t2 > t2.log && cat t2.log | tail -n 2 &
--- start-doom-t2.sh
+++
@@ -1,4 +0,0 @@
-# 	fixed everything.
-#	problem is at the end of each of t1 and t2 execution, different present directory is expected so 
-#	this script may be just toast. 
-/bin/bash ./yeti-game-test.sh doom yeti 2 t2 
