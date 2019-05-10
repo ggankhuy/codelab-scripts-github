5450342b13796e36a1b532f841778da4a6a4287b
root <root@debian.debian>
2019-05-10 14:46:13 -0400

    common.sh: code clean up

--- common.sh
+++ common.sh
@@ -104,16 +104,16 @@ function common_setup () {
 		fi
 	fi 
 
-	if [[ -z `cat ~/.bashrc | grep "cd /git.co/ad-hog-scripts"` ]] ; then
+	if [[ -z $GIB_DROP_ROOT ]] ; then
+        	echo "GIB_DROP_ROOT is not defined. Please defined the root in ~/.bashrc"
+        	exit 1
+	fi
+
+	if [[ -z `cat ~/.bashrc | grep "cd /git.co/ad-hoc-scripts"` ]] ; then
 		echo "adding: cd /git.co/ad-hoc-scripts..."
 		echo "cd /git.co/ad-hoc-scripts" >> ~/.bashrc
 	else
 		echo "already present: cd /git.co/ad-hoc-scripts..."
-	fi
-
-	if [[ -z $GIB_DROP_ROOT ]] ; then
-        	echo "GIB_DROP_ROOT is not defined. Please defined the root in ~/.bashrc"
-        	exit 1
 	fi
 
 	#  inserting amdgpu module just in case, sometimes not loaded.
