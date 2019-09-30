29fdef45974700299f7f242fdb022424ad45131e
root <root@ixt-rack-70.local.lan>
2019-09-30 22:44:07 +0000

    tool-test.sh: included other tests

--- tool-test.sh
+++ tool-test.sh
@@ -1,6 +1,12 @@
 ATITOOL=/root/tools/atitool/tool-test/atitool
+AGTTOOL=/root/tools/agt/agt
+AMDVBFLASH=/root/tools/amdbvflash/tool-test/amdvbflash
+AMDVBFLASH=/root/tools/amdvbflash/tool-test/amdvbflash
 DATE=`date +%Y%m%d-%H-%M-%S`
 
+echo =======================================
+echo STARTING ATITOOL test
+echo =======================================
 if [[ -z $ATITOOL ]] ; then
 	echo $ATITOOL does not exit!
 	exit 1
@@ -8,7 +14,7 @@ fi
 
 $ATITOOL 
 $ATITOOL pm
-$ATITOOL  -pmlogall -pmperiod=100 -pmcount=500 -pmoutput=atitool.$DATE.log -pmnoesckey
+$ATITOOL  -pmlogall -pmperiod=100 -pmcount=500 -pmoutput=atitool.$DATE.log 
 
 $ATITOOL  hwid
 $ATITOOL -devid 
@@ -22,3 +28,23 @@ $ATITOOL -ppdpmstatus
 $ATITOOL -ppstatus
 $ATITOOL -pplist
 $ATITOOL -pplist=full
+
+echo =======================================
+echo STARTING AGT TOOL test
+echo =======================================
+
+$AGTTOOL clock
+$AGTTOOL -clkstatus 
+$AGTTOOL  -pmlogall -pmperiod=100 -pmcount=500 -pmoutput=agttool.$DATE.log
+$AGTTOOL -ppdpmstatus
+$AGTTOOL -ppstatus
+$AGTTOOL -pplist
+$AGTTOOL -pplist=full
+
+echo =======================================
+echo STARTING AMDVBFLASH TOOL test
+echo =======================================
+$AMDVBFLASH -ai
+$AMDVBFLASH -s 0 amdvblfash.out.vbios.$DATE.bin
+
+
