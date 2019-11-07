07883f4147c773d55e45282d618f8ff2ed424f37
Gankhuyag <ggankhuy@amd.com>
2019-11-06 22:57:07 -0800

    print priority count OK.

--- bug-report/generate-report-csv.py
+++ bug-report/generate-report-csv.py
@@ -4,8 +4,8 @@ fileName=None
 import numpy as np
 from numpy import *
 import csv
-
-#import panda as pd
+headers=None
+debug=0
 
 try:
 	fileName=sys.argv[1]
@@ -20,21 +20,27 @@ if not fp:
 	print("Failed to open file.")
 	quit(1)
 	
-#content=fp.readlines()
-#print(content)
-#print(type(content))
-
 with open(fileName) as f:
 	reader = csv.reader(f, delimiter=',')
-	headers = next(reader)
+	#headers = next(reader)
 	data = list(reader)
 	data=np.array(data)
 
-print(headers)
-print(data.shape)
-print(data)
-#data=pf.read_csv(fileName, sep=',', header=None)
-#print(data.values)
+if debug:
+	print("data dimension: ", data.shape)
+	print("data type: ", type(data))
+	print("------------------")
+
+priority=list(data[:,1])
+
+if debug:
+	print(type(priority), priority)
+
+print("Total tickets:")
+for i in range(0, 7):
+	priority_index='P' + str(i)
+	print(priority_index, ": ", priority.count(priority_index))
+	
 
 
 	
