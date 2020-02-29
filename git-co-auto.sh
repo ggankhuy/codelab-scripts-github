11b0d2effaee93a9346b4ea060a89e6a70f08496
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-02-28 19:16:16 -0500

    git-co-auto.sh: added

---
+++ git-co-auto.sh
@@ -0,0 +1,50 @@
+CONFIG_USE_SSH_AMD_CLOUD_GPU=1
+
+if [[ $CONFIG_USE_SSH_AMD_CLOUD_GPU -eq 1 ]] ; then
+    amd_cloud_gpu_repos=(\
+    git@github.com:AMD-CloudGPU/Gibraltar-LibGV.git
+    )
+else
+    amd_cloud_gpu_repos=(\
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LinuxGuestKernel.git \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Vulkan \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-GIM \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Libdrm \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LibGV.git \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-misc.git\
+    )
+fi
+
+mkdir amd-gib
+cd amd-gib
+
+for i in ${amd_cloud_gpu_repos[@]}
+do
+    echo $i
+    echo cloning $i...
+    echo "amd1234A#" | git clone $i
+    #expect "Password for 'https://ggghamd@github.com'"
+    #expect "Password for"
+    #send -- "amd1234A#\r" 
+    sleep 1
+done
+
+cd  ..
+
+amd_gerrit_repos=(\
+ssh://gerritgit/gpu-virtual/ec/driver/libgv \
+ssh://gerritgit/gpu-virtual/ec/tool/smi-lib \
+ssh://gerritgit/brahma/ec/drm \
+)
+
+mkdir gerritt
+cd gerritt
+
+for i in ${amd_gerrit_repos[@]}
+do
+    echo $i
+    git clone $i
+    sleep 1
+done
+
+cd ..
