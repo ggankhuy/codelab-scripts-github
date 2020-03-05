0039ebcae7716331ceb4233ebfeb6c7fb40ece4d
Guyen Gn <Guyen.Gankhuyag@amd.com>
2020-03-04 17:50:37 -0800

    filter invalid bug code complete, print buganizer issues afterward, test OK.

--- bug-report/compliance_check.py
+++ bug-report/compliance_check.py
@@ -10,6 +10,11 @@ from numpy import *
 from datetime import datetime, timedelta
 
 debug = 1
+debugL2 = 0
+
+colIndices=None
+validStats=["NEW","ACCEPTED","ASSIGNED"]
+PRIORITY_LOWEST = 7
 
 # Open internal jira
 # Open issues.csv (buganizer)
@@ -36,12 +41,47 @@ if debug:
 	print(type(buganizerIssues))
 	print(type(np_jiras))
 	print(type(np_buganizerIssues))
+
+# 	Construct a column indices from headers. 
+	
+colIndicesMain=setColumnIndices(headersBuganizerIssues)
+
+if not colIndicesMain:
+	print("Error: colIndicesMain failed to populate for ", fileName)
+	quit(1)
+
+print("colIndicesMain: ", colIndicesMain)
+
+#	Filter out tickets with invalid status
+
+rowsToDel=[]
+
+for i in range(0, len(np_buganizerIssues[:,colIndicesMain[COL_NAME_STATUS]])):
+	if debugL2:
+		print(i, ":")
+		
+	if not np_buganizerIssues[i, colIndicesMain[COL_NAME_STATUS]] in validStats:
+		if debugL2:
+			print("--- INFO: Removing the row with status: ", ", ID: ", np_buganizerIssues[i,colIndicesMain[COL_NAME_ISSUE_ID]], ", STATUS: ", np_buganizerIssues[i,colIndicesMain[COL_NAME_STATUS]])
+		rowsToDel.append(i)
+	else:
+		if debugL2:
+			print("--- INFO: Keeping the row with status: ", ", ID: ", np_buganizerIssues[i,colIndicesMain[COL_NAME_ISSUE_ID]], ", STATUS: ", np_buganizerIssues[i,colIndicesMain[COL_NAME_STATUS]])
+
+np_buganizerIssues = np.delete(np_buganizerIssues, rowsToDel, 0)		
+
+	
+if debug:
+	print("np_buganizerIssues dimension: ", np_buganizerIssues.shape)
+	print("np_buganizerIssues type: ", type(np_buganizerIssues))
+	printBarSingle()	
 	
 # 	Iterate through all bus buganizer issues and search corresponding internal jira.
 #		Check to see internal jira opened
 #		1. Search ID in title, if found OK.
 #		2. Search title string, if found OK.
 #		3. If 1. and 2. fails output error.
+
 
 
 
