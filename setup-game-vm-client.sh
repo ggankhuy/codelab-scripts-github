5a505f982bbe720ede09b129aed731eadd5312fb
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-04-22 08:56:14 -0700

    setup-game-vm-client.sh: use master now.

--- setup-game-vm-client.sh
+++ setup-game-vm-client.sh
@@ -2,9 +2,8 @@ echo "mkdir git.co directory..."
 echo -e "amd1234\n" | sudo mkdir /git.co
 uname -r
 
-nmcli c mod "Wired connection 1" ipv4.never-default true
+sudo nmcli c mod "Wired connection 1" ipv4.never-default true
 echo "make sure to reboot after nmcli configuration..."
-sleep 5
 
 echo "git clone..."
 #cd /git.co ; echo -e "g00db0y\n" | sudo git clone ssh://ixt-rack-85@10.216.64.102:32029/home/ixt-rack-85/gg-git-repo/
