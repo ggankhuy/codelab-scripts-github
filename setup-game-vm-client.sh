7d3be2900cf596ac467e1bfddda555f4cebee32b
Your Name <you@example.com>
2020-04-24 02:18:28 +0800

    setup-game-vm-client.sh: pal setting file configuration support added

--- setup-game-vm-client.sh
+++ setup-game-vm-client.sh
@@ -1,9 +1,16 @@
+AMD_PAL_SETTING_CFG_FILE=/etc/amd/amdPalSettings.cfg
 echo "mkdir git.co directory..."
 echo -e "amd1234\n" | sudo mkdir /git.co
 uname -r
 
 sudo nmcli c mod "Wired connection 1" ipv4.never-default true
 echo "make sure to reboot after nmcli configuration..."
+
+echo "DebugOverlayEnabled,1  > $AMD_PAL_SETTING_CFG_FILE
+echo "DebugOverlayConfig.UseDebugOverlayOnColorSpaceConversionCopy,1 >> $AMD_PAL_SETTING_CFG_FILE
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 
 echo "git clone..."
 #cd /git.co ; echo -e "g00db0y\n" | sudo git clone ssh://ixt-rack-85@10.216.64.102:32029/home/ixt-rack-85/gg-git-repo/
