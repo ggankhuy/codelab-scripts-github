54117fde8a475c64701813c2d3c92037055d2916
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-04-24 00:27:45 -0400

    git-co-auto.sh: added vkexamples

--- git-co-auto.sh
+++ git-co-auto.sh
@@ -1,4 +1,11 @@
 CONFIG_USE_SSH_AMD_CLOUD_GPU=0
+#git config credential.helper store
+if [[ -z `cat ~/.git-credentials | grep ggghamd`  ]] ; then
+    echo "storing credentials for ggghamd@github.com"
+    echo "https://ggghamd:amd1234A%23@github.com" >> ~/.git-credentials
+else
+    echo "credentials for ggghamd@github.com is already stored, continuing..."
+fi
 
 if [[ $CONFIG_USE_SSH_AMD_CLOUD_GPU -eq 1 ]] ; then
     amd_cloud_gpu_repos=(\
@@ -6,12 +13,13 @@ if [[ $CONFIG_USE_SSH_AMD_CLOUD_GPU -eq 1 ]] ; then
     )
 else
     amd_cloud_gpu_repos=(\
-    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LinuxGuestKernel.git \
+#    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LinuxGuestKernel.git \
     https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Vulkan \
     https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-GIM \
     https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Libdrm \
     https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LibGV.git \
-    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-misc.git\
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-misc.git \
+    https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-VkExamples \
     )
 fi
 
@@ -22,11 +30,14 @@ for i in ${amd_cloud_gpu_repos[@]}
 do
     echo $i
     echo cloning $i...
-    echo "amd1234A#" | git clone $i
-    #expect "Password for 'https://ggghamd@github.com'"
-    #expect "Password for"
-    #send -- "amd1234A#\r" 
-    sleep 1
+    #git clone $i 
+    ln ../expect.sh .
+    export EXPECT_URL=$i
+    echo "EXPECT_URL1: $EXPECT_URL"
+    expect ./expect.sh
+    #expect "Password for '$i':"
+    #send -- "amd1234A#\r"
+    #expect eof
 done
 
 cd  ..
