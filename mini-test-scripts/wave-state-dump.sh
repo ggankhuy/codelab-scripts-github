e62791ded49ac3db2146f79c2d4651bfb7dca673
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-05-15 15:32:35 +0800

    wave-state-dump.sh: conditionalize umr path

--- mini-test-scripts/wave-state-dump.sh
+++ mini-test-scripts/wave-state-dump.sh
@@ -1,3 +1,5 @@
+CONFIG_UMR_USE_VATS2_PATH=0
+
 dmesg --clear
 modprobe amdgpu
 dmesg >> dmesg.amdgpu.log
@@ -11,7 +13,15 @@ dmesg  >> dmesg.quark.log
 dmesg --clear
 cd ..
 echo "calling umr..."
-./umr -O halt_waves -wa
+
+
+
+if [[ $CONFIG_UMR_USE_VATS2_PATH -eq 0 ]] ; then
+	./umr -O halt_waves -wa
+else
+	/usr/local/bin/umr -O halt_waves -wa
+fi
+
 ls -ltr /var/log/Hang*
 dmesg >> dmesg.umr.log
 dmesg
