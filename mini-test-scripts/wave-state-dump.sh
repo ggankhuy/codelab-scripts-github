a9ff9386dea7151f50a57ee3e04092d45bc3b4d1
Your Name <you@example.com>
2020-05-15 08:18:47 +0800

    mini-test-scripts/wave-state-dump.sh: dmesg save added

--- mini-test-scripts/wave-state-dump.sh
+++ mini-test-scripts/wave-state-dump.sh
@@ -1,13 +1,20 @@
+dmesg --clear
 modprobe amdgpu
+dmesg >> dmesg.amdgpu.log
+dmesg --clear
 pushd /work/ubuntu_guest_package/utilities/debug-tool/debug
 python3 hangdump.py start
 cd ../quark
 ./quark tests/production/hang/hang_vm_gfx0_bad_cs_dispatch.lua  &
-sleep 5
+sleep 10
+dmesg  >> dmesg.quark.log
+dmesg --clear
 cd ..
 echo "calling umr..."
 ./umr -O halt_waves -wa
 ls -ltr /var/log/Hang*
+dmesg >> dmesg.umr.log
+dmesg
 
 
 
