36e6b66feff9f1c2c919a9e64d13a3e06772e010
Guyen Gn <Guyen.Gankhuyag@amd.com>
2020-09-07 14:51:57 -0700

    jira-report-mathplotlib.py: generated closed and open dates as dates only, trimmed time

--- bug-report/jira-report/jira-report-mathplotlib.py
+++ bug-report/jira-report/jira-report-mathplotlib.py
@@ -6,6 +6,27 @@ import csv
 with open('INTERNAL 2020-09-07T14_15_52-0500-filtered.csv', 'r') as f:
     jiradata = list(csv.reader(f, delimiter=','))
 
+#for i in jiradata:
+#	print(i)
+
+#npjiradata = np.array(jiradata[1:], dtype=np.float)
+npjiradata = np.array(jiradata)
+
+npjiradata_date_closed=npjiradata[:,8:9]
+npjiradata_date_open=npjiradata[:,9:10]
+
+print(npjiradata_date_closed)
+print(type(npjiradata_date_closed))
+print(npjiradata_date_closed.shape[0])
+
+for i in range(0, npjiradata_date_closed.shape[0]):
+	npjiradata_date_closed[i][0]=npjiradata_date_closed[i][0].split(' ')[0]
+	npjiradata_date_open[i][0]=npjiradata_date_open[i][0].split(' ')[0]
+	#print(npjiradata_date_closed[i][0])
+	#print(type(npjiradata_date_closed[i][0]))
+	
+print(npjiradata_date_closed)
+print(npjiradata_date_open)
 '''
 t = np.arange(0., 5., 0.2)
 
@@ -14,4 +35,4 @@ plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 #plt.plot([1, 2, 3, 4], [10, 20, -4, 100], 'ro')
 #plt.ylabel('some numbers')
 '''
-plt.show()
+#plt.show()
