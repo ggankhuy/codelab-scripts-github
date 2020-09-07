a2982432610e3c6783b6bbe542a8a3c92f4482cc
Your Name <you@example.com>
2020-09-07 14:22:42 -0700

    import csv to numPy, not working error.

--- bug-report/jira-report/jira-report-mathplotlib.py
+++ bug-report/jira-report/jira-report-mathplotlib.py
@@ -1,12 +1,17 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
+with open('INTERNAL 2020-09-07T14_15_52-0500-filtered.csv', 'r') as f:
+    jiradata = list(csv.reader(f, delimiter=','))
+
+'''
 t = np.arange(0., 5., 0.2)
 
 plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 
 #plt.plot([1, 2, 3, 4], [10, 20, -4, 100], 'ro')
 #plt.ylabel('some numbers')
-
+'''
 plt.show()
--- bug-report/jira-report/matplotlib-tutorial.py
+++ bug-report/jira-report/matplotlib-tutorial.py
@@ -4,9 +4,31 @@ import matplotlib.pyplot as plt
 
 t = np.arange(0., 5., 0.2)
 
-plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+#plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 
 #plt.plot([1, 2, 3, 4], [10, 20, -4, 100], 'ro')
 #plt.ylabel('some numbers')
 
+'''
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+plt.figure(figsize=(9, 3))
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+'''
+
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
 plt.show()
