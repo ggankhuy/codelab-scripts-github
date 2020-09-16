757ea0ce1902685c06bd47b86752c8d31fe7cb29
Guyen Gn <Guyen.Gankhuyag@amd.com>
2020-09-16 12:38:24 -0700

    github-co.sh: consolidated from git-co-auto.sh

--- github-co.sh
+++ github-co.sh
@@ -1,11 +1,30 @@
-amd_cloud_gpu_repos=(\
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LinuxGuestKernel.git \
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Vulkan \
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-GIM \
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-Libdrm \
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-LibGV.git \
-https://ggghamd@github.com/AMD-CloudGPU/Gibraltar-misc.git\
-)
+CONFIG_USE_SSH_AMD_CLOUD_GPU=0
+#git config credential.helper store
+
+if [[ -z `cat ~/.git-credentials | grep ggghamd`  ]] ; then
+    echo "storing credentials for ggghamd@github.com"
+    echo "https://ggghamd:amd1234A%23@github.com" >> ~/.git-credentials
+else
+    echo "credentials for ggghamd@github.com is already stored, continuing..."
+fi
+
+if [[ $CONFIG_USE_SSH_AMD_CLOUD_GPU -eq 1 ]] ; then
+    amd_cloud_gpu_repos=(\
+    git@github.com:AMD-CloudGPU/Gibraltar-LibGV.git
+    )
+else
+    amd_cloud_gpu_repos=(\
+#    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-LinuxGuestKernel.git \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-Vulkan \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-GIM \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-Libdrm \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-LibGV.git \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-misc.git \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/Gibraltar-VkExamples \
+    https://ggghamd:amd1234A%23@github.com/AMD-CloudGPU/SMI-Lib.git \
+	
+    )
+fi
 
 mkdir amd-gib
 cd amd-gib
@@ -22,10 +41,13 @@ cd  ..
 amd_gerrit_repos=(\
 ssh://gerritgit/gpu-virtual/ec/driver/libgv \
 ssh://gerritgit/gpu-virtual/ec/tool/smi-lib \
+ssh://gerritgit/gpu-virtual/ec/driver/vats2 \
+ssh://gerritgit/gpu-virtual/ec/driver/vats \	
 ssh://gerritgit/brahma/ec/drm \
 ssh://gerritgit/brahma/ec/linux \
 ssh://gerritgit/brahma/ec/umr \
 ssh://gerritgit/gpu-virtual/ec/driver/vats2 \
+ssh://gerritgit/gpu-virtual/ec/driver/gim \
 ssh://gerritgit/android/ec/amd/vendor/amd/proprietary/bin/tools/quark \
 )
 
