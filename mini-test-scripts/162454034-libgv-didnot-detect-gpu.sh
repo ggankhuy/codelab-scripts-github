23e131cde10410621967b2ff73d753e810f854d1
root <root@ixt-hq-44.local.lan>
2020-10-07 06:24:12 +0000

    162454034-libgv-didnot-detect-gpu.sh: truncate new line from ssh output

--- mini-test-scripts/162454034-libgv-didnot-detect-gpu.sh
+++ mini-test-scripts/162454034-libgv-didnot-detect-gpu.sh
@@ -85,10 +85,10 @@ for (( i=0 ; i < $CONFIG_ITER ; i++ )) ; do
 	echo "sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP 'lspci | grep -i amd | grep Disp'"
 	sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP "$CONFIG_PATH_AMDVBFLASH -i" >  $CONFIG_PATH_LOG/amdbvflash.log
 	echo "--- iteration $i: ---" >> $DIRNAME/summary.log
-	res=`sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP "lspci | grep -i amd | grep Disp | wc -l" | tee -a  >> $DIRNAME/summary.log`
-	echo "No. of gpu-s detected by lspci: " $res | tee -a  >> $DIRNAME/summary.log
-	res=`sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP "$CONFIG_PATH_AMDVBFLASH -ai | grep Adapter | wc -l" | tee -a  >> $DIRNAME/summary.log`
-	echo "No. of gpu-s detected by amdvbflash:" $res | tee -a  >> $DIRNAME/summary.log
+	res=`sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP "lspci | grep -i amd | grep Disp | wc -l" | tee -a  >> $DIRNAME/summary.log` | tr -d '\n'
+	echo -n "retry 1. No. of gpu-s detected by lspci : $res " | tee -a  >> $DIRNAME/summary.log
+	res=`sshpass -p $CONFIG_OS_PW ssh -o StrictHostKeyChecking=no $CONFIG_OS_USERNAME@$CONFIG_OS_IP "$CONFIG_PATH_AMDVBFLASH -ai | grep Adapter | wc -l" | tee -a  >> $DIRNAME/summary.log`  | tr -d '\n'
+	echo -n "No. of gpu-s detected by amdvbflash: $res" | tee -a  >> $DIRNAME/summary.log
 
 	for (( j=$CONFIG_GPU_FLASH_IDX_MIN ; j < $CONFIG_GPU_FLASH_IDX_MAX; j++ )) ; do
 		if [[ $CONFIG_ENABLE_CLEAR -eq 1 ]] ; then clear ; fi
