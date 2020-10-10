0c58f7c096a6be3429fcf802445bcf2f8b4a31ed
Guyen Gn <Guyen.Gankhuyag@amd.com>
2020-10-10 10:36:20 -0700

    jira-report-mathplotlib.py: improved histogram visuals

--- bug-report/jira-report/jira-report-mathplotlib.py
+++ bug-report/jira-report/jira-report-mathplotlib.py
@@ -7,6 +7,11 @@ from datetime import datetime, timedelta
 
 datetimeToday=datetime.today()
 fileName='INTERNAL 2020-10-10T01_44_07-0500-filtered.csv'
+
+# Open exported file: exported from Jira. Column 8-9 must contain closed and open dates, respectively otherwise
+# Code will either break or need adjustment. 
+# Column 13 must be a priority, otherwise code will either break or need adjustment. 
+
 with open(fileName, 'r') as f:
     jiraData = list(csv.reader(f, delimiter=','))
 jiraDataDates=[]
@@ -16,8 +21,8 @@ jiraDataP0Dates=[]
 jiraDataP1=[]
 jiraDataP1Dates=[]
 
-#jiraData=jiraData[1][1]
-
+# jiraDataP0/P1: Gather p0 and p1 designated tickets.
+# jiraDataDates: Gather only column 8, 9 which contains closed and open dates only. 
 for i in range(0, len(jiraData)):
     jiraDataDates.append(jiraData[i][8:10])
 	
@@ -26,6 +31,8 @@ for i in range(0, len(jiraData)):
 		
     if re.search("P2", jiraData[i][13]):
         jiraDataP1.append(jiraData[i])
+
+# jiraDataDatesP0/P1: Gather only column 8, 9 for P0/P1 which contains closed and open dates only. 
 
 for i in range(0, len(jiraDataP0)):
     jiraDataP0Dates.append(jiraDataP0[i][8:10])
@@ -42,12 +49,14 @@ for i in jiraData:
 
 for i in jiraDataDates:
     print(i)
-	
+
+# holds delta (closed - open) in number of days	
 delta=[]
 deltaP0=[]
 deltaP1=[]
 
 print("converting to datetime format...")
+
 for i in range(1, len(jiraDataDates)):
     closedDate=datetime.strptime(jiraDataDates[i][0], '%m/%d/%Y %H:%M')
     openDate=datetime.strptime(jiraDataDates[i][1], '%m/%d/%Y %H:%M')
@@ -67,6 +76,8 @@ for i in jiraDataDates:
     print(i)
 print(len(jiraDataDates), len(jiraDataDates[0]))
 
+# Sort deltas
+
 delta.sort()
 deltaP0.sort()
 deltaP1.sort()
@@ -75,13 +86,16 @@ print("delta:")
 for i in delta:
     print(i)
 
+# convert delta (closed-open) to numpy format.
+
 npdelta=np.asarray(delta)
-print(npdelta.shape)
 npdeltaP0=np.asarray(deltaP0)
-print(npdelta.shape)
 npdeltaP1=np.asarray(deltaP1)
 print(npdelta.shape)
+print(npdeltaP0.shape)
+print(npdeltaP1.shape)
 
+# Obsolete code. 
 '''
 #n, bins, patches = plt.hist(delta, num_bins, normed=1, facecolor='blue', alpha=0.5)
 
@@ -91,42 +105,63 @@ print("hist: ", hist)
 print("hist.sum(): ", hist.sum())
 #print(np.sum(hist * np.diff(bin_edges)))
 '''
+
+# Create bins for histogram
+
 bins = [0, 7, 14,21, 28, 400] # your bins
 
 data=npdelta
 dataP0=npdeltaP0
 dataP1=npdeltaP1
 
-hist1, bin_edges = np.histogram(data,bins) # make the histogram
-hist2, bin_edges = np.histogram(dataP0,bins) # make the histogram
-hist3, bin_edges = np.histogram(dataP1,bins) # make the histogram
+# Create histogram data. 
+
+hist1, bin_edges = np.histogram(data, bins) # make the histogram
+hist2, bin_edges = np.histogram(dataP0, bins) # make the histogram
+hist3, bin_edges = np.histogram(dataP1, bins) # make the histogram
+
+# Create plot with 3 subplots arranged horizontally, set total size of plot.
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 
-# Plot the histogram heights against integers on the x axis
-ax1.bar(range(len(hist1)),hist1,width=1) 
-ax2.bar(range(len(hist2)),hist2,width=1) 
-ax3.bar(range(len(hist3)),hist3,width=1) 
+# Plot the histogram heights against integers on the x axis, specify fill and border colors and titles. 
+
+ax1.bar(range(len(hist1)), hist1, width=0.8, color='#aaaaff', edgecolor='#0000ff') 
+ax2.bar(range(len(hist2)), hist2, width=0.8, color='#aaffaa', edgecolor='#00ff00') 
+ax3.bar(range(len(hist3)), hist3, width=0.8, color='#ffaaaa', edgecolor='#ff0000') 
 ax1.set_title("All tickets: P0-Pn")
 ax2.set_title("P0 tickets")
 ax3.set_title("P1 tickets")
 
+# Set axis labels for x and y axis.	
 
 ax1.set(xlabel='Number of days to resolve', ylabel='Number of tickets')
 ax2.set(xlabel='Number of days to resolve', ylabel='Number of tickets')
 ax3.set(xlabel='Number of days to resolve', ylabel='Number of tickets')
 
+# Set the ticks to the middle of the bars.
 
-# Set the ticks to the middle of the bars
 ax1.set_xticks([0.5+i for i,j in enumerate(hist1)])
 ax2.set_xticks([0.5+i for i,j in enumerate(hist2)])
 ax3.set_xticks([0.5+i for i,j in enumerate(hist3)])
 
-# Set the xticklabels to a string that tells us what the bin edges were
+# Set the xticklabels to a string that tells us what the bin edges were.
+
 ax1.set_xticklabels(['{} - {}'.format(bins[i],bins[i+1]) for i,j in enumerate(hist1)])
 ax2.set_xticklabels(['{} - {}'.format(bins[i],bins[i+1]) for i,j in enumerate(hist2)])
 ax3.set_xticklabels(['{} - {}'.format(bins[i],bins[i+1]) for i,j in enumerate(hist3)])
 
+#	Make Y axis integer only.
+yint = []
+
+locs, labels = plt.yticks()
+for each in locs:
+    yint.append(int(each))
+plt.yticks(yint)
+
+plt.show()
+
+# obsolete code
 '''
 #this was cultprit!!!
 plt.hist(npdelta, bins=bins_list)
@@ -135,7 +170,6 @@ plt.ylabel('Number of tickets')
 plt.title(r'Defect resolution data')
 plt.xticks(bins_list, bins_list_x_axis_ticks)
 '''
-plt.show()
 '''
 plt.plot(bins, npdelta)
 plt.show()
