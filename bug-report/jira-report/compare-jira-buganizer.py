48aaa657e38be1c8b0808ec246b04ed448fd9a61
Guyen Gn <Guyen.Gankhuyag@amd.com>
2020-10-21 15:40:52 -0700

    compare-jira-buganizer.py: code complete, brief sanity test OK

--- bug-report/jira-report/compare-jira-buganizer.py
+++ bug-report/jira-report/compare-jira-buganizer.py
@@ -93,14 +93,17 @@ for i in range(0, len(jiraData)):
 		jiraDataIssueIds.append(jiraData[i][IDX_COL_JIRA_ISSUE_KEY])
 		jiraListIllegaSummaryTitle.append(jiraData[i][IDX_COL_JIRA_ISSUE_KEY])
 
-input("Press Enter to continue...") 
+if DEBUGL2:
+	input("Press Enter to continue...") 
 	
 print(DOUBLE_BAR)
 print("len of jiradata: ", len(jiraData))
 print("Len of jiraDataIssueIdsBuganizer: ", len(jiraDataIssueIdsBuganizer))
 print("len of jiraDataIssueIds:", len(jiraDataIssueIds))
 print(DOUBLE_BAR)
-input("Press Enter to continue...") 
+
+if DEBUGL2:
+	input("Press Enter to continue...") 
 	
 print("Following jira summary has illegal title, unable to find buganizer ID-s:")		
 for i in jiraListIllegaSummaryTitle:
@@ -113,12 +116,14 @@ for i in range(0,len(jiraDataIssueIdsBuganizer)):
 		for j in bugData:
 			#print("Checking if ", jiraDataIssueIdsBuganizer[i], " matches ", j)
 			if j[IDX_COL_BUG_ID].strip() == str(jiraDataIssueIdsBuganizer[i]):
-				print("Found matching issue ID in buganizer: ", str(j[IDX_COL_BUG_ID]))
+				if DEBUG:
+					print("Found matching issue ID in buganizer: ", str(j[IDX_COL_BUG_ID]))
 				
 				# Now real check if the jira id is not closed (closed data is empty) and buganizer status is NOT CLOSED.
 				
 				if not j[IDX_COL_BUG_STATUS] in BUGANIZER_OPEN_STATUSES and jiraData[i][IDX_COL_JIRA_CLOSED_DATE].strip()=='' and  jiraData[i][IDX_COL_JIRA_REJECTED_DATE].strip()=='':
-					print("Following jira is not closed: ", jiraData[i][IDX_COL_JIRA_ISSUE_KEY], ", Buganizer ID / status: ", j[IDX_COL_BUG_ID], "/", j[IDX_COL_BUG_STATUS])
+					if DEBUG:
+						print("Following jira is not closed: ", jiraData[i][IDX_COL_JIRA_ISSUE_KEY], ", Buganizer ID / status: ", j[IDX_COL_BUG_ID], "/", j[IDX_COL_BUG_STATUS])
 						
 					jiraListUnclosedTickets.append([jiraData[i][IDX_COL_JIRA_ISSUE_KEY], j[IDX_COL_BUG_ID], j[IDX_COL_BUG_STATUS]])
 					
@@ -128,99 +133,10 @@ for i in range(0,len(jiraDataIssueIdsBuganizer)):
 	else:
 		if DEBUGL2:
 			print("Skipping ", jiraDataIssueIds[i], " as it is empty.")
-		
-	
-'''	
-    
-print("Total No. of P0 tickets imported: ", len(jiraDataP0))    
-print("Total No. of P1 tickets imported: ", len(jiraDataP1))    
-    
-for i in jiraData:
-    print(i)
 
-for i in jiraDataDates:
-    print(i)
+print(DOUBLE_BAR)
+print("Jira tickets that are not closed (but closed on buganizer): ")
+for i in jiraListUnclosedTickets:
+		print(i)
+print(DOUBLE_BAR)
 
-# holds delta (closed - open) in number of days	
-delta=[]
-deltaP0=[]
-deltaP1=[]
-
-print("converting to datetime format...")
-
-for i in range(1, len(jiraDataDates)):
-    closedDate=datetime.strptime(jiraDataDates[i][0], '%m/%d/%Y %H:%M')
-    openDate=datetime.strptime(jiraDataDates[i][1], '%m/%d/%Y %H:%M')
-    delta.append((closedDate-openDate).days)
-	
-for i in range(1, len(jiraDataP0Dates)):
-    closedDate=datetime.strptime(jiraDataP0Dates[i][0], '%m/%d/%Y %H:%M')
-    openDate=datetime.strptime(jiraDataP0Dates[i][1], '%m/%d/%Y %H:%M')
-    deltaP0.append((closedDate-openDate).days)
-
-for i in range(1, len(jiraDataP1Dates)):
-    closedDate=datetime.strptime(jiraDataP1Dates[i][0], '%m/%d/%Y %H:%M')
-    openDate=datetime.strptime(jiraDataP1Dates[i][1], '%m/%d/%Y %H:%M')
-    deltaP1.append((closedDate-openDate).days)
-
-for i in jiraDataDates:
-    print(i)
-print(len(jiraDataDates), len(jiraDataDates[0]))
-
-# Sort deltas
-
-delta.sort()
-deltaP0.sort()
-deltaP1.sort()
-
-print("delta:")
-for i in delta:
-    print(i)
-
-# convert delta (closed-open) to numpy format.
-
-npdelta=np.asarray(delta)
-npdeltaP0=np.asarray(deltaP0)
-npdeltaP1=np.asarray(deltaP1)
-print(npdelta.shape)
-print(npdeltaP0.shape)
-print(npdeltaP1.shape)
-
-# Create bins for histogram
-
-bins = [0, 7, 14, 21, 28, 400] # your bins
-
-#data=npdelta
-#dataP0=npdeltaP0
-#dataP1=npdeltaP1
-data=[npdelta, npdeltaP0, npdeltaP1]
-# Create histogram data. 
-
-hist=[]
-for i in data:
-	hist.append(np.histogram(i, bins)[0])
-	
-# Create plot with 3 subplots arranged horizontally, set total size of plot.
-
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
-
-# Plot the histogram heights against integers on the x axis, specify fill and border colors and titles. 
-
-ax=[ax1, ax2, ax3]
-
-for i in range(0, len(ax)):
-	ax[i].bar(range(len(hist[i])), hist[i], width=0.8, color=color[i], edgecolor=edgecolor[i]) 
-	ax[i].set_title(titles[i])
-	ax[i].set(xlabel='Number of days to resolve', ylabel='Number of tickets')
-	ax[i].set_xticks([0.5+i for i,j in enumerate(hist[i])])
-	ax[i].set_xticklabels(['{} - {}'.format(bins[i],bins[i+1]) for i,j in enumerate(hist[i])])
-
-#	Make Y axis integer only.
-yint = []
-
-locs, labels = plt.yticks()
-for each in locs:
-    yint.append(int(each))
-plt.yticks(yint)
-plt.show()
-'''
