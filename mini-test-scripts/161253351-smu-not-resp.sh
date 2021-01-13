8a33882078bf1435afd82fb288964002dd60e3c7
root <root@ixt-rack-70.local.lan>
2021-01-13 03:48:05 -0500

    161253351-smu-not-resp.sh: made adjustment for ST server, pending test

--- mini-test-scripts/161253351-smu-not-resp.sh
+++ mini-test-scripts/161253351-smu-not-resp.sh
@@ -2,23 +2,30 @@
 # For 5.4.39, flash 20201023 VBIOS
 # For 4.15 kernel, flash 19Q3 VBIOS.
 
-HOST_IP="10.216.66.54"
+HOST_IP="10.6.168.76"
+BMC_IP="10.6.170.182"
 LOOP_COUNT=3
 EXTRA_SLEEP=14
 VBIOS_5438=/drop/20201023/linux_host_package/vbios/V340L/D0531800.D04
 VBIOS_415=/drop/drop-2019-q3-rc8-GOOD-install/drop-2019-q3-rc8/vbios/V340L/D0531800.Y03
 AMDVBFLASH_PATH=/root/tools/amdvbflash/amdvbflash-4.68/amdvbflash
-
+BMC_PW=0penBmc
+BMC_USE=root
+HOST_PW=amd1234
+HOST_USER=root
 for i in {0..3};
 do
 	echo "--- LOOP COUNT $i ----"
 
 	echo "setting kernel 4.15... and vbios to $VBIOS_415"
-	for i in {0..3}
+	for i in {0..14}
 	do
-		sshpass -p amd1234 ssh root@$HOST_IP "echo flash gpu $i ... ; $AMDVBFLASH_PATH -f -p $i $VBIOS_415"
+		sshpass -p $HOST_PW ssh $HOST_USER@$HOST_IP "echo flash gpu $i ... ; $AMDVBFLASH_PATH -f -p $i $VBIOS_415"
 	done
-	sshpass -p amd1234 ssh root@$HOST_IP 'cp /root/grub.4.15.log /etc/default/grub ; update-grub ; reboot'
+	sshpass -p $HOST_PW ssh $HOST_USER@$HOST_IP 'cp /root/grub.4.15.log /etc/default/grub ; update-grub'
+	sleep $EXTRA_SLEEP
+	sshpass -p $BMC_PW ssh $BMC_USER@$BMC_IP 'cd ~/BMC_Scripts/ ; python shut_down_system.py ; sleep 15; python boot_up_system.py'
+	if [[ $? -ne 0 ]] ; then echo "BMC is not pingable, can not continue..." ; exit 1; fi
 	sleep $EXTRA_SLEEP
 	for i in {0..60} ; 
 	do
@@ -33,14 +40,17 @@ do
 	echo "kernel: "
 	sshpass -p amd1234 ssh root@$HOST_IP 'uname -r'
 	echo "vbios:" 
-	sshpass -p amd1234 ssh root@$HOST_IP "$AMDVBFLASH_PATH -i"
+	sshpass -p $HOST_PW ssh root@$HOST_IP "$AMDVBFLASH_PATH -i"
 
 	echo "setting kernel 5.4.38+ and vbios to $VBIOS_5438..."
-	for i in {0..3}
+	for i in {0..14}
 	do
-		sshpass -p amd1234 ssh root@$HOST_IP "echo flash gpu $i ... ; $AMDVBFLASH_PATH -f -p $i $VBIOS_5438"
+		sshpass -p $HOST_PW ssh root@$HOST_IP "echo flash gpu $i ... ; $AMDVBFLASH_PATH -f -p $i $VBIOS_5438"
 	done
-	sshpass -p amd1234 ssh root@$HOST_IP 'cp grub.5.4.38.log /etc/default/grub ; update-grub ; reboot'
+	sshpass -p $HOST_PW ssh root@$HOST_IP 'cp grub.5.4.38.log /etc/default/grub ; update-grub'
+	sleep $EXTRA_SLEEP
+	sshpass -p $BMC_PW ssh $BMC_USER@$BMC_IP 'cd ~/BMC_Scripts/ ; python shut_down_system.py ; sleep 15; python boot_up_system.py'
+	if [[ $? -ne 0 ]] ; then echo "BMC is not pingable, can not continue..." ; exit 1; fi
 	sleep $EXTRA_SLEEP
 	for i in {0..60} ; 
 	do
@@ -53,7 +63,7 @@ do
 		sleep 5
 	done	
 	echo "kernel: "
-	sshpass -p amd1234 ssh root@$HOST_IP 'uname -r'
+	sshpass -p $HOST_PW ssh root@$HOST_IP 'uname -r'
 	echo "vbios:" 
-	sshpass -p amd1234 ssh root@$HOST_IP "$AMDVBFLASH_PATH -i"
+	sshpass -p $HOST_PW ssh root@$HOST_IP "$AMDVBFLASH_PATH -i"
 done	
