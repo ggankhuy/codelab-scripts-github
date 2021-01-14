66eeff906db94acb6ee9a386315fc9ba18087e1f
Your Name <you@example.com>
2021-01-13 20:14:04 -0800

    161253351-smu-not-resp.sh: reboot/powercycle logic fixed.

--- mini-test-scripts/161253351-smu-not-resp.sh
+++ mini-test-scripts/161253351-smu-not-resp.sh
@@ -16,6 +16,12 @@ BMC_IP="10.216.52.51"
 VBIOS_5438=/drop/20201023/linux_host_package/vbios/NAVI12/Gemini
 VBIOS_415=$VBIOS_5438
 
+#	IXT39 configuration
+
+HOST_IP="10.216.66.54"
+VBIOS_5438=/drop/20201023/linux_host_package/vbios/V340L/D0531800.D04
+VBIOS_415=/drop/drop-2019-q3-rc8-GOOD-install/drop-2019-q3-rc8/vbios/V340L/D0531800.Y03
+
 #	misc. configuration 
 
 LOOP_COUNT=3
@@ -60,27 +66,15 @@ function build_install_libgv() {
 	modprobe gim
 }
 
-function powercycle_st_server()
+function powercycle_server()
 {
+	HOST_RESPONSIVE=0
+
 	if [[ $CONFIG_REBOOT -eq 1 ]] ; then
 		echo "CONFIG_REBOOT is enabled. Rebooting instead of powercycling..." ; sleep 5
 		sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "reboot"
-		return 
-	fi
-
-	echo "Powercycling ..." 
-
-	# Power cycle the server now.
-
-	HOST_RESPONSIVE=0
-
-	for j in {0..3} ; 
-		do
-		echo "Trying to power cycle...Loop No. $j"
 		sleep $EXTRA_SLEEP
-		sshpass -p $BMC_PW ssh -o StrictHostKeyChecking=no $BMC_USER@$BMC_IP 'cd ~/BMC_Scripts/ ; python shut_down_system.py ; sleep 15; python boot_up_system.py'
-		sleep $EXTRA_SLEEP
-		for i in {0..60} ; 
+		for i in {0..60} ;
 		do
 			ping -c 1 $HOST_IP 
 			if [[ $? -eq 0 ]] ; then
@@ -91,8 +85,29 @@ function powercycle_st_server()
 			fi
 			sleep 5
 		done	
-	done
-	if [[ $HOST_RESPONSIVE -ne 1 ]] ; then echo "Host is not responding after 3 retries to powercycle. Can not continue..." ; exit 1; fi
+	else
+		# Power cycle the server now.
+
+		for j in {0..3} ; 
+		do
+			echo "Trying to power cycle...Loop No. $j"
+			sleep $EXTRA_SLEEP
+			sshpass -p $BMC_PW ssh -o StrictHostKeyChecking=no $BMC_USER@$BMC_IP 'cd ~/BMC_Scripts/ ; python shut_down_system.py ; sleep 15; python boot_up_system.py'
+			sleep $EXTRA_SLEEP
+			for i in {0..60} ;
+			do
+				ping -c 1 $HOST_IP 
+				if [[ $? -eq 0 ]] ; then
+					echo "system is pingable, give another $EXTRA_SLEEP sec..."
+					sleep $EXTRA_SLEEP
+					HOST_RESPONSIVE=1
+					break				
+				fi
+				sleep 5
+			done	
+		done
+	fi
+	if [[ $HOST_RESPONSIVE -ne 1 ]] ; then echo "Host is not responding after N? retries to powercycle. Can not continue..." ; exit 1; fi
 }
  
 for i in {0..3};
@@ -100,39 +115,34 @@ do
 	echo "--- LOOP COUNT $i ----"
 
 	echo "setting kernel 4.15... and vbios to $VBIOS_415"
-	for i in {0..14}
+	for i in {0..3}
 	do
 		if [[ $CONFIG_FAKE_FLASH_VBIOS -eq 1 ]] ; then
 			sleep 1
 			echo sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_415"
 			
 		else
-			#sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_415"
-			#ret=$?
-			#if [[ ! -z $ret ]] ; then echo "ret: $?. Can not continue." ; exit 1; fi
+			sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_415"
 		fi
 	done
 
 	build_install_legacy_gim
-	powercycle_st_server
+	powercycle_server
 
 	echo "vbios:" 
 	sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no root@$HOST_IP "$AMDVBFLASH_PATH -i"
 
-	echo "setting kernel 5.4.38+ and vbios to $VBIOS_5438..."
-	for i in {0..14}
+	for i in {0..3}
 	do
 		if [[ $CONFIG_FAKE_FLASH_VBIOS -eq 1 ]] ; then
 			sleep 1
 			echo sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_5438"
 		else
-			#sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_415"
-			#ret=$?
-			#if [[ ! -z $ret ]] ; then echo "ret: $?. Can not continue." ; exit 1; fi
+			sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_IP "$AMDVBFLASH_PATH -f -p $i $VBIOS_415"
 		fi
 	done
 	build_install_libgv
-	powercycle_st_server
+	powercycle_server
 
 	echo "vbios:" 
 	sshpass -p $HOST_PW ssh -o StrictHostKeyChecking=no root@$HOST_IP "$AMDVBFLASH_PATH -i"
