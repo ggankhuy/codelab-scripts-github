67792ebac7d9b3980afc63b9db31fbc2bf78eb43
Your Name <you@example.com>
2021-01-29 08:45:52 -0800

    161253351-smu-not-resp.sh: code fix and improvement

--- mini-test-scripts/monitor.sh
+++ mini-test-scripts/monitor.sh
@@ -4,6 +4,8 @@ CONFIG_INTERVAL_SEC=5
 CONFIG_LOOP_TIME=$(($CONFIG_DURATION_SEC / $CONFIG_INTERVAL_SEC))
 LOG_FILE=monitor.log
 echo "Running for Loop: $CONFIG_LOOP_TIME, Duration (sec): $CONFIG_DURATION_SEC" > $LOG_FILE
+cd /usr/src/gim-2.0.1.G.20201023/smi-lib/examples/monitor 
+make
 
 for i in $(seq 0 $CONFIG_DURATION_SEC) ; do
         echo "Loop No: $i" >> $LOG_FILE
