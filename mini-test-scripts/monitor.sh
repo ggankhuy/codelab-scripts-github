d0b527c20559f0abf3c985904b449d7a96e59562
Your Name <you@example.com>
2021-01-28 17:12:57 -0800

    monitor.sh: added

---
+++ mini-test-scripts/monitor.sh
@@ -0,0 +1,13 @@
+CONFIG_DURATION_HOURS=8
+CONFIG_DURATION_SEC=$((CONFIG_DURATION_HOURS * 3600))
+CONFIG_INTERVAL_SEC=5
+CONFIG_LOOP_TIME=$(($CONFIG_DURATION_SEC / $CONFIG_INTERVAL_SEC))
+LOG_FILE=monitor.log
+echo "Running for Loop: $CONFIG_LOOP_TIME, Duration (sec): $CONFIG_DURATION_SEC" > $LOG_FILE
+
+for i in $(seq 0 $CONFIG_DURATION_SEC) ; do
+        echo "Loop No: $i" >> $LOG_FILE
+        sleep $CONFIG_INTERVAL_SEC;
+        ./monitor  >> $LOG_FILE
+done
+
