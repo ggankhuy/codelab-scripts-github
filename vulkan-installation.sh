5677cf5dfcccf82ec7bad3ac785ad4d1cff2d9b6
root <root@ixt-rack-70.local.lan>
2021-01-30 03:50:20 -0500

    vulkan-installation.sh: enhancement.

--- vulkan-installation.sh
+++ vulkan-installation.sh
@@ -4,12 +4,12 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install libglm-dev cmake libxcb-dri3-0 libxcb-present0 libpciaccess0 libpng-dev libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev g++ gcc g++-multilib libmirclient-dev libwayland-dev libxrandr-dev libxcb-ewmh-dev git python3 bison -y
-sudo apt-get install qt5-default qtwebengine5-dev
+sudo apt-get install qt5-default qtwebengine5-dev -y
 apt update
 wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-bionic.list http://packages.lunarg.com/vulkan/lunarg-vulkan-bionic.list
 sudo apt update
-sudo apt install vulkan-sdk
+sudo apt install vulkan-sdk -y
 which vkvia
 vkvia
 vulkaninfo
