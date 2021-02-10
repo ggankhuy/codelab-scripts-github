47c6c638fb23c0533b6c119fce8ced7f48d5f957
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2021-02-10 13:20:31 -0500

    161253351: scripts

--- mini-test-scripts/161253351/monitor-bob.sh
+++ mini-test-scripts/161253351/monitor-bob.sh
@@ -1,6 +1,2 @@
-cd /git.co/ad-hoc-scripts/mini-test-scripts/161253351
-i=0; 
-while true; 
-do echo "monitor: $i" > monitor.log ; ./monitor > monitor.log ;i=$((i+1)); 
-done
+i=0; while true; do echo "monitor: $i"; ./monitor ;((i++)); done 
 
--- mini-test-scripts/161253351/vk-example-bob.sh
+++ mini-test-scripts/161253351/vk-example-bob.sh
@@ -1,7 +1,3 @@
 cd /work/vats/package_script/run/ubuntu
 chmod 755 vk_example.sh
-i=0
-while true; do 
-	echo "vkexample: $i" > 1.log; ./vk_example.sh /work/ubuntu_guest_package/utilities/test-apps/vk_examples/  > 1.log
-	i=$((i+1))
- done
+i=0; while true; do echo "vkexample: $i"; ./vk_example.sh /work/ubuntu_guest_package/utilities/test-apps/vk_examples/  >> 1.log ;((i++)); done
