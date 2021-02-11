6e40d817b4d7eb2ebc8bbc3250daf3ec25974e76
root <root@dvt1-74.amd.com>
2021-02-11 03:40:32 -0500

    vk-example-bob.sh: monitor counter log fix

--- mini-test-scripts/161253351/vk-example-bob.sh
+++ mini-test-scripts/161253351/vk-example-bob.sh
@@ -2,4 +2,4 @@ cd /work/vats/package_script/run/ubuntu
 chmod 755 yeti_environment.sh
 ./yeti_environment.sh /work/ubuntu_guest_package/utilities/test-apps/yeti
 chmod 755 vk_example.sh
-i=0; while true; do echo "vkexample: $i"; ./vk_example.sh /work/ubuntu_guest_package/utilities/test-apps/vk_examples/  >> 1.log ;((i++)); done
+i=0; while true; do echo "vkexample: $i"; ./vk_example.sh /work/ubuntu_guest_package/utilities/test-apps/vk_examples/  >> $i.log ;((i++)); done
