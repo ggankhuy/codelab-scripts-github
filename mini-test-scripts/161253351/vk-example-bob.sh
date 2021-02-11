c1dfae3c93ad53d85d96e20a74764a087eec2320
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2021-02-11 01:20:39 -0500

    mini-test-scripts/161253351/vk-example-bob.sh: add yeti env setup support

--- mini-test-scripts/161253351/vk-example-bob.sh
+++ mini-test-scripts/161253351/vk-example-bob.sh
@@ -1,3 +1,5 @@
 cd /work/vats/package_script/run/ubuntu
+chmod 755 yeti_environment.sh
+./yeti_environment.sh /work/ubuntu_guest_package/utilities/test-apps/yeti
 chmod 755 vk_example.sh
 i=0; while true; do echo "vkexample: $i"; ./vk_example.sh /work/ubuntu_guest_package/utilities/test-apps/vk_examples/  >> 1.log ;((i++)); done
