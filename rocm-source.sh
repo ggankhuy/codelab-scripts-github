a1e1f449dee7a065b483a8ed8f7524b97f097d8b
Your Name <you@example.com>
2021-03-30 19:25:49 -0400

    rocm-source.sh/ROCM-install.sh: changed to 4.1

--- rocm-source.sh
+++ rocm-source.sh
@@ -5,7 +5,7 @@ pushd  ~/ROCm/
 mkdir -p ~/bin/
 apt install curl -y && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-~/bin/repo init -u https://github.com/RadeonOpenCompute/ROCm.git -b roc-4.0.x
+~/bin/repo init -u https://github.com/RadeonOpenCompute/ROCm.git -b roc-4.1.x
 ~/bin/repo sync
 echo "ROCm source is downloaded to ~/ROCm"
 echo "push ~/ROCm to get there..."
