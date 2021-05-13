a80d6160163103d74ecd2555e837ca90094e5e6f
Your Name <you@example.com>
2021-05-14 07:31:06 +0800

    build-rocm.sh: rocm-compiiler-support build error fixed

--- build-rocm.sh
+++ build-rocm.sh
@@ -7,10 +7,11 @@ fi
 pushd llvm-project
 LOG_DIR=/log/rocmbuild/
 NPROC=`nproc`
-rm ~/ROCm
-ln -s ~/ROCm-$p1.0 ~/ROCm
-
+ROCM_SRC_FOLDER=~/ROCm-$p1
+ROCM_INST_FOLDER=/opt/rocm-$p1-0/
 function setup_root_rocm_softlink () {
+	rm ~/ROCm
+	ln -s $ROCM_SRC_FOLDER  ~/ROCm
 	if [[ $? -ne  0 ]] ; then 
 		echo "Error during setting up the softlink ~/ROCm"
 		ls -l ~
@@ -20,7 +21,7 @@ function setup_root_rocm_softlink () {
 
 function setup_opt_rocm_softlink () {
 	rm /opt/rocm
-	ln -s /opt/rocm-$p1-0/ /opt/rocm
+	ln -s /opt/$ROCM_INST_FOLDER/ /opt/rocm
 
 	if [[ $? -ne  0 ]] ; then 
 		echo "Error during setting up the softlink /opt/rocm"
@@ -32,24 +33,13 @@ function setup_opt_rocm_softlink () {
 setup_root_rocm_softlink
 setup_opt_rocm_softlink
 
-mkdir -p $LOG_DIR
-mkdir build ; cd build
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/opt/rocm-$p1.0/llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lld;lldb;clang-tools-extra;compiler-rt" ../llvm | tee $LOG_DIR/llvm.log
-make -j$NPROC  | tee -a $LOG_DIR/llvm.log
-make install  | tee -a $LOG_DIR/llvm.log
-popd
-
-pushd rocBLAS
-./install.sh -icd | tee $LOG_DIR/rocBLAS.log
-popd
-
-
-pushd ROCm-CompilerSupport
+pushd $ROCM_SRC_FOLDER/ROCm-CompilerSupport
 cd lib/comgr/
-LLVM_PROJECT=~/ROCm/llvm-project
-DEVICE_LIBS=~/ROCm/ROCm-Device-Libs/
-COMGR=~/ROCm/ROCm-CompilerSupport/lib/comgr
+LLVM_PROJECT=$ROCM_SRC_FOLDER/llvm-project
+DEVICE_LIBS=$ROCM_SRC_FOLDER/ROCm-Device-Libs/
+COMGR=$ROCM_SRC_FOLDER/ROCm-CompilerSupport/lib/comgr
 
+setup_root_rocm_softlink
 mkdir -p "$DEVICE_LIBS/build"
 cd "$DEVICE_LIBS/build"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$LLVM_PROJECT/build" .. | tee $LOG_DIR/ROCm-CompilerSupport.log
@@ -57,7 +47,7 @@ make -j`nproc` ; make install | tee -a $LOG_DIR/ROCm-CompilerSupport.log
 
 mkdir -p "$COMGR/build"
 cd "$COMGR/build"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$LLVM_PROJECT/build;$DEVICE_LIBS/build" .. | tee -a $LOG_DIR/ROCm-CompilerSupport.log
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$LLVM_PROJECT/build;$DEVICE_LIBS/build" .. | tee  $LOG_DIR/ROCm-CompilerSupport.log
 make -j$NPROC  | tee -a $LOG_DIR/ROCm-CompilerSupport.log
 make test | tee -a $LOG_DIR/ROCm-CompilerSupport.log
 make install | tee -a $LOG_DIR/ROCm-CompilerSupport.log
@@ -69,10 +59,24 @@ OPENCL_DIR=/root/ROCm/ROCm-OpenCL-Runtime/
 ROCclr_DIR=/root/ROCm/ROCclr/
 OLDPWD=/root/ROCm/ROCclr
 
-setup_root_rocm_softlink
 mkdir build; cd build
 cmake -DOPENCL_DIR="$OPENCL_DIR" -DCMAKE_INSTALL_PREFIX=/opt/rocm/rocclr .. | tee $LOG_DIR/ROCclr.log
 make -j$NPROC install | tee -a $LOG_DIR/ROCclr.log
+popd
+
+
+exit 0
+
+echo  qualified codes
+mkdir -p $LOG_DIR
+mkdir build ; cd build
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/opt/rocm-$p1.0/llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lld;lldb;clang-tools-extra;compiler-rt" ../llvm | tee $LOG_DIR/llvm.log
+make -j$NPROC  | tee -a $LOG_DIR/llvm.log
+make install  | tee -a $LOG_DIR/llvm.log
+popd
+
+pushd rocBLAS
+./install.sh -icd | tee $LOG_DIR/rocBLAS.log
 popd
 
 
