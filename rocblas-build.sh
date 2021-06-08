8e52ab4ee9a46738d83330ca90f90a9332998ad9
Your Name <you@example.com>
2021-06-08 11:37:33 -0700

    rocblas-build.sh: test version, code fix

--- rocblas-build.sh
+++ rocblas-build.sh
@@ -1,13 +1,15 @@
-builds=("-icd" "-icda gfx908:xnack-" "-icdn" "-icdna gfx908:xnack-")
-#builds=("-icda gfx908:xnack-")
+#builds=("-icd" "-icda gfx908:xnack-" "-icdn" "-icdna gfx908:xnack-")
+builds=("-icdn" "-icdna gfx908:xnack-")
 LOG_FOLDER=./log
 mkdir -p $LOG_FOLDER
+rm -rf ./build
+rm -rf ./$LOG_FOLDER
 for (( i=0 ; i < ${#builds[@]} ; i++ ))  ;do
-    rm -rf ./build
-    rm -rf ./log
+    CURR_LOG=$LOG_FOLDER/build.${builds[$i]}.log
     echo $i: ${builds[$i]}
     start=$SECONDS
-    ./install.sh ${builds[$i]} | tee $LOG_FOLDER/build.${builds[$i]}.log
+    ./install.sh ${builds[$i]} | tee $CURR_LOG
+    tree -f build | tee -a $CURR_LOG
     end=$SECONDS
     duration=$(( $end - $start ))
     echo "build: ${builds[$i]} - duration: $duration" >> $LOG_FOLDER/rocblas-build.log
