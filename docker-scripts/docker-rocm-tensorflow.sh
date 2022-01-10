5520980a5e089ca2152d75e5a59bf0891ca0bdfd
Your Name <you@example.com>
2022-01-10 09:30:18 -0800

    docker-rocm-tensorflow.sh: enhancement

--- docker-scripts/docker-rocm-tensorflow.sh
+++ docker-scripts/docker-rocm-tensorflow.sh
@@ -1,4 +1,35 @@
-VOLUME_NAME=rocm-tensorflow-persistent
+function usage (){
+    echo "Usage: $0 --ml <tensorflow|tf|pytorch|py>"
+}
+
+for var in "$@"
+do
+    if [[ ! -z `echo "$var" | grep "ml="` ]]  ; then
+        echo "ML selected: $var"
+        ML=`echo $var | cut -d '=' -f2`
+    fi
+done
+
+case "$ML" in
+   tensorflow|tf)
+        echo "Selected tf..."
+        ML=tensorflow
+      ;;
+   pytorch|py)
+        echo "Selected py..."
+        ML=pytorch
+      ;;
+   "")
+        echo "ml is not specified, defaulting to py..."
+        ML=pytorch
+      ;;
+   *)
+     usage; exit 1
+     ;;
+esac
+
+echo "ML: $ML"
+VOLUME_NAME=rocm-$ML-persistent
 sudo docker volume ls | grep $VOLUME_NAME
 
 ret=$?
@@ -9,5 +40,6 @@ else
     echo "volume $VOLUME_NAME does not exist. Creating one..."    
     sudo docker volume create --name $VOLUME_NAME
 fi
-sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx --mount source=$VOLUME_NAME,target=/data rocm/tensorflow:latest
+sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx --mount source=$VOLUME_NAME,target=/data \
+rocm/$ML:latest
 
