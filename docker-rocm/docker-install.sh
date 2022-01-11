f0798fed94d6aa05bbc7aa217cf004c6fb53703a
Your Name <you@example.com>
2022-01-10 17:51:48 -0800

    docker-install.sh: enhancement

--- docker-rocm/docker-install.sh
+++ docker-rocm/docker-install.sh
@@ -8,13 +8,13 @@ function docker_install_apt() {
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 }
 
 function docker_install_yum() {
@@ -60,4 +60,4 @@ case "$OS_NAME" in
      ;;
 esac
 
-docker_install_yum
+docker_install_apt
