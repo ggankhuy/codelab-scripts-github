9e19b72836b2a8352045851a6fa1b953c5889637
Your Name <you@example.com>
2022-02-09 07:07:40 +0000

    cmake-update.sh: centos update, test pending

--- cmake-update.sh
+++ cmake-update.sh
@@ -1,10 +1,31 @@
+
 p1=$1
 if [[ -z $p1 ]] ; then
 	VERSION=3.16.8
 else
 	VERSION=$p1
 fi
-sudo apt install build-essential libssl-dev -y
+
+OS_NAME=`cat /etc/os-release  | grep ^NAME=  | tr -s ' ' | cut -d '"' -f2`
+echo "OS_NAME: $OS_NAME"
+case "$OS_NAME" in
+   "Ubuntu")
+      echo "Ubuntu is detected..."
+      PKG_EXEC=apt
+      sudo apt install build-essential libssl-dev -y
+      ;;
+   "CentOS Linux")
+      echo "CentOS is detected..."
+      PKG_EXEC=yum
+      ln -s /usr/bin/python3  /usr/bin/python
+      yum groupinstall "Development Tools" -y
+      yum install openssl-devel -y
+      ;;
+   *)
+     echo "Unsupported O/S, exiting..." ; exit 1
+     ;;
+esac
+
 wget https://github.com/Kitware/CMake/releases/download/v$VERSION/cmake-$VERSION.tar.gz
 tar -zxvf cmake-$VERSION.tar.gz
 cd cmake-$VERSION
