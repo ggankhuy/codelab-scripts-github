848fb284f9d2345fa3fe4f8927a3b9f5a9167d0a
Your Name <you@example.com>
2022-03-01 11:36:37 -0500

    pXX-simple.cpp: added

---
+++ rocm/hip/cuda-by-example-port/pXX-managed/pXX-simple.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include "hip/hip_runtime.h"
+
+#define N 64
+#define ARRSIZE 3
+#define LOOPSTRIDE 8
+__global__ void add(int *a, int*b, int *c) {
+	int tid = hipBlockIdx_x;
+	c[tid] = a[tid] + b[tid];
+}
+
+/*
+int managed_memory = 0;
+HIPCHECK(hipDeviceGetAttribute(&managed_memory,
+ hipDeviceAttributeManagedMemory,p_gpuDevice));
+if (!managed_memory ) {
+ printf ("info: managed memory access not supported on the device %d\n Skipped\n",
+p_gpuDevice);
+}
+else {
+ HIPCHECK(hipSetDevice(p_gpuDevice));
+ HIPCHECK(hipMallocManaged(&Hmm, N * sizeof(T)));
+. . .
+}*/
+
+int main (void) {
+    int *a, *b, *c;
+    int *dev_a, *dev_b, *dev_c;
+    int i ;
+
+    int * devCount;
+    hipGetDeviceCount(devCount);
+    printf("No. of devices: %u.\n", devCount);
+    
+    if (*devCount < 1) {
+        printf("Unable to find gpu.\n");
+        return 1;
+    }
+    
+    int managed_memory = 0;
+    hipDeviceGetAttribute(&managed_memory, hipDeviceAttributeManagedMemory, 0);
+    if (!managed_memory ) {
+         printf ("info: managed memory access not supported on the device %d\n Skipped\n", 0);
+    } 
+    return 1;
+
+    hipSetDevice(0);
+    hipMallocManaged(&a, N * sizeof(*dev_a));
+    hipMallocManaged(&a, N * sizeof(*dev_b));
+    hipMallocManaged(&a, N * sizeof(*dev_c));
+
+	for (int i = 0; i < N ; i ++ ) {
+		a[i]  = i;
+		b[i] = i + i;
+		c[i] = 999;
+	}
+
+	for (int i = 0; i < N ; i+=LOOPSTRIDE ) {
+        printf("Before add: a/b: %d, %d.\n", a[i], b[i]);
+	}
+
+    const unsigned blocks = 256;
+    const unsigned threadsPerBlock = 1;
+
+    hipLaunchKernelGGL(add, blocks, threadsPerBlock, 0, 0, dev_a, dev_b, dev_c);
+    hipDeviceSynchronize();
+
+	for (int i = 0; i < N; i+=LOOPSTRIDE )
+		printf("After add: %d: %u + %u = %u\n", i, a[i], b[i], c[i]);
+
+    /*
+    hipFreeManaged(dev_a);
+    hipFreeManaged(dev_b);
+    hipFreeManaged(dev_c);
+    */
+    
+	return 0;
+}
