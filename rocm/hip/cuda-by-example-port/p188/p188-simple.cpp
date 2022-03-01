8c69998adb5cce0ad048ed73f85e9e7320871e16
Your Name <you@example.com>
2022-03-01 01:44:19 -0500

    hip vector examples are added

---
+++ rocm/hip/cuda-by-example-port/p188/p188-simple.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "hip/hip_runtime.h"
+
+#define N 64
+#define ARRSIZE 3
+#define LOOPSTRIDE 8
+__global__ void add(int *a, int*b, int *c) {
+	int tid = hipBlockIdx_x;
+	c[tid] = a[tid] + b[tid];
+}
+
+int main (void) {
+    int *a, *b, *c;
+    int *dev_a, *dev_b, *dev_c;
+    int i ;
+    
+
+    hipHostMalloc((void**)&a, N * sizeof(*a), hipHostRegisterDefault);
+    hipHostMalloc((void**)&b, N * sizeof(*b), hipHostRegisterDefault);
+    hipHostMalloc((void**)&c, N * sizeof(*c), hipHostRegisterDefault);
+
+ 	hipMalloc(&dev_a, N * sizeof(int) );
+ 	hipMalloc(&dev_b, N * sizeof(int) );
+ 	hipMalloc(&dev_c, N * sizeof(int) );
+
+	for (int i = 0; i < N ; i ++ ) {
+		a[i]  = i;
+		b[i] = i + i;
+		c[i] = 999;
+	}
+
+	for (int i = 0; i < N ; i+=LOOPSTRIDE ) {
+        printf("Before add: a/b: %d, %d.\n", a[i], b[i]);
+	}
+
+   	hipMemcpy(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice);
+   	hipMemcpy(dev_b, b, N * sizeof(int), hipMemcpyHostToDevice);
+   	hipMemcpy(dev_c, c, N * sizeof(int), hipMemcpyHostToDevice);
+    
+    const unsigned blocks = 256;
+    const unsigned threadsPerBlock = 1;
+
+    hipLaunchKernelGGL(add, blocks, threadsPerBlock, 0, 0, dev_a, dev_b, dev_c);
+
+    hipMemcpy(a, dev_a, N * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(b, dev_b, N * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(c, dev_c, N * sizeof(int), hipMemcpyDeviceToHost);
+
+	for (int i = 0; i < N; i+=LOOPSTRIDE )
+		printf("After add: %d: %u + %u = %u\n", i, a[i], b[i], c[i]);
+
+    hipFree(dev_a);
+    hipFree(dev_b);
+    hipFree(dev_c);
+    hipHostFree(a);
+    hipHostFree(b);
+    hipHostFree(c);
+    
+	return 0;
+}
