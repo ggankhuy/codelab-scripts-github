0cc12a74c70d189cd0b27b51abc4227b118ce715
Your Name <you@example.com>
2022-03-10 13:41:19 -0500

    repro-fba-212.sh: code complete, test ok, further enhancement still possible and desired

--- tickets/Ph/fba-212/repro-fba-212.sh
+++ tickets/Ph/fba-212/repro-fba-212.sh
@@ -6,26 +6,57 @@ USER=master
 PW=amd1234
 USER=root
 
+DATE=`date +%Y%m%d-%H-%M-%S`
+OUTPUT_DIR=fba-212-$DATE
 CONFIG_IP_GUEST=10.216.54.96
 LOOP_COUNT=3
 SINGLE_BAR="-----------------------------"
-for loop in {1..3} ; do
-    echo $SINGLE_BAR
+DOUBLE_BAR="============================="
+function wait_host_up() {
+    sleep 3
+    for i in {1..60} ; do
+        echo -ne "."
+        ping -c 1 -W 3 $CONFIG_IP_GUEST
+        if  [[ $? -eq 0 ]] ; then
+            echo "Host $CONFIG_IP_GUEST is back online."
+            return 
+        fi
+    done
+    echo "Host is not unreachable after reboot or not reachable at all."
+    exit 1
+}
+
+wait_host_up
+for loop in {1..10} ; do
+    echo $DOUBLE_BAR
     echo "Current loop: $loop........"
 
-    for cmd in "mkdir fba-212" "sudo dmesg --clear"  "sudo modprobe amdgpu" \
+    for cmd in  "sudo mkdir fba-212" \
+                "sudo dmesg | sudo tee fba-212/dmesg.after.reboot.'$loop'.log" \
+                "sudo dmesg --clear"  "sudo modprobe amdgpu" \
                 "sudo dmesg | sudo tee fba-212/dmesg.after.modprobe.amdgpu.'$loop'.log" \
-                "sudo dmesg --clear" "sudo rmmod amdgpu" "sudo dmesg | sudo tee fba-212/dmesg.after.rmmod.'$loop'.log"\
+                "/usr/local/bin/run_kfdtest.sh | sudo tee fba-212/$loop.kfdtest.log " \
+                "sudo dmesg --clear" "sudo rmmod amdgpu" "sudo dmesg | sudo tee fba-212/dmesg.after.rmmod.'$loop'.log" \
+                "sudo reboot" \
         ;do
-        echo $cmd
+        echo "$SINGLE_BAR"
+        echo --- $cmd ---
         sshpass -p $PW ssh -o StrictHostKeyChecking=no $USER@$CONFIG_IP_GUEST $cmd
         ret=$?
-
-        if [[ $ret != 0 && $ret != 1 ]] ; then 
-            echo "code: $ret. Error executing $cmd on remote $CONFIG_IP_GUEST with credentials: $USER/$PW"
-            exit 1
-        fi
+        
+        #if [[ $ret != 0 && $ret != 1 ]] ; then 
+        #    echo "code: $ret. Error executing $cmd on remote $CONFIG_IP_GUEST with credentials: $USER/$PW"
+        #    exit 1
+        #fi
         sleep 1
+
+        if [[ "$cmd" == "sudo reboot" ]] ; then
+            echo "Reboot issued. Waiting for system to become online"
+            wait_host_up
+        else
+            echo "not a reboot cmd"
+        fi
     done
 done
 
+sudo sshpass -p $PW scp -o StrictHostKeyChecking=no -r $USER@$CONFIG_IP_GUEST:~/fba-212 $OUTPUT_DIR
