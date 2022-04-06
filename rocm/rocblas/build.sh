400ca6e87e0588fd6e0a92d18ba0baee800a424e
Your Name <you@example.com>
2022-04-06 23:29:47 +0000

    rocsparse: example_axpyi.cpp, build.sh: added

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,5 +1,9 @@
 #/opt/rocm-4.5.2/hip/include/hip/
 #-I$(ROCM_PATH)/hsa/include
-hipcc -c sscal.cpp
+mkdir build
+cd build
+ln -s ../sscal.cpp
+hipcc --offload-arch=gfx908 --save-temps -c sscal.cpp
 hipcc sscal.o  /opt/rocm-4.5.2/lib/librocblas.so
+cd ..
 
