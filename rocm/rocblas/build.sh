5eda6742aee6ca6b7235fa86d6f4cf4d089519b6
Your Name <you@example.com>
2022-04-07 00:42:28 +0000

    build.sh: statement correction

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -4,7 +4,7 @@ rm -rf build/*
 mkdir build
 cd build
 #FILENAME=sscal
-FILENAME=example_sgemm-3
+FILENAME=example_sgemm
 ln -s ../$FILENAME.cpp
 hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
 hipcc $FILENAME.o  /opt/rocm-4.5.2/lib/librocblas.so
