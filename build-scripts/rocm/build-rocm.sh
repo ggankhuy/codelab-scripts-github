2f3d456bdbe2c171a972fa26dcd77e1cd3d47830
Your Name <you@example.com>
2022-05-08 05:11:17 +0000

    build-rocm.sh: syntax error

--- build-scripts/rocm/build-rocm.sh
+++ build-scripts/rocm/build-rocm.sh
@@ -159,7 +159,7 @@ case "$OS_NAME" in
    "Ubuntu")
       echo "Ubuntu is detected..."
       PKG_EXEC=apt
-  	  $PKG_EXEC install sqlite3 libsqlite3-dev libbz2-dev nlohmann-json-dev half libboost-all-dev python-msgpack pybind11-dev rubydev numactl libudev libudev-dev-y 2>&1 | tee -a $LOG_SUMMARY_L2 
+  	  $PKG_EXEC install sqlite3 libsqlite3-dev libbz2-dev nlohmann-json-dev half libboost-all-dev python-msgpack pybind11-dev rubydev numactl libudev libudev-dev -y 2>&1 | tee -a $LOG_SUMMARY_L2 
       if [[ $? -ne 0 ]] ; then echo "Not all packages are installed" ; exit 0 ; fi 
       gem install json
         
