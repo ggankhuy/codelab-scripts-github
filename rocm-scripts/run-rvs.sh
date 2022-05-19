25c66e355727ab9ea186baad3483012a878af8ab
Your Name <you@example.com>
2022-05-19 08:29:25 -0700

    run-rvs.sh: added

---
+++ rocm-scripts/run-rvs.sh
@@ -0,0 +1,11 @@
+ROCM_ROOT=/opt/rocm-4.5.2/
+RVS=$ROCM_ROOT/rvs/rvs
+RVS_CONFIG_ROOT=$ROCM_ROOT/rvs/conf/
+LOG_FOLDER=log
+mkdir $LOG_FOLDER -p
+
+for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
+    dmesg --clear
+    $RVS -c $RVS_CONFIG_ROOT/$i 2>&1 | sudo tee $LOG_FOLDER/rvs.gpu.$i.log
+    dmesg | sudo tee $LOG_FOLDER/rvs.gpu.$i.dmesg.log
+done
