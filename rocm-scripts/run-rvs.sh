b5b42ef1d128ae8007d9f44fa5f0ada117bb262a
Your Name <you@example.com>
2022-05-20 02:03:00 -0400

    run-rvs, rocm-techsupport.sh: improvement

--- rocm-scripts/run-rvs.sh
+++ rocm-scripts/run-rvs.sh
@@ -1,8 +1,14 @@
-ROCM_ROOT=/opt/rocm-4.5.2/
+ROCM_ROOT=/opt/rocm-5.0.0
 RVS=$ROCM_ROOT/rvs/rvs
 RVS_CONFIG_ROOT=$ROCM_ROOT/rvs/conf/
 LOG_FOLDER=log
 mkdir $LOG_FOLDER -p
+
+if [[ ! -f $RVS ]] ; then
+    echo "Unable to find rvs in $RVS."
+    ls -l $RVS
+    exit 1
+fi
 
 for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
     dmesg --clear
