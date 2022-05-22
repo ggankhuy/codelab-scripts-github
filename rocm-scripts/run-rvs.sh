537a4d914abb93229363ae1a2c3138ffbc80d7e0
Your Name <you@example.com>
2022-05-22 14:49:51 -0400

    run-rvs.sh: update

--- rocm-scripts/run-rvs.sh
+++ rocm-scripts/run-rvs.sh
@@ -1,8 +1,9 @@
 
-ROCM_ROOT=/opt/rocm-5.0.0
+DATE_FIXED=`date +%Y%m%d-%H-%M-%S`
+ROCM_ROOT=/opt/rocm-5.1.3
 RVS=$ROCM_ROOT/rvs/rvs
 RVS_CONFIG_ROOT=$ROCM_ROOT/rvs/conf/
-LOG_FOLDER=log
+LOG_FOLDER=log/rvs-log-$DATE_FIXED
 mkdir $LOG_FOLDER -p
 LOG_SUMMARY=$LOG_FOLDER/summary.log
 
@@ -13,7 +14,9 @@ if [[ ! -f $RVS ]] ; then
     exit 1
 fi
 
-for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
+#for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
+for i in gst_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
+#for i in pebb_single.conf babel.conf gpup_single.conf  ; do
     dmesg --clear
     DATE=`date +%Y%m%d-%H-%M-%S`
     echo $DATE | sudo tee -a $LOG_SUMMARY
