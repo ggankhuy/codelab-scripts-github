2e9502a39c9690b6be9db8acb63be2bb6e1e4de2
Your Name <you@example.com>
2022-05-24 11:44:30 -0400

    run-rvs.sh: improvement

--- rocm-scripts/run-rvs.sh
+++ rocm-scripts/run-rvs.sh
@@ -3,8 +3,7 @@ DATE_FIXED=`date +%Y%m%d-%H-%M-%S`
 ROCM_ROOT=/opt/rocm-5.1.3
 RVS=$ROCM_ROOT/rvs/rvs
 RVS_CONFIG_ROOT=$ROCM_ROOT/rvs/conf/
-LOG_FOLDER=log/rvs-log-$DATE_FIXED
-mkdir $LOG_FOLDER -p
+LOG_FOLDER_BASE=log/rvs-log
 LOG_SUMMARY=$LOG_FOLDER/summary.log
 
 echo "start" | sudo tee $LOG_SUMMARY
@@ -15,13 +14,14 @@ if [[ ! -f $RVS ]] ; then
 fi
 
 #for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
-for i in gst_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
-#for i in pebb_single.conf babel.conf gpup_single.conf  ; do
-    dmesg --clear
+for i in babel.conf ; do
     DATE=`date +%Y%m%d-%H-%M-%S`
+    LOG_FOLDER=$LOG_FOLDER_BASE.$i/$DATE
+    mkdir $LOG_FOLDER -p
+    dmesg --clear
     echo $DATE | sudo tee -a $LOG_SUMMARY
     echo $RVS -c $i | sudo tee -a $LOG_SUMMARY
     $RVS -c $RVS_CONFIG_ROOT/$i 2>&1 | sudo tee -a $LOG_FOLDER/rvs.gpu.$i.log
     dmesg | sudo tee $LOG_FOLDER/rvs.gpu.$i.dmesg.log
-    sleep 300
+    sleep 30
 done
