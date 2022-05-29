9c40669b3b592652616fce811d653d8a8b385dc3
Your Name <you@example.com>
2022-05-29 01:08:56 -0400

    run-kfd-1gpu-dmesg.sh: improvement

--- rocm-scripts/run-kfd-1gpu-dmesg.sh
+++ rocm-scripts/run-kfd-1gpu-dmesg.sh
@@ -1,11 +1,15 @@
 KFD=/usr/local/bin/run_kfdtest.sh
-LOG_FOLDER=log
+DATE=`date +%Y%m%d-%H-%M-%S`
+LOG_FOLDER=log-kfd-$DATE
 mkdir $LOG_FOLDER -p
 LOG_SUMMARY=$LOG_FOLDER/summary.log
+echo -ne "" > $LOG_SUMMARY
 t1=$SECONDS
 for i in {2..9} ; do
     t1a=$SECONDS
     dmesg --clear
+    echo "Running kfd for gpu $i..." | tee -a $LOG_SUMMARY
+    sleep 5
     $KFD -n $i 2>&1 | sudo tee $LOG_FOLDER/kfd.gpu.$i.log
     dmesg | sudo tee $LOG_FOLDER/kfd.gpu.$i.dmesg.log
     t2a=$SECONDS
