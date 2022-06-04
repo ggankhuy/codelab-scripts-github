798eec0c4436438bb4238a1a9c829816f0898804
Your Name <you@example.com>
2022-06-03 20:46:07 -0700

    rocm-scripts/rocm_techsupport.sh: added support for amdgpu param dumping

--- rocm-scripts/run-rvs.sh
+++ rocm-scripts/run-rvs.sh
@@ -1,12 +1,13 @@
 
 DATE_FIXED=`date +%Y%m%d-%H-%M-%S`
-ROCM_ROOT=/opt/rocm-5.2.0
+ROCM_ROOT=/opt/rocm-5.1.3
 RVS=$ROCM_ROOT/rvs/rvs
 RVS_CONFIG_ROOT=$ROCM_ROOT/rvs/conf/
 LOG_FOLDER_BASE=log/rvs/
 mkdir -p $LOG_FOLDER_BASE
 LOG_SUMMARY=$LOG_FOLDER_BASE/summary.log
 CONF_PATH=$ROCM_ROOT/share/rocm-validation-suite/conf/
+CONF_PATH=$ROCM_ROOT/rvs/conf/
 AMDXIO_PATH=/root/gg/tools-cp/amdxio/MI250/AMDXIO
 echo "start" | sudo tee $LOG_SUMMARY
 if [[ ! -f $RVS ]] ; then
@@ -18,8 +19,9 @@ fi
 echo "START..." >> $LOG_SUMMARY
 
 #for i in gst_single.conf pqt_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
-for i in pqt_single.conf ; do
-    for j in {1..2} ; do
+for i in gst_single.conf pebb_single.conf babel.conf gpup_single.conf  ; do
+#for i in pqt_single.conf ; do
+    for j in {1..10} ; do
         FILE=$i-$j
         DATE=`date +%Y%m%d-%H-%M-%S`
 
