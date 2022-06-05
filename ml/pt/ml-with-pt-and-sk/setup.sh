6540c7e1d7a0d7334b6b79053a0f08759be03e60
Your Name <you@example.com>
2022-06-05 03:06:47 +0000

    setup.sh: updates

--- ml/pt/ml-with-pt-and-sk/setup.sh
+++ ml/pt/ml-with-pt-and-sk/setup.sh
@@ -2,5 +2,5 @@ yum groupinstall "Development Tools" -y
 python3 -m pip3 install --upgrade pip
 yum update
 yum install zlib python-devel -y
-pip3 install neuralnet matplotlib pandas sklearn Pillow scipy==1.2.0
+pip3 install neuralnet matplotlib pandas sklearn Pillow scipy==1.2.0 cloudpickle pyparsing  requests idna --upgrade
 pip3 install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm4.5.2
