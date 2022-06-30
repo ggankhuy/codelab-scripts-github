cd71c78f3ae2c5712c2e05828ceed48108d7d320
root <root@localhost.localdomain>
2022-06-30 05:47:19 +0000

    setup.sh: add'l fixes

--- ml/pt/ml-with-pt-and-sk/setup.sh
+++ ml/pt/ml-with-pt-and-sk/setup.sh
@@ -1,26 +1,37 @@
 counter=0
-yum groupinstall "Development Tools" -y | tee setup.development.tools.log
-pip3 install --upgrade pip 2>&1 | tee pip.install.upgrade.log
+LOG_FOLDER=./log
+mkdir -p $LOG_FOLDER
+yum groupinstall "Development Tools" -y | tee $LOG_FOLDER/setup.development.tools.log
+pip3 install --upgrade pip 2>&1 | tee $LOG_FOLDER/pip.install.upgrade.log
 for i in \
 "python3 -m pip3 install --upgrade pip" \
 "yum update -y" \
-"yum install zlib kernel-devel python3-devel -y"
 do
-    echo "------------------------------" | tee setup.$counter.log
-    #echo "DBG: executing '$i'..." | tee -a setup.$counter.log
-    echo "------------------------------"  | tee -a setup.$counter.log
-    #$i 2>&1 | tee -a setup.$counter.log
+    echo "------------------------------" | tee $LOG_FOLDER/setup.generic.$counter.log
+    echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.generic.$counter.log
+    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.generic.$counter.log
+    $i 2>&1 | tee -a $LOG_FOLDER/setup.generic.$counter.log
     counter=$((counter+1))
 done 
+
+for i in zlib kernel-devel python3-devel
+do
+    echo "------------------------------" | tee $LOG_FOLDER/setup.yum.$counter.log
+    echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.yum.$counter.log
+    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.yum.$counter.log
+    yum install -y $i 2>&1 | tee -a setup.$counter.log
+    counter=$((counter+1))
+done 
+
 
 counter=0
 for i in neuralnet matplotlib pandas sklearn Pillow scipy==1.2.0 cloudpickle
 do
-    echo "------------------------------" | tee setup.pip3.$counter.log
-    echo "DBG: executing '$i'..." | tee -a setup.pip3.$counter.log
-    echo "------------------------------"  | tee -a setup.pip3.$counter.log
-    pip3 install $i 2>&1 | tee -a setup.pip3.$counter.log
+    echo "------------------------------" | tee $LOG_FOLDER/setup.pip3.$counter.log
+    echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.pip3.$counter.log
+    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.pip3.$counter.log
+    pip3 install $i 2>&1 | tee -a $LOG_FOLDER/setup.pip3.$counter.log
     counter=$((counter+1))
 done
 
-pip3 install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm4.5.2
+pip3 install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm4.5.2 2>&1  | tee $LOG_FOLDER/torch.torchvision.log
