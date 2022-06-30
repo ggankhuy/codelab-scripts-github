4416f00e017631af8ba4928b26a03879f6d20bdd
root <root@localhost.localdomain>
2022-06-30 05:58:13 +0000

    setup.sh: further improvement, syntax error fixes

--- ml/pt/ml-with-pt-and-sk/setup.sh
+++ ml/pt/ml-with-pt-and-sk/setup.sh
@@ -1,24 +1,23 @@
 counter=0
 LOG_FOLDER=./log
 mkdir -p $LOG_FOLDER
+SINGLE_BAR='----'
 yum groupinstall "Development Tools" -y | tee $LOG_FOLDER/setup.development.tools.log
 pip3 install --upgrade pip 2>&1 | tee $LOG_FOLDER/pip.install.upgrade.log
-for i in \
-"python3 -m pip3 install --upgrade pip" \
-"yum update -y" \
+for i in "python3 -m pip3 install --upgrade pip" "yum update -y"
 do
-    echo "------------------------------" | tee $LOG_FOLDER/setup.generic.$counter.log
+    echo $SINGLE_BAR | tee $LOG_FOLDER/setup.generic.$counter.log
     echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.generic.$counter.log
-    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.generic.$counter.log
+    echo $SINGLE_BAR  | tee -a $LOG_FOLDER/setup.generic.$counter.log
     $i 2>&1 | tee -a $LOG_FOLDER/setup.generic.$counter.log
     counter=$((counter+1))
 done 
 
 for i in zlib kernel-devel python3-devel
 do
-    echo "------------------------------" | tee $LOG_FOLDER/setup.yum.$counter.log
+    echo $SINGLE_BAR | tee $LOG_FOLDER/setup.yum.$counter.log
     echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.yum.$counter.log
-    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.yum.$counter.log
+    echo $SINGLE_BAR  | tee -a $LOG_FOLDER/setup.yum.$counter.log
     yum install -y $i 2>&1 | tee -a setup.$counter.log
     counter=$((counter+1))
 done 
@@ -27,9 +26,9 @@ done
 counter=0
 for i in neuralnet matplotlib pandas sklearn Pillow scipy==1.2.0 cloudpickle
 do
-    echo "------------------------------" | tee $LOG_FOLDER/setup.pip3.$counter.log
+    echo $SINGLE_BAR | tee $LOG_FOLDER/setup.pip3.$counter.log
     echo "DBG: executing '$i'..." | tee -a $LOG_FOLDER/setup.pip3.$counter.log
-    echo "------------------------------"  | tee -a $LOG_FOLDER/setup.pip3.$counter.log
+    echo $SINGLE_BAR  | tee -a $LOG_FOLDER/setup.pip3.$counter.log
     pip3 install $i 2>&1 | tee -a $LOG_FOLDER/setup.pip3.$counter.log
     counter=$((counter+1))
 done
