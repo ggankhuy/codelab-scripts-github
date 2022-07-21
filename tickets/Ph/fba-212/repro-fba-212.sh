63518cabffab361619e4a54bec795734dbe99e9a
Your Name <you@example.com>
2022-07-21 01:02:14 -0700

    example.cfg: added

--- tickets/Ph/fba-212/repro-fba-212.sh
+++ tickets/Ph/fba-212/repro-fba-212.sh
@@ -27,23 +27,28 @@ function wait_host_up() {
     exit 1
 }
 
+#                "sudo reboot" \
+#                "sudo dmesg | sudo tee fba-212/dmesg.after.reboot.'$loop'.log" \
+#                "/usr/local/bin/run_kfdtest.sh 2>&1 | sudo tee fba-212/$loop.kfdtest.log " \
+
 wait_host_up
-for loop in {1..10} ; do
+for loop in {1..5} ; do
     echo $DOUBLE_BAR
     echo "Current loop: $loop........"
 
     for cmd in  "sudo mkdir fba-212" \
-                "sudo dmesg | sudo tee fba-212/dmesg.after.reboot.'$loop'.log" \
                 "sudo dmesg --clear"  "sudo modprobe amdgpu" \
                 "sudo dmesg | sudo tee fba-212/dmesg.after.modprobe.amdgpu.'$loop'.log" \
-                "/usr/local/bin/run_kfdtest.sh | sudo tee fba-212/$loop.kfdtest.log " \
+                "sudo dmesg --clear" \
+                "/usr/local/bin/kfdtest --gtest_filter=-*LargestSysBuffero* 2>&1 | sudo tee fba-212/$loop.kfdtest.log " \
+                "sudo dmesg | sudo tee fba-212/dmesg.after.workload.'$loop'.log" \
                 "sudo dmesg --clear" "sudo rmmod amdgpu" "sudo dmesg | sudo tee fba-212/dmesg.after.rmmod.'$loop'.log" \
-                "sudo reboot" \
-        ;do
+        ; do
         echo "$SINGLE_BAR"
         echo --- $cmd ---
         sshpass -p $PW ssh -p $CONFIG_PORT_GUEST -o StrictHostKeyChecking=no $USER@$CONFIG_IP_GUEST $cmd
         ret=$?
+        sleep 3
         
         #if [[ $ret != 0 && $ret != 1 ]] ; then 
         #    echo "code: $ret. Error executing $cmd on remote $CONFIG_IP_GUEST with credentials: $USER/$PW"
