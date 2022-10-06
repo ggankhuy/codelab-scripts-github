b8861f0da174015d9e2be855b747cfd0d13b9405
Your Name <you@example.com>
2022-10-06 01:10:40 +0000

    sh/common.sh: install_python function added

--- build-scripts/rocm/helper/sh/common.sh
+++ build-scripts/rocm/helper/sh/common.sh
@@ -6,6 +6,56 @@ CONFIG_BYPASS_LLVM=0
 CONFIG_DISABLE_rocSOLVER=1
 CONFIG_DISABLE_hipBLAS=1
 
+function install_python() {
+    #Python defs.
+
+    CURR_VER=`python3 --version  | cut -d ' ' -f2`
+    PYTHON_VER_MAJOR=3.9
+    PYTHON_VER_MINOR=10
+
+    PYTHON_VER=$PYTHON_VER_MAJOR.$PYTHON_VER_MINOR
+    PYTHON_FULL_NAME=Python-$PYTHON_VER
+    PYTHON_TAR=$PYTHON_FULL_NAME.tgz
+
+    if [[ $CURR_VER == $PYTHON_VER ]] ; then
+        echo "Current installed version is same as the one being installed..., exiting"
+        return 0
+    else
+        echo "Installing..."
+    fi
+
+    sudo yum -y install epel-release
+    sudo yum update -y
+    sudo yum groupinstall "Development Tools" -y
+
+    sudo yum install openssl-devel libffi-devel bzip2-devel -y
+    wget -nc https://www.python.org/ftp/python/$PYTHON_VER/$PYTHON_TAR
+    tar -xvf $PYTHON_TAR
+    cd $PYTHON_FULL_NAME
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Can not cd into $PYTHON_VER directory..."
+        exit 1
+    fi
+    ./configure --enable-optimizations
+    sudo make -j`nproc` install
+
+    echo "Testing the installation..."
+    python$PYTHON_VER_MAJOR --version
+    if [[ $? -ne 0 ]] ; then
+        echo "Unable to find 3.9"
+    fi
+    PATH_PYTHON_U=`which python$PYTHON_VER_MAJOR`
+    echo "new path: $PATH_PYTHON_U"
+    rm -rf /usr/bin/python
+    echo ln -s $PATH_PYTHON_U /usr/bin/python
+    ln -s $PATH_PYTHON_U /usr/bin/python
+
+    rm -rf /usr/bin/python3
+    ln -s /usr/bin/python /usr/bin/python3
+    cd ..
+}
+
 function build_entry () {
     t2=$SECONDS
     if  [[ ! -z $t1 ]] ; then
@@ -96,3 +146,4 @@ case "$OS_NAME" in
      ;;
 esac 
 
+install_python
