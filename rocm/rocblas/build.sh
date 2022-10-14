c361ea0a151fcec3c607f90610e0ed1bd2e74c48
Your Name <you@example.com>
2022-10-13 22:23:32 -0700

    example_sgemm.cpp: bug fixed, compile run ok

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,17 +1,18 @@
 #/opt/rocm-4.5.2/hip/include/hip/
+ROCM_VERSION=5.2.0
 #-I$(ROCM_PATH)/hsa/include
 rm -rf build/*
 mkdir build
 cd build
 FILENAME=sscal
 FILENAME=example_sgemm
-FILENAME=gemm
+#FILENAME=gemm
 ln -s ../$FILENAME.cpp
 for i in helpers.hpp ArgParser.hpp ; do 
     ln -s ../$i .
 done
 
 hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
-hipcc $FILENAME.o  /opt/rocm-4.5.2/lib/librocblas.so
+hipcc $FILENAME.o  /opt/rocm-$ROCM_VERSION/lib/librocblas.so
 cd ..
 
