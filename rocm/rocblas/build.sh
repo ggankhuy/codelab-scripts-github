75f9b2485c721738e6475ddac77a8d43bbdc2c0d
Your Name <you@example.com>
2022-10-14 20:36:57 -0700

    build.sh, run.sh: improvement

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,18 +1,15 @@
-#/opt/rocm-4.5.2/hip/include/hip/
 ROCM_VERSION=5.2.0
-#-I$(ROCM_PATH)/hsa/include
 rm -rf build/*
 mkdir build
 cd build
-FILENAME=sscal
-FILENAME=example_sgemm
-#FILENAME=gemm
-ln -s ../$FILENAME.cpp
-for i in helpers.hpp ArgParser.hpp ; do 
-    ln -s ../$i .
+for FILENAME in sscal example_sgemm; do 
+    echo building $FILENAME...
+    ln -s ../$FILENAME.cpp
+    for i in helpers.hpp ArgParser.hpp ; do 
+        ln -s ../$FILENAME .
+    done
+
+    hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
+    hipcc $FILENAME.o  /opt/rocm-$ROCM_VERSION/lib/librocblas.so -o $FILENAME.out
 done
-
-hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
-hipcc $FILENAME.o  /opt/rocm-$ROCM_VERSION/lib/librocblas.so
 cd ..
-
--- rocm/rocblas/run.sh
+++ rocm/rocblas/run.sh
@@ -1,2 +1,6 @@
-ROCBLAS_LAYER=6 ./build/a.out 2>&1 | tee ROCBLAS_LAYER=6.a.log
-TENSILE_DB=0x08000 ROCBLAS_LAYER=6 ./build/a.out 2>&1 | tee TENSILE_DB.ROCBLAS_LAYER=6.a.log
+mkdir log -p
+for FILENAME in sscal example_sgemm ; do
+    echo "------- running $FILENAME .... ---------"
+    ROCBLAS_LAYER=6 ./build/$FILENAME.out 2>&1 | tee log/ROCBLAS_LAYER6.$FILENAME.log
+    TENSILE_DB=0x08000 ROCBLAS_LAYER=6 ./build/$FILENAME.out 2>&1 | tee log/TENSILE_DB.ROCBLAS_LAYER6.$FILENAME.log
+done
