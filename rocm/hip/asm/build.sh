e69f7986d0be7a322c192e373662a4a64fef123f
Your Name <you@example.com>
2022-10-17 22:43:13 +0000

    mm.cpp: compiling and running ok

--- rocm/hip/asm/build.sh
+++ rocm/hip/asm/build.sh
@@ -1,6 +1,8 @@
 FILENAME=mm
+#FILENAME=p41
 mkdir -p build/$FILENAME
 cd build/$FILENAME
+rm -rf ./*
 ln -s ../../$FILENAME.cpp .
-hipcc --save-temps $FILENAME.cpp -o $FILENAME.out
+hipcc --save-temps $FILENAME.cpp -o $FILENAME.out -g -O0
 cd ../..
--- rocm/hip/asm/mm.cpp
+++ rocm/hip/asm/mm.cpp
@@ -2,14 +2,10 @@
 
 #include <stdio.h>
 #include "hip/hip_runtime.h"
-#include <vector>
-#include <iostream> 
-#include <stdlib.h>
-#include <time.h>
 
-#define N 16
-#define LOOPSTRIDE 1
-
+#define N 64
+#define ARRSIZE 3
+#define LOOPSTRIDE 8
 __global__ void add(int *a, int*b, int *c) {
 	//int tid = hipBlockIdx_x;
 	//c[tid] = a[tid] + b[tid];
@@ -28,56 +24,50 @@ __global__ void add(int *a, int*b, int *c) {
 }
 
 int main (void) {
-    int *h_A, *h_B, *h_C;
+    int *a, *b, *c;
     int *dev_a, *dev_b, *dev_c;
     int i ;
+    
 
-    // initialize host array. 
+    a = (int*)malloc(N * sizeof(int));
+    b = (int*)malloc(N * sizeof(int));
+    c = (int*)malloc(N * sizeof(int));
+ 	hipMalloc(&dev_a, N * sizeof(int) );
+ 	hipMalloc(&dev_b, N * sizeof(int) );
+ 	hipMalloc(&dev_c, N * sizeof(int) );
 
-    h_A = (int*)malloc(N * sizeof(int));
-    h_B = (int*)malloc(N * sizeof(int));
-    h_C = (int*)malloc(N * sizeof(int));
+	for (int i = 0; i < N ; i ++ ) {
+		a[i]  = i;
+		b[i] = i + i;
+		c[i] = 999;
+	}
 
-    /*vector<float>h_A(SIZE);
-    vector<float>h_B(SIZE);
-    vector<float>h_C(SIZE);
-    */
+	for (int i = 0; i < N ; i+=LOOPSTRIDE ) {
+        printf("Before add: a/b: %d, %d.\n", a[i], b[i]);
+	}
 
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
-            h_A[i*N+j] = sin(i);
-            h_B[i*N+j] = cos(j);
-        }
-    }
-
-    // initialize device array.
-
-    hipMalloc(&dev_a, N * sizeof(int) );
-    hipMalloc(&dev_b, N * sizeof(int) );
-    hipMalloc(&dev_c, N * sizeof(int) );
-
-   	hipMemcpy(dev_a, h_A, N * sizeof(int), hipMemcpyHostToDevice);
-   	hipMemcpy(dev_b, h_B, N * sizeof(int), hipMemcpyHostToDevice);
-   	hipMemcpy(dev_c, h_C, N * sizeof(int), hipMemcpyHostToDevice);
+   	hipMemcpy(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice);
+   	hipMemcpy(dev_b, b, N * sizeof(int), hipMemcpyHostToDevice);
+   	hipMemcpy(dev_c, c, N * sizeof(int), hipMemcpyHostToDevice);
     
     const unsigned blocks = 256;
     const unsigned threadsPerBlock = 1;
 
     hipLaunchKernelGGL(add, blocks, threadsPerBlock, 0, 0, dev_a, dev_b, dev_c);
 
-    hipMemcpy(h_A, dev_a, N * sizeof(int), hipMemcpyDeviceToHost);
-    hipMemcpy(h_B, dev_b, N * sizeof(int), hipMemcpyDeviceToHost);
-    hipMemcpy(h_C, dev_c, N * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(a, dev_a, N * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(b, dev_b, N * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(c, dev_c, N * sizeof(int), hipMemcpyDeviceToHost);
 
 	for (int i = 0; i < N; i+=LOOPSTRIDE )
-		printf("After add: %d: %u + %u = %u\n", i, h_A[i], h_B[i], h_C[i]);
+		printf("After add: %d: %u + %u = %u\n", i, a[i], b[i], c[i]);
 
     hipFree(dev_a);
     hipFree(dev_b);
     hipFree(dev_c);
-    free(h_A);
-    free(h_B);
-    free(h_C);
+    free(a);
+    free(b);
+    free(c);
     
 	return 0;
 }
