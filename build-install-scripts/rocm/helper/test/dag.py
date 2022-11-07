9c5e74d31a72ba3ba98349c4cf40004e3710b4bd
Your Name <you@example.com>
2022-11-07 20:23:47 +0000

    dag.py: graph support

--- build-install-scripts/rocm/helper/test/dag.py
+++ build-install-scripts/rocm/helper/test/dag.py
@@ -1,6 +1,13 @@
+from matplotlib import pyplot as plt
 import networkx as nx
 graph = nx.DiGraph()
 graph.add_edges_from([("root", "a"), ("a", "b"), ("a", "e"), ("b", "c"), ("b", "d"), ("d", "e")])
 print(nx.shortest_path(graph, 'root', 'e'))
 #print(nx.dag_longest_path(graph, 'root', 'e'))
 print(list(nx.topological_sort(graph)))
+
+plt.tight_layout()
+nx.draw_networkx(graph, arrows=True)
+plt.savefig("graph.png", format="PNG")
+# tell matplotlib you're done with the plot: https://stackoverflow.com/questions/741877/how-do-i-tell-matplotlib-that-i-am-done-with-a-plot
+plt.clf()
