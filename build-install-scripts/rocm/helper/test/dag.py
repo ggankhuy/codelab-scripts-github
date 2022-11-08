96a536a513237fbd04101c2e383f50dfe6f2adc4
Your Name <you@example.com>
2022-11-08 23:05:05 +0000

    def recur_pred() implemented but order still in question in all_pred

--- build-install-scripts/rocm/helper/test/dag.py
+++ build-install-scripts/rocm/helper/test/dag.py
@@ -6,8 +6,38 @@ print(nx.shortest_path(graph, 'root', 'e'))
 #print(nx.dag_longest_path(graph, 'root', 'e'))
 print(list(nx.topological_sort(graph)))
 
+'''
 plt.tight_layout()
 nx.draw_networkx(graph, arrows=True)
 plt.savefig("graph.png", format="PNG")
 # tell matplotlib you're done with the plot: https://stackoverflow.com/questions/741877/how-do-i-tell-matplotlib-that-i-am-done-with-a-plot
 plt.clf()
+'''
+all_pred=[]
+indent=""
+def recur_pred(lNode, indent):
+    print(indent, "recur_pred: ", lNode)
+    preds=list(graph.predecessors(lNode))
+    print(indent, "predecessors returned for ", lNode, ": ", preds)
+    indent+="  "
+    for i in preds:
+        if not i in all_pred:
+            print("adding ", i, " to all_pred")
+            all_pred.append(i)
+        else:
+            print(i, " is already in all_pred list, bypassing.")
+        recur_pred(i, indent)
+
+print("pred: e:")
+print(list(graph.predecessors('e')))
+print("subgraph: e:" )
+print(list(graph.subgraph('e')))
+print("succ: a:")
+print(list(graph.successors('a')))
+print("neigh: a:")
+print(list(graph.neighbors('e')))
+
+print("Gathering all predecessors of e:")
+recur_pred('e', indent)
+
+print("all predecesors: ", all_pred)
