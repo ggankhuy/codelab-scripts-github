007d52088ff5c2da0a199832ea69a09a7540984c
Your Name <you@example.com>
2022-12-05 18:40:25 +0000

    build.sh: added run support, log gathered in log folder

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,4 +1,6 @@
 set -x
+rm -rf log/*
+mkdir log/
 rm -rf build/*
 mkdir build
 cd build
@@ -11,6 +13,8 @@ for i in sscal example_sgemm example_sgemm_strided_batched; do
     ln -s ../$i.cpp
     hipcc --offload-arch=gfx908 --save-temps -c $i.cpp
     hipcc $i.o  /opt/rocm-5.3.0/lib/librocblas.so -o $i.out
+    ROCBLAS_LAYER=5 TENSILE_DB=8000 ./$i.out 2>&1 | tee ../log/ROCBLAS_LAYER.5.TENSILE_DB.8000.$i.log
+    ROCBLAS_LAYER=4 TENSILE_DB=8000 ./$i.out 2>&1 | tee ../log/ROCBLAS_LAYER.4.TENSILE_DB.8000.$i.log
 done
 
 chmod 755 *.out
