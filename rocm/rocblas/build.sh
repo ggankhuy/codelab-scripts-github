ac3c0af6dfb32169f27171d5b397e5c8488e2af7
Your Name <you@example.com>
2022-12-05 18:18:35 +0000

    rocblas: fixed build.sh script

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,17 +1,17 @@
-#/opt/rocm-4.5.2/hip/include/hip/
-#-I$(ROCM_PATH)/hsa/include
+set -x
 rm -rf build/*
 mkdir build
 cd build
-FILENAME=sscal
-FILENAME=example_sgemm
-FILENAME=gemm
-ln -s ../$FILENAME.cpp
-for i in helpers.hpp ArgParser.hpp ; do 
-    ln -s ../$i .
+
+for j in helpers.hpp ArgParser.hpp ; do 
+    ln -s ../$j .
 done
 
-hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
-hipcc $FILENAME.o  /opt/rocm-4.5.2/lib/librocblas.so
-cd ..
+for i in sscal example_sgemm ; do
+    ln -s ../$i.cpp
+    hipcc --offload-arch=gfx908 --save-temps -c $i.cpp
+    hipcc $i.o  /opt/rocm-5.3.0/lib/librocblas.so -o $i.out
+done
 
+chmod 755 *.out
+cd ..
