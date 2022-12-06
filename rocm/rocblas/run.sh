c512944218a1094dd8a984fbaabf8274e276b5ff
Your Name <you@example.com>
2022-12-06 05:23:37 +0000

    run.sh: improvement

--- rocm/rocblas/run.sh
+++ rocm/rocblas/run.sh
@@ -1,21 +1,17 @@
 set -x
-rm -rf log/*
-mkdir log/
-rm -rf build/*
-mkdir build
+DATE=`date +%Y%m%d-%H-%M-%S`
 cd build
-
-for j in helpers.hpp ArgParser.hpp ; do 
-    ln -s ../$j .
-done
+rm -rf ../log/*
+mkdir -p ../log/$DATE/
+LOG_FOLDER=../log/$DATE/
+ls -l ../log/
+sleep 3
 
 for i in sscal example_sgemm example_sgemm_strided_batched; do
-    ln -s ../$i.cpp
-    hipcc --offload-arch=gfx908 --save-temps -c $i.cpp
-    hipcc $i.o  /opt/rocm-5.3.0/lib/librocblas.so -o $i.out
-    ROCBLAS_LAYER=5 TENSILE_DB=8000 ./$i.out 2>&1 | tee ../log/ROCBLAS_LAYER.5.TENSILE_DB.8000.$i.log
-    ROCBLAS_LAYER=4 TENSILE_DB=8000 ./$i.out 2>&1 | tee ../log/ROCBLAS_LAYER.4.TENSILE_DB.8000.$i.log
+    echo "======================" | tee $LOG_FOLDER/ROCBLAS_LAYER.4.TENSILE_DB.8000.$i.log
+    ROCBLAS_LAYER=5 TENSILE_DB=8000 ./$i.out 2>&1 | tee -a $LOG_FOLDER/ROCBLAS_LAYER.5.TENSILE_DB.8000.$i.log
+    echo "----------------------" | tee -a $LOG_FOLDER/ROCBLAS_LAYER.4.TENSILE_DB.8000.$i.log
+    ROCBLAS_LAYER=4 TENSILE_DB=8000 ./$i.out 2>&1 | tee -a $LOG_FOLDER/ROCBLAS_LAYER.4.TENSILE_DB.8000.$i.log
 done
 
-chmod 755 *.out
 cd ..
