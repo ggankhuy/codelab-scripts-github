5fb885d18c563f2afb481be9951e8902639d6bbb
Your Name <you@example.com>
2022-12-20 06:25:55 +0000

    hipMalloc.cpp, run.sh: enhancement: timer using chrono, conditional variable to preprocess to en/dis timer

--- rocm/hip/debug-log/hipMalloc.cpp
+++ rocm/hip/debug-log/hipMalloc.cpp
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 #include "hip/hip_runtime.h"
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 #define N 64
 #define ARRSIZE 3
@@ -12,8 +17,33 @@ int main (void) {
     int *dev_a, *dev_b, *dev_c;
     int i ;
     
+    #if timer == 1
+    auto start = std::chrono::high_resolution_clock::now();
+    #endif
 
  	hipMalloc(&dev_a, N * sizeof(int) );
+
+    #if timer == 1
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = (end - start);
+    auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+    int ns_fractional = static_cast<int>(ns.count());
+    cout << setw(30) << "hipMalloc duration: " << ns_fractional << " ns or " << ns_fractional / 1000000 << " ms" << endl;
+    #endif
+
+    #if timer == 1
+    start = std::chrono::high_resolution_clock::now();
+    #endif
+
     hipFree(dev_a);
+
+    #if timer == 1
+    end = std::chrono::high_resolution_clock::now();
+    duration = (end - start);
+    ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+    ns_fractional = static_cast<int>(ns.count());
+    cout << setw(30) << "hipFree duration: " << ns_fractional << " ns or " << ns_fractional / 1000000 << " ms " << endl;
+    #endif
+
 	return 0;
 }
--- rocm/hip/debug-log/run.sh
+++ rocm/hip/debug-log/run.sh
@@ -2,6 +2,8 @@ FILENAME=hipMalloc
 LOG_DIR=log/$FILENAME
 echo Making directory $LOG_DIR
 mkdir $LOG_DIR -p 
-hipcc $FILENAME.cpp && AMD_LOG_LEVEL=4  ./a.out 2>&1 | tee $LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
-rocprof --sys-trace -d ./$LOG_DIR/ ./a.out
+hipcc -D timer=0 $FILENAME.cpp  -o $FILENAME.out && AMD_LOG_LEVEL=4  ./$FILENAME.out 2>&1 | tee $LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
+rocprof --sys-trace -d ./$LOG_DIR/ ./$FILENAME.out
+
+hipcc -D timer=1 $FILENAME.cpp  -o $FILENAME-t.out && ./$FILENAME-t.out
 mv results* ./$LOG_DIR/
