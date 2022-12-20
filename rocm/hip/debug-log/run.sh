c4fd1757b11f48cb3bd15ffa35febfcacff781d4
Your Name <you@example.com>
2022-12-20 05:45:47 +0000

    run.sh: enhanced

--- rocm/hip/debug-log/run.sh
+++ rocm/hip/debug-log/run.sh
@@ -1,7 +1,7 @@
-FILENAME=file2.cpp
-FILENAME=file2a.cpp
-FILENAME=file2b.cpp
-
-FILENAME=file2.cpp
-mkdir log
-hipcc $FILENAME && AMD_LOG_LEVEL=4  ./a.out 2>&1 | tee log/$FILENAME.AMD_LOG_LEVEL.4.log
+FILENAME=hipMalloc
+LOG_DIR=log/$FILENAME
+echo Making directory $LOG_DIR
+mkdir $LOG_DIR -p 
+hipcc $FILENAME.cpp && AMD_LOG_LEVEL=4  ./a.out 2>&1 | tee $LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
+rocprof --sys-trace -d ./$LOG_DIR/ ./a.out
+mv results* ./$LOG_DIR/
