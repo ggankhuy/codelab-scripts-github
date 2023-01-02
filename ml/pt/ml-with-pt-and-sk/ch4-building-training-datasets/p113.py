90d308081feb4fd584f0fe59e537a87d280c93cc
Your Name <you@example.com>
2023-01-02 01:13:54 -0800

    p113.py: cleaned up redundant code

--- ml/pt/ml-with-pt-and-sk/ch4-building-training-datasets/p113.py
+++ ml/pt/ml-with-pt-and-sk/ch4-building-training-datasets/p113.py
@@ -11,9 +11,9 @@ df.columns=['color','size', 'price', 'classlabel']
 print(df.columns)
 print(df)
 
-# convert size mapping to numerical ordinal.
+# convert size column numerical ordinal.
 
-print("Ordinal assignment...")
+print("Encoding size column...")
 size_mapping={'XL':3, 'L':2, 'M':1}
 df['size'] = df['size'].map(size_mapping)
 print(df.columns)
@@ -27,30 +27,11 @@ print(X)
 
 # Encode class label.
 
-print("Encodingclass labels...")
+print("Encoding classlabels...")
 class_mapping = {label: idx for idx, label in enumerate(np.unique(df['classlabel']))}
 df['classlabel']=df['classlabel'].map(class_mapping)
 print(class_mapping)
 print(df)
-
-#encoding class labels.
-'''
-
-print("Reversing...")
-inv_class_mapping = {v: k for k, v in class_mapping.items()}
-df['classlabel']=df['classlabel'].map(inv_class_mapping)
-print(df)
-
-print("Using LabelEncoder from sklearn...")
-
-from sklearn.preprocessing import LabelEncoder
-class_le=LabelEncoder()
-y=class_le.fit_transform(df['classlabel'].values)
-print(y)
-'''
-#print("Performing one-hot encoding now.")
-#color_ohe=OneHotEncoder()
-#print(color_ohe.fit_transform(X[:,0].reshape(-1,1)).toarray())
 
 print("Performing selectively transforming multi-feature array...")
 
@@ -61,25 +42,3 @@ c_transf=ColumnTransformer([\
 ('nothing', 'passthrough', [1,2]) \
 ])
 print(c_transf.fit_transform(X).astype(float))
-'''
----
-#mapping ordinal features.
-
-import pandas as pd
-df=pd.DataFrame([['green','M',10.1,'class2'],['red','L',13.5, 'class1'],['blue','XL',15.3,'class2']])
-df.columns=['color','size', 'price', 'classlabel']
-print(df.columns)
-print(df)
-
-#ordinal:
-print("Ordinal assignment...")
-size_mapping={'XL':3, 'L':2, 'M':1}
-df['size'] = df['size'].map(size_mapping)
-print(df.columns)
-print(df)
-
-#inverting back  the integer values of size to original string representation
-
-inv_size_mapping={v: k for k, v in size_mapping.items()}
-print(df['size'].map(inv_size_mapping))
-'''
