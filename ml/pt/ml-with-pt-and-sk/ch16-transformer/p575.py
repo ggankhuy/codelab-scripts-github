66b19f97492ce9fecf319bda8cd48d4165fc89b7
Your Name <you@example.com>
2023-01-05 11:21:37 -0800

    p575.py: code complete, running...

--- ml/pt/ml-with-pt-and-sk/ch16-transformer/p575.py
+++ ml/pt/ml-with-pt-and-sk/ch16-transformer/p575.py
@@ -40,3 +40,102 @@ with gzip.open('movie_data.csv.gz','rb') as f_in:
 
 df=pd.read_csv('movie_data.csv')
 print(df.head(3))
+
+train_texts=df.iloc[:35000]['review'].values
+train_labels=df.iloc[:35000]['sentiment'].values
+valid_texts=df.iloc[35000:40000]['review'].values
+valid_labels=df.iloc[35000:40000]['sentiment'].values
+test_texts=df.iloc[40000:]['review'].values
+test_labels=df.iloc[40000:]['sentiment'].values
+
+tokenizer=DistilBertTokenierFast.from_pretrained('distilbert-base-uncased')
+train_encodings=tokenizer(list(train_texts), truncation=True, padding=True)
+valid_encodings=tokenizer(list(train_texts), truncation=True, padding=True)
+test_encodings=tokenizer(list(train_texts), truncation=True, padding=True)
+
+class IMDBDataset(torch.util.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings=encodings
+        self.labels=labels
+
+    def __getitem__(self, idx):
+        item={key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels']=torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset=IMDBdataset(train_encodings, train_labels)
+valid_dataset=IMDBdataset(train_encodings, valid_labels)
+test_dataset=IMDBdataset(train_encodings, test_labels)
+
+train_loader=torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+valid_loader=torch.utils.data.DataLoader(valid_dataset, batch_size=16, shuffle=True)
+test_loader=torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True)
+
+model=DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncase')
+model.to(DEVICE)
+model.train()
+
+def compute_accuracy(model, data_laoder, device):
+    with torch.no_grad():
+        correct_pred, num_examples=0,0
+        for batch_idx, batch in enumerate(data_loader):
+
+        # Prepare data.
+
+            input_ds = batch['input_ids'].to(device)
+            attention_mask=batch['attention_mask'].to(device)
+            labels=batch['labels'].to(device)
+
+            outputs=model(input_ids, attention_mask=attention_mask)
+            logits=output['logits']
+            predicted_labels=torch.argmax(logits,1)
+            num_examples+= labels.size(0)
+            correct_pred+=(predicted_labels==labels).sum()
+    return correct_pred.float()/num_examples * 100
+
+start_time=time.time()
+
+for epoch in range(NUM_EPOCHS):
+    model.train()
+    for batch_idx, batch in enumerate(train_loader):
+
+        ## prep data.
+
+        input_ids=batch['input_ids'].to(DEVICE)
+        attention_mask = batch['attention_mask'].to(DEVICE)
+        labels=batch['labels'].to(DEVICE)
+
+        ## fwd pass.
+
+        outputs=model(inputs_ids, attention_mask=attention_mask, labels=labels)
+        loss, logits=output['loss'], outputs['logits']
+
+        # backward pass.
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        ## logging.
+    
+        if not batch_idx % 250:
+            print(f'Epoch: {epoch+1:04d}/{NUM_EPOCHS:04d} | Batch {batch_idx:04d} | Loss: {loss:.4f}')
+
+    model.eval()
+
+    with torch.set_grad_enabled(False):
+        print(f'Training accuracy: {compute_accuracy(model, train_loader, DEVICE):.2f}%\nValid accuracy: {compute_accuracy(model, valid_loader, DEVICE):.2f}%')
+
+        print(f'Time elapsed: {(time.time() - start_time) / 60:.2f} min')
+
+    
+    print(f'Total training time: {(time.time() - start_time)/60:.2f} min')
+    print(f'test accuracy: {compute_accuracy(model, test_loader, DEVICE):.2f}%')    
+
+
+
+            
+    
