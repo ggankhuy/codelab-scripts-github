4398eb4c72b9ebc25b842662bc59ff41f529a2e5
Your Name <you@example.com>
2023-01-12 09:54:55 -0800

    5-stream-event.cpp: code complete, runs ok, trace not ok

--- rocm/hip/stream-exercises/5-stream-event.cpp
+++ rocm/hip/stream-exercises/5-stream-event.cpp
@@ -4,10 +4,8 @@
 #include <cstdlib>
 #include "hip/hip_runtime.h"
 
-#define N 64
-#define ARRSIZE 3
-#define LOOPSTRIDE 8
 #define STREAMS 2
+
 __global__ void k1() {
     size_t start = clock64();
     size_t elapsed = 0;
@@ -23,52 +21,30 @@ __global__ void k2() {
         elapsed = clock64() - start;
     }
 }
-__global__ void k3() {
-    size_t start = clock64();
-    size_t elapsed = 0;
-    while (elapsed < 100000000) {
-        elapsed = clock64() - start;
-    }
-}
-__global__ void k4() {
-    size_t start = clock64();
-    size_t elapsed = 0;
-    while (elapsed < 100000000) {
-        elapsed = clock64() - start;
-    }
-}
 
 int main (void) {
     int *a, *b, *c;
     int *dev_a, *dev_b, *dev_c;
     int i ;
 
-    // create streams. 
-    hipStream_t streams[STREAMS];
-
-    for (int i = 0; i < STREAMS; i ++) {
-        int ret = hipStreamCreate(&streams[i]);
-        if (ret != 0) {
-            printf("Failure creating stream %u.\n", i);
-            return 1;
-        } else {
-            printf("Stream %u create ok.\n", i);
-        }
-
-    }
-
+    hipStream_t s1, s2;
+    hipStreamCreate(&s1);
+    hipStreamCreate(&s2);
+    
     // create event. s1 records event. 
 
+    printf("Creating event...\n");
     hipEvent_t e1;
     hipEventCreate(&e1); 
-    hipEventRecord(e1, streams[0]);
+    hipEventRecord(e1, s1);
 
     const unsigned blocks = 256;
     const unsigned threadsPerBlock = 1;
 
-    k1<<<256,1,0,streams[0]>>>();
+    printf("Launch streams...\n");
+    k1<<<256,1,0,s1>>>();
     hipEventSynchronize(e1);
-    k2<<<256,1,0,streams[1]>>>();
+    k2<<<256,1,0,s2>>>();
 
 	return 0;
 }
--- rocm/hip/stream-exercises/run.sh
+++ rocm/hip/stream-exercises/run.sh
@@ -1,10 +1,10 @@
-#for FILENAME in 2-gpu-serial 2-stream-parallel 2-stream-parallel-barrier-and 2+1-stream-parallel-barrier-and; do
-for FILENAME in 2+1-stream-parallel-barrier-and; do
+#for FILENAME in 1-2-gpu-serial 2-2-stream-parallel 3-2-stream-parallel-barrier-and 4-2+1-stream-parallel-barrier-and; do
+for FILENAME in 5-stream-event; do
     
     LOG_FOLDER=log/$FILENAME/
     mkdir $LOG_FOLDER -p
     hipcc $FILENAME.cpp -o $FILENAME.out && AMD_LOG_LEVEL=4 ./$FILENAME.out 2>&1 | tee $LOG_FOLDER/$FILENAME.AMD_LOG_LEVEL.4.log
-    rocprof --sys-trace $FILENAME.out
+    if [[ $? -eq 0 ]] ; then rocprof --sys-trace $FILENAME.out else echo "compile with hipcc failed." ; exit 1 ; fi
     #rocprof --hip-trace $FILENAME.out
-    mv results.* *.csv $LOG_FOLDER
+    if [[ $? -eq 0 ]] ; then mv results.* *.csv $LOG_FOLDER else echo "trace failed." ; exit 1 ; fi
 done
