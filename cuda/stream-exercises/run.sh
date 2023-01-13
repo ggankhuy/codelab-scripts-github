bc2a4249aeb02d0a517deeadd2c86c555ec2135e
Your Name <you@example.com>
2023-01-13 00:32:32 -0800

    2-2-stream-parallel.cu

--- cuda/stream-exercises/run.sh
+++ cuda/stream-exercises/run.sh
@@ -1,6 +1,7 @@
 #for FILENAME in 1-2-gpu-serial 2-2-stream-parallel 3-2-stream-parallel-barrier-and 4-2+1-stream-parallel-barrier-and 5-stream-event 6-2-gpu-2-stream-synchronize; do
 #for FILENAME in 6-2-gpu-2-stream-synchronize ; do
-for FILENAME in 1-2-gpu-serial ; do
+#for FILENAME in 1-2-gpu-serial ; do
+for FILENAME in 2-2-stream-parallel ; do
     
     LOG_FOLDER=log/$FILENAME/
     rm -rf $LOG_FOLDER/*
