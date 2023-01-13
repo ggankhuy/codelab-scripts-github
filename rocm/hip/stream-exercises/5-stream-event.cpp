f434d206b55284b18632e7de2435f8102aa0a9a6
Your Name <you@example.com>
2023-01-12 21:44:49 -0800

    checkpoint: 5 working

--- rocm/hip/stream-exercises/5-stream-event.cpp
+++ rocm/hip/stream-exercises/5-stream-event.cpp
@@ -5,8 +5,9 @@
 #include "hip/hip_runtime.h"
 
 #define STREAMS 2
+#define N 1024
 
-__global__ void k1() {
+__global__ void k1(int *a) {
     size_t start = clock64();
     size_t elapsed = 0;
     while (elapsed < 100000000) {
@@ -14,7 +15,7 @@ __global__ void k1() {
     }
 }
 
-__global__ void k2() {
+__global__ void k2(int *a) {
     size_t start = clock64();
     size_t elapsed = 0;
     while (elapsed < 300000000) {
@@ -26,6 +27,13 @@ int main (void) {
     int *a, *b, *c;
     int *dev_a, *dev_b, *dev_c;
     int i ;
+
+    a = (int*)malloc(N * sizeof(int));
+    hipMalloc(&dev_a, N * sizeof(int) );
+
+    for (int i = 0; i < N ; i ++ ) {
+        a[i]  = i;
+    }
 
     hipStream_t s1, s2;
     hipStreamCreate(&s1);
@@ -41,10 +49,17 @@ int main (void) {
     const unsigned blocks = 256;
     const unsigned threadsPerBlock = 1;
 
+    hipMemcpyAsync(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice, s1);
+
     printf("Launch streams...\n");
-    k1<<<256,1,0,s1>>>();
+    k1<<<256,1,0,s1>>>(dev_a);
     hipEventSynchronize(e1);
-    k2<<<256,1,0,s2>>>();
+    k2<<<256,1,0,s2>>>(dev_a);
+
+    hipMemcpyAsync(a, dev_a, N * sizeof(int), hipMemcpyDeviceToHost, s1);
+
+    hipFree(dev_a);
+    free(a);
 
 	return 0;
 }
--- rocm/hip/stream-exercises/run.sh
+++ rocm/hip/stream-exercises/run.sh
@@ -2,9 +2,22 @@
 for FILENAME in 5-stream-event; do
     
     LOG_FOLDER=log/$FILENAME/
+    rm -rf $LOG_FOLDER/*
     mkdir $LOG_FOLDER -p
     hipcc $FILENAME.cpp -o $FILENAME.out && AMD_LOG_LEVEL=4 ./$FILENAME.out 2>&1 | tee $LOG_FOLDER/$FILENAME.AMD_LOG_LEVEL.4.log
-    if [[ $? -eq 0 ]] ; then rocprof --sys-trace $FILENAME.out else echo "compile with hipcc failed." ; exit 1 ; fi
+    if [[ $? -eq 0 ]] ; then 
+        rocprof --sys-trace $FILENAME.out 
+    else 
+        echo "compile with hipcc failed." ; 
+        exit 1 
+    fi
     #rocprof --hip-trace $FILENAME.out
-    if [[ $? -eq 0 ]] ; then mv results.* *.csv $LOG_FOLDER else echo "trace failed." ; exit 1 ; fi
+    if [[ $? -eq 0 ]] ; then 
+        echo "moving traces to $LOG_FOLDER"
+        mv results.* *.csv $LOG_FOLDER 
+        tree -s $LOG_FOLDER/
+    else 
+        echo "trace failed." 
+        exit 1 
+    fi
 done
