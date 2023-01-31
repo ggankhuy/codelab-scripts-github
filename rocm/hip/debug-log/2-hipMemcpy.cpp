bb656459f576a9edb1b6d680c9817a5efa7cca0c
Your Name <you@example.com>
2023-01-31 03:00:37 +0000

    checkpoint

--- rocm/hip/debug-log/2-hipMemcpy.cpp
+++ rocm/hip/debug-log/2-hipMemcpy.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -19,50 +20,44 @@ __global__ void add(int *a, int*b, int *c) {
 	c[tid] = a[tid] + b[tid];
 }
 
-#define timer 0
-
 int main (void) {
     int *a, *b, *c;
     int *dev_a, *dev_b, *dev_c;
     int i ;
 
-    const char* env_timer = std::getenv("timer");
+    string env_timer = getenv("timer");
     cout << "env_timer: " << env_timer << endl;
 
-    const char* env_no_copy = std::getenv("env_no_copy");
+    string env_no_copy = getenv("no_copy");
     cout << "env_no_copy: " << env_no_copy << endl;
 
-    #if timer == 1
     auto end = std::chrono::high_resolution_clock::now();
     auto start = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::high_resolution_clock::now();
     auto ns = std::chrono::high_resolution_clock::now();
-    #endif 
-
+    int ns_fractional;
     a = (int*)malloc(N * sizeof(int));
  	hipMalloc(&dev_a, N * sizeof(int) );
 
-	for (int i = 0; i < N ; i ++ ) {
+	for (int i = 0; i < N ; i ++ )
 		a[i]  = i;
-	}
 
-    #if timer == 1
-    auto start = std::chrono::high_resolution_clock::now();
-    #endif
+    //if (env_timer == "1")
+    start = std::chrono::high_resolution_clock::now();
 
-    #if no_copy == 1
-    printf("hipMemcpy.start.\n");
-   	hipMemcpy(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice);
-    printf("hipMemcpy.end\n");
-    #endif
+    //if (env_no_copy == "1") {
+        printf("hipMemcpy.start.\n");
+   	    hipMemcpy(dev_a, a, N * sizeof(int), hipMemcpyHostToDevice);
+        printf("hipMemcpy.end\n");
+    }
 
-    #if timer == 1
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = (end - start);
-    auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
-    int ns_fractional = static_cast<int>(ns.count());
+    //if (env_timer == "1") {
+    end = std::chrono::high_resolution_clock::now();
+    duration = (end - start);
+    ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+    ns_fractional = static_cast<int>(ns.count());
     cout << setw(30) << "hipMemcpy duration: " << ns_fractional << " ns or " << ns_fractional / 1000000 << " ms" << endl;
-    #endif
+    
 
     hipFree(dev_a);
     free(a);
--- rocm/hip/debug-log/run.sh
+++ rocm/hip/debug-log/run.sh
@@ -10,32 +10,26 @@ mkdir $LOG_DIR -p
 
 # vanilla run.
 
-FILENAME=$FILENAME_ORIG
 SUB_LOG_DIR=$LOG_DIR/$FILENAME
 mkdir -p $SUB_LOG_DIR
 hipcc -D timer=0 $FILENAME_ORIG.cpp  -o $FILENAME.out && \
-AMD_LOG_LEVEL=4  ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
+AMD_LOG_LEVEL=4 ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
 rocprof --sys-trace -d ./$SUB_LOG_DIR/ ./$FILENAME.out
 mv results* ./$SUB_LOG_DIR/
 
 # nosdma
 
-FILENAME=$FILENAME-no-sdma
-SUB_LOG_DIR=$LOG_DIR/$FILENAME
+SUB_LOG_DIR=$LOG_DIR/$FILENAME-no-sdma
 mkdir -p $SUB_LOG_DIR
-export HSA_ENABLE_SDMA=0 
-export AMD_LOG_LEVEL=4  
- ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME-no-sdma.AMD_LOG_LEVEL.4.log
+HSA_ENABLE_SDMA=0 AMD_LOG_LEVEL=4  ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
 rocprof --sys-trace -d ./$SUB_LOG_DIR/ ./$FILENAME.out
 mv results* ./$SUB_LOG_DIR/
 
 # no memCpy 
 
-FILENAME=$FILENAME_ORIG-no-copy
-SUB_LOG_DIR=$LOG_DIR/$FILENAME
+SUB_LOG_DIR=$LOG_DIR/$FILENAME-no-copy
 hipcc -D nocopy=1 $FILENAME_ORIG.cpp  -o $FILENAME.out && \
-export AMD_LOG_LEVEL=4  
- ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
+AMD_LOG_LEVEL=4  ./$FILENAME.out 2>&1 | tee $SUB_LOG_DIR/$FILENAME.AMD_LOG_LEVEL.4.log
 rocprof --sys-trace -d ./$SUB_LOG_DIR/ ./$FILENAME.out
 mv results* ./$SUB_LOG_DIR/
 
