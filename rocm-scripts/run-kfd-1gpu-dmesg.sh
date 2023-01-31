a99c1e0b70d8b2ea2b737c83666251467841681e
Your Name <you@example.com>
2023-01-30 18:23:35 -0800

    run-kfd-1gpu-dmesg.sh: severe bypass logic flaw is fixed

--- rocm-scripts/run-kfd-1gpu-dmesg.sh
+++ rocm-scripts/run-kfd-1gpu-dmesg.sh
@@ -3,16 +3,17 @@ KFD=/usr/local/bin/kfdtest
 DATE=`date +%Y%m%d-%H-%M-%S`
 LOG_FOLDER=log/kfd/log-kfd-$DATE
 mkdir $LOG_FOLDER -p
+DEBUG=0
 TEST_MODE=0
 #LOG_SUMMARY=$LOG_FOLDER/summary.log
 #echo -ne "" > $LOG_SUMMARY
 #t1=$SECONDS
 
-bypass_tests=(CacheInvalidateOnRemoteWrite)
 for i in {2..3} ; do
     #t1a=$SECONDS
     echo "Running kfd for gpu $i..." | tee -a $LOG_SUMMARY
     input="kfdtest-list.txt"
+    counter=0
     while IFS= read -r line
     do
         echo "------------------"
@@ -21,8 +22,8 @@ for i in {2..3} ; do
         dmesg --clear
         LOG_FOLDER_CURR=$LOG_FOLDER/gpu$i
         mkdir -p $LOG_FOLDER_CURR
-        LOG_FILE=$LOG_FOLDER_CURR/$testgroup$line.test.log
-        LOG_FILE_DMESG=$LOG_FOLDER_CURR/$testgroup$line.dmesg.log
+        LOG_FILE=$LOG_FOLDER_CURR/$counter-$testgroup$line.test.log
+        LOG_FILE_DMESG=$LOG_FOLDER_CURR/$counter-$testgroup$line.dmesg.log
         echo $LOG_FILE | grep '\.\.'
         if [[ $? -eq 0 ]] ; then
             echo "setting testgroup to : $line"
@@ -31,31 +32,39 @@ for i in {2..3} ; do
             echo LOG_FILE: $LOG_FILE
             echo LOG_FILE_DMESG: $LOG_FILE_DMESG
             bypass_flag=0
-            for j in CacheInvalidateOnRemoteWrite LargestSysBufferTest LargestSysBufferTest;  do
+            for j in CacheInvalidateOnRemoteWrite LargestSysBufferTest LargestSysBufferTest CheckZeroInitializationVram;  do
+
+                if [[ $DEBUG == 1 ]] ; then
+                    echo "Checking if bypass: j: $j, line: $line:"
+                fi
                 if [[ $j == $line ]] ; then
-                    echo "Bypassing $line"
+                    echo "Setting $line to bypass"
                     if [[ $TEST_MODE == 1 ]] ; then
                         sleep 1
                     fi
                     bypass_flag=1
-                else        
-                    echo "test command:"
-                    echo "$KFD --gtest_filter=$testgroup$line 2>&1 | tee $LOG_FILE"
-                    sleep 1
-                    if [[ $TEST_MODE == 0 ]] ; then
-                        echo "Launching test..."
-                        $KFD --gtest_filter=$testgroup$line 2>&1 | tee $LOG_FILE
-                    fi
-                    dmesg | tee $LOG_FILE_DMESG
-                    break
-                 fi
+                fi
                 if [[ $bypass_flag == 1 ]] ; then
-                    echo "Breaking out of bypass test loop as bypass_flag is set"
                     break
                 fi
             done
+            if [[ $bypass_flag == "1" ]] ; then
+                echo "Bypassing test: $line..."
+                sleep 1
+            else
+                echo "test command:"
+                echo "$KFD --gtest_filter=$testgroup$line 2>&1 | tee $LOG_FILE"
+                sleep 1
+                if [[ $TEST_MODE == 0 ]] ; then
+                    echo "Launching test..."
+                    $KFD --gtest_filter=$testgroup$line 2>&1 | tee $LOG_FILE
+                fi
+                dmesg | tee $LOG_FILE_DMESG
+            fi
         fi
+        counter=$((counter+1))
     done < "$input"
+    
     #t2a=$SECONDS
     #perGpuTime=$((t2a-t1a))
     #echo "gpu $i run time: $perGpuTime" | tee -a $LOG_SUMMARY
