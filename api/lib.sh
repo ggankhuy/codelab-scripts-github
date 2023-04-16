3bb578dacabb39ff86ece72b8b9bf2b2ea36ee16
Your Name <you@example.com>
2023-04-16 10:59:42 -0700

    test-api.sh: added

---
+++ api/lib.sh
@@ -0,0 +1,95 @@
+function install_python() {
+    #Python defs.
+
+    sudo yum -y install epel-release
+    sudo yum update -y
+    sudo yum groupinstall "Development Tools" -y
+
+    sudo yum install openssl-devel libffi-devel bzip2-devel -y
+
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Can not cd into $PYTHON_VER directory..."
+        exit 1
+    fi
+    ./configure --enable-optimizations
+    sudo make -j`nproc` install
+
+    echo "Testing the installation..."
+    python$PYTHON_VER_MAJOR --version
+    if [[ $? -ne 0 ]] ; then
+        echo "Unable to find 3.9"
+    fi
+    PATH_PYTHON_U=`which python$PYTHON_VER_MAJOR`
+    echo "new path: $PATH_PYTHON_U"
+    rm -rf /usr/bin/python
+    echo ln -s $PATH_PYTHON_U /usr/bin/python
+    ln -s $PATH_PYTHON_U /usr/bin/python
+    rm -rf /usr/bin/python3
+    ln -s /usr/bin/python /usr/bin/python3
+    cd ..
+}
+
+function install_conda() {
+    
+    # Anaconda defs.
+
+    FILE_ANACONDA=Anaconda3-2021.11-Linux-x86_64.sh
+    PREFIX_ANACONDA=/Anaconda3
+    CONFIG_UPGRADE_ANACONDA=1
+    EXEC_PATH_CONDA=$PREFIX_ANACONDA/bin/conda
+
+    wget -nc https://repo.anaconda.com/archive/$FILE_ANACONDA
+    ret=$? ; echo $ret.
+    if [[ $ret -ne 0 ]] ; then
+        echo "code: $ret. Download failure for $FILE_ANACONDA, check the url."
+        exit 1
+    fi
+    chmod  777 $FILE_ANACONDA
+    mkdir -p $PREFIX_ANACONDA
+    if [[ $CONFIG_UPGRADE_ANACONDA=="1" ]] ; then
+        echo "Installing/upgrading regardless of existing installation..."
+        ./$FILE_ANACONDA  -u -b -p $PREFIX_ANACONDA
+    else
+        echo "Installing only if it is not installed in $PREFIX_ANACONDA location..."
+        ./$FILE_ANACONDA  -b -p $PREFIX_ANACONDA
+    fi    
+
+
+    #if [[ `cat ~/.bashrc | grep PATH | grep $PREFIX_ANACONDA` ]] ; then
+    #    echo "Inserting path onto bashrc in case reboot next time. "
+    #    echo "export PATH=$PATH:$PREFIX_ANACONDA/bin" >> ~/.bashrc
+    #else
+    #    echo "conda path is already defined in bashrc."
+    #fi
+    ln -s $EXEC_PATH_CONDA /usr/bin/conda
+    echo "Testing the installation..."
+    $EXEC_PATH_CONDA
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Can not find $EXEC_PATH_CONDA."
+        exit 1
+    fi  
+}
+
+function set_os_type() {
+
+    OS_NAME=Ubuntu
+    echo OS_NAME: 
+    case  in
+    Ubuntu)
+            echo Ubuntu is detected...
+            PKG_EXEC=apt
+      ;;
+   CentOS Linux)
+      echo CentOS is detected...
+      PKG_EXEC=yum
+   CentOS Stream)
+      echo CentOS is detected...
+      PKG_EXEC=yum
+   *)
+      echo Unsupported O/S, exiting... ; exit 1
+      PKG_EXEC=""
+     ;;
+esac
+
