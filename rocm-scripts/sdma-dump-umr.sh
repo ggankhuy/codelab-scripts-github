030359f1a1fd6df11b58d14d9b63f5b0bac2a357
Your Name <you@example.com>
2023-04-17 09:24:30 -0700

    add back removed files due to conflict reso

---
+++ rocm-scripts/sdma-dump-umr.sh
@@ -0,0 +1,20 @@
+DATE_FIXED=`date +%Y%m%d-%H-%M-%S`
+ROCM_ROOT=/opt/rocm-5.2.0
+PATH_UMR=/root/gg/git/umr-mod-cp/build/src/app/umr
+LOG_FOLDER_BASE=log/umr/$DATE_FIXED
+mkdir -p $LOG_FOLDER_BASE
+LOG_SUMMARY=$LOG_FOLDER_BASE/summary.log
+
+$PATH_UMR -go 0
+
+for gpu in {0..3}; do
+    gpu_addr=`lspci | grep Disp  | head -$((gpu+1)) | tail -1 | cut -d ' ' -f1`
+    for sdma in {0..4}; do
+
+#        $PATH_UMR --pci 0000:$gpu_addr -O no_follow_ib -RS sdma$sdma | tee $LOG_FOLDER_BASE/umr.gpu-$gpu$i.sdma-$sdma.log
+#        $PATH_UMR --pci 0000:$gpu_addr -O no_follow_ib -RS sdma$sdma[.] | tee $LOG_FOLDER_BASE/umr.gpu-$gpu$i.sdma-$sdma.log
+        $PATH_UMR --pci 0000:$gpu_addr -O follow -RS sdma$sdma[:] | tee $LOG_FOLDER_BASE/umr.gpu-$gpu$i.sdma-$sdma.log
+#        $PATH_UMR --pci 0000:$gpu_addr -RS sdma$sdma | tee $LOG_FOLDER_BASE/umr.gpu-$gpu$i.sdma-$sdma.log
+        done
+done
+        
