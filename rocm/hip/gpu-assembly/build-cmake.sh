84034129c48f5e6e1f228061120f7a2b8b81aeab
Your Name <you@example.com>
2023-04-18 11:03:33 -0700

    build-cmake.sh: support for multiple executables generation

--- rocm/hip/gpu-assembly/build-cmake.sh
+++ rocm/hip/gpu-assembly/build-cmake.sh
@@ -3,17 +3,21 @@ rocm_rpath=" -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib:/opt/rocm/lib64"
 compiler="hipcc"
 cmake_executable="cmake"
 
-mkdir ./log
-mkdir build
-cd build
-rm -rf ./*
-#cmake -DCMAKE_PREFIX_PATH=/opt/rocm/hip/lib .. 2>&1 | tee ./log/cmake.log/
-#make 2>&1 | tee ./log/make.log
-CXX=${rocm_path}/bin/$compiler ${cmake_executable} \
-    -DCMAKE_PREFIX_PATH="${rocm_path}/hip/lib" \
-    -DCMAKE_MODULE_PATH="${rocm_path}/hip/cmake" \
-    -DCMAKE_SHARED_LINKER_FLAGS="${rocm_rpath}" \
-    -DROCM_PATH=${rocm_path} \
-    ..
-make
+for i in project_name in vector vector-1024 ; do
+    mkdir ./log
+    mkdir build
+    cd build
+    rm -rf ./*
+    #cmake -DCMAKE_PREFIX_PATH=/opt/rocm/hip/lib .. 2>&1 | tee ./log/cmake.log/
+    #make 2>&1 | tee ./log/make.log
+    CXX=${rocm_path}/bin/$compiler ${cmake_executable} \
+        -DCMAKE_PREFIX_PATH="${rocm_path}/hip/lib" \
+        -DCMAKE_MODULE_PATH="${rocm_path}/hip/cmake" \
+        -DCMAKE_SHARED_LINKER_FLAGS="${rocm_rpath}" \
+        -DROCM_PATH=${rocm_path} \
+        -DPROJECT_NAME=${project_name}
+        ..
+        make
+    cd ..
+done
 
