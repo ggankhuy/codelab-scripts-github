805db390b68e9f382f7167237571bc7b4f8a298a
Your Name <you@example.com>
2023-04-20 14:00:38 -0700

    build-2.sh: renamed build.sh

--- rocm/hip/gpu-assembly/build.sh
+++ rocm/hip/gpu-assembly/build.sh
@@ -1,4 +1,34 @@
+#!/bin/bash
+
 rocm_path=/opt/rocm
+
+# Process compiler arguments defines, create source sh and header file.
+
+CMP_ARGS_FILENAME_SRC=cmp_args_2.txt
+CMP_ARGS_FILENAME_SH=cmp_args.sh
+CMP_ARGS_FILENAME_C_HDR=cmp_args.h
+SUMMARY_LOG=summary.log
+
+cat $CMP_ARGS_FILENAME_SRC > $CMP_ARGS_FILENAME_SH
+echo "" | tee $CMP_ARGS_FILENAME_C_HDR
+while IFS= read -r line
+do
+    VAR=`echo "$line" | tr -s ' ' | cut -d '=' -f1`
+    VALUE=`echo "$line" | tr -s ' ' | cut -d '=' -f2`
+    if [[ ! -z $VAR ]] && [[ ! -z $VALUE ]] ; then 
+        echo "#define $VAR $VALUE" >> $CMP_ARGS_FILENAME_C_HDR 
+    else
+        echo "" >> $CMP_ARGS_FILENAME_C_HDR
+
+    fi
+done < "$CMP_ARGS_FILENAME_SRC"
+
+
+source $CMP_ARGS_FILENAME_SH
+SLEEP=0
+
+# This script defines.
+
 rocm_rpath=" -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib:/opt/rocm/lib64"
 compiler="hipcc"
 cmake_executable="cmake"
@@ -7,79 +37,92 @@ project_name=vector
 LOG_FILE_BUILD=cmake-stdout.log
 LOG_FILE_EXEC=exec.log
 
-#for sub_project_name in vector vector-4 ; do
 rm -rf bindir/*
+echo -ne "" | tee $SUMMARY_LOG
 
-for sub_project_name in vector matrix_256x256_32x32x1 matrix_256x256_32x32x1_float ; do
-#for sub_project_name in vector vector4 vector64 vector1024 matrix_32x32_8x8x1 matrix_256x256_32x32x1 matrix_256x256_32x32x1_float; do
+# Create executables through loop.
 
-#    if (env_project_name_str == "matrix_32x32_8x8x1") { MAT_X=32; MAT_Y=32; N=(MAT_X*MAT_Y; T_X=8; T_Y=8; T_Z=1; }
-#    if (env_project_name_str == "matrix_32x32_4x4x1") { MAT_X=32; MAT_Y=32; N=(MAT_X*MAT_Y; T_X=4; T_Y=4; T_Z=1;  }
-#    if (env_project_name_str == "matrix_256x256_32x32x1") { MAT_X=16; MAT_Y=64; N=(MAT_X*MAT_Y; T_X=32; T_Y=32; T_Z=1; }
-#    if (env_project_name_str == "matrix_256x256_64x16x1") { MAT_X=256; MAT_Y=256; N=(MAT_X*MAT_Y; T_X=64; T_Y=16; T_Z=1; }
-#    if (env_project_name_str == "matrix_256x256_16x64x1") { MAT_X=256; MAT_Y=256; N=(MAT_X*MAT_Y; T_X=16; T_Y=64; T_Z=1; }
+FILENAME=tensor
 
-    for envvar in OP DATATYPE DATASHAPE ; do
-        unset $envvar
-    done
-    echo "-----------------"
-    case "$sub_project_name" in
-        "matrix_32x32_8x8x1")
-            echo "Setting export variables for 32x32_8x8x1"
-            export FILENAME=matrix
+    #ADD_INT32_4 \
+    #ADD_INT32_1024 \
+    #ADD_FP32_16_16; do
+for EXEC_NAME_PART in \
+    ADD_INT32_8 \
+    ADD_INT32_64 \
+    ADD_INT32_16_16 \
+    ;do
 
-            # unused for now.
-            export DATATYPE=int
-            export DATASHAPE=matrix
-            export OP=add
-            ;;
-        "matrix_256x256_32x32x1")
-            echo "Setting export variables for 32x32_8x8x1"
-            export FILENAME=matrix
-            ;;
-        "matrix_256x256_32x32x1_float")
-            echo "Setting export variables for 32x32_8x8x1"
-            export FILENAME=matrix
-            export DATATYPE=float
-            ;;
-        "vector")
-            echo "Setting export variables for vector"
-            export FILENAME=vector
-            export OP=add
-            ;;
-        "vector4")
-            echo "Setting export variables for vector4"
-            export FILENAME=vector
-            export OP=mul
-            ;;
-        "vector1024")
-            echo "Setting export variables for vector1024"
-            export FILENAME=vector
-            export OP=mul_add
-            
-            ;;
-    esac
+    echo "========================"
+    TOKEN_OP=`echo $EXEC_NAME_PART | tr -s ' '  |cut -d '_' -f1`
+    TOKEN_DATATYPE=`echo $EXEC_NAME_PART | tr -s ' '  |cut -d '_' -f2`
+    TOKEN_X=`echo $EXEC_NAME_PART | tr -s ' '  |cut -d '_' -f3`
+    TOKEN_Y=`echo $EXEC_NAME_PART | tr -s ' '  |cut -d '_' -f4`
+    TOKEN_Z=`echo $EXEC_NAME_PART | tr -s ' '  |cut -d '_' -f5`
 
-    echo "Generating project: $project_name..."
-    BUILD_DIR=build-$sub_project_name
-    mkdir ./log
-    mkdir $BUILD_DIR
-    mkdir ./bindir
-    #sudo rm -rf ./bindir/*
-    #rm -rf $BUILD_DIR/*
-    cd $BUILD_DIR
-    sudo ln -s ../$FILENAME.cpp .
-    sudo ln -s ../$FILENAME.h .
+    if TOKEN_Y="" ; then TOKEN_Y=1 ; fi
+    if TOKEN_Z="" ; then TOKEN_Z=1 ; fi
 
-    export PROJECT_NAME=$sub_project_name
-    CMD="hipcc --save-temps $FILENAME.cpp -o $sub_project_name"
-    echo $CMD
-    $CMD
+    ARG1="-DOP=$TOKEN_OP"
+    ARG2="-DDATATYPE=$TOKEN_DATATYPE"
+    ARG3="-DX=$TOKEN_X"
+    ARG4="-DY=$TOKEN_Y"
+    ARG5="-DZ=$TOKEN_Z"
 
-    echo ln -s `pwd`/${sub_project_name} ../bindir/${sub_project_name}
-    ln -s `pwd`/${sub_project_name} ../bindir/${sub_project_name}
-    ../bindir/${sub_project_name} | tee -a ./$LOG_FILE_EXEC
-    cd ..
-    ls -l bindir
+    COMPILER_ARGS=" $ARG1 $ARG2 $ARG3 $ARG4 $ARG5"
+    echo "args: $ARGS"
+
+    # tiles (workgroup)
+    # 4x4, 8x8, 16x16, 32x32, 16x64, 64x16
+    # inside vector. 
+
+    for tile in 4x1 8x1 4x4 8x8; do
+        echo "------------------------"
+        TILE_X=`echo $tile | tr -s ' '  |cut -d 'x' -f1`
+        TILE_Y=`echo $tile | tr -s ' '  |cut -d 'x' -f2`
+
+        TILE_ARG="-DTILEX=$TILE_X -DTILEY=$TILE_Y"
+
+        BUILD_DIR=build-$EXEC_NAME_PART-$tile
+        EXEC_NAME_FULL=$BUILD_DIR
+        echo "Generating project: $BUILD_DIR"
+        echo "tokens: $TOKEN_X $TOKEN_Y $TOKEN_Z, tiles: $TILE_X $TILE_Y"
+
+        if [[ $TILE_X -gt $TOKEN_X ]] ; then 
+            echo "1. tile X=$TILE_X is greater than X-dim=$TOKEN_X of tensor. This build will skip." ; 
+            sleep $SLEEP
+            echo "$BUILD_DIR: BYPASS" | tee -a $SUMMARY_LOG
+            continue 
+        fi
+
+        if [[ $TILE_Y -gt $TOKEN_Y ]] ; then 
+            echo "2. tile Y=$TILE_Y is greater than Y-dim=$TOKEN_Y of tensor. This build will skip." ; 
+            sleep $SLEEP 
+            echo "$BUILD_DIR: BYPASS" | tee -a $SUMMARY_LOG
+            continue 
+        fi
+
+        mkdir ./log
+        mkdir $BUILD_DIR
+        mkdir ./bindir
+        #sudo rm -rf ./bindir/*
+        #rm -rf $BUILD_DIR/*
+        cd $BUILD_DIR
+        sudo ln -s ../$FILENAME.cpp .
+        sudo ln -s ../$FILENAME.h .
+        sudo ln -s ../$CMP_ARGS_FILENAME_C_HDR .
+
+        export PROJECT_NAME=$EXEC_NAME_PART
+
+        CMD="hipcc --save-temps $COMPILER_ARGS $TILE_ARG $FILENAME.cpp -o $EXEC_NAME_FULL"
+        echo "$CMD" 2>&1 | tee build.log
+        $CMD 2>&1 | tee -a build.log
+
+        echo ln -s `pwd`/${EXEC_NAME_FULL} ../bindir/${EXEC_NAME_FULL}
+        ln -s `pwd`/${EXEC_NAME_FULL} ../bindir/${EXEC_NAME_FULL}
+        ../bindir/${EXEC_NAME_FULL} | tee -a ./$LOG_FILE_EXEC
+        cd ..
+        echo "$BUILD_DIR: OK" | tee -a $SUMMARY_LOG
+        done
 done
 tree -fs bindir
