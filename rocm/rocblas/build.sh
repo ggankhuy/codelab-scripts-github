39dad25630fbe7a5719a613dbc2e172d002561e3
Your Name <you@example.com>
2023-04-23 17:31:38 +0000

    build.sh: improvement in build directory hierarchy, debug symbol option

--- rocm/rocblas/build.sh
+++ rocm/rocblas/build.sh
@@ -1,17 +1,24 @@
-#/opt/rocm-4.5.2/hip/include/hip/
-#-I$(ROCM_PATH)/hsa/include
-rm -rf build/*
-mkdir build
-cd build
-FILENAME=sscal
-FILENAME=example_sgemm
-#FILENAME=gemm
-ln -s ../$FILENAME.cpp
-for i in helpers.hpp ArgParser.hpp ; do 
-    ln -s ../$i .
+set -x
+ENABLE_DEBUG_SYMBOLS=1
+
+OPT_DEBUG=""
+
+if [[ $ENABLE_DEBUG_SYMBOLS -eq 1 ]] ; then OPT_DEBUG=" -g " ; fi
+
+for FILENAME in sscal example_sgemm ; do
+    for i in helpers.hpp ArgParser.hpp ; do 
+        BUILD_DIR=build-$FILENAME
+        mkdir $BUILD_DIR
+        rm -rf $BUILD_DIR/*
+        cd $BUILD_DIR
+        #ln -s ../$i .
+        ln -s ../$FILENAME.cpp .
+        hipcc  --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
+        hipcc $FILENAME.o  /opt/rocm/lib/librocblas.so -o $FILENAME.out
+        hipcc $OPT_DEBUG --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
+        hipcc $FILENAME.o  /opt/rocm/lib/librocblas.so -o $FILENAME.dbg.out
+        cd ..
+    done
 done
 
-hipcc --offload-arch=gfx908 --save-temps -c $FILENAME.cpp
-hipcc $FILENAME.o  /opt/rocm/lib/librocblas.so
-cd ..
 
