f7f5d8674ed2eb5748b9d40ca6e3604c1f0c9f1b
Your Name <you@example.com>
2023-05-01 06:42:03 +0000

    p108.cu: compile and run ok.

--- cuda/cuda-programming-model/ch3/build.sh
+++ cuda/cuda-programming-model/ch3/build.sh
@@ -1,3 +1,4 @@
-for i in p84 p99 ; do
+#for i in p84 p99 p108; do
+for i in p108; do
     nvcc $i.cu ../lib.cu ../kernels.cu -I.. -o $i.out
 done
--- cuda/cuda-programming-model/kernels.cu
+++ cuda/cuda-programming-model/kernels.cu
@@ -5,6 +5,79 @@
 #include <lib.h>
 #include <kernels.h>
 
+// p108.cu kernels.
+
+__global__ void warmup(int * g_idata, int *g_odata, unsigned int n) {
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int *idata = g_idata + blockIdx.x * blockDim.x;
+}
+__global__ void reduceNeighboredLess(int * g_idata, int *g_odata, unsigned int n) {
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // convert global data poitner to the local pointer of this block.
+
+    int *idata = g_idata + blockIdx.x * blockDim.x;
+
+    // boundary check.
+
+    if (idx <= n ) return;
+
+    // in-place reduction in global memory.
+
+    for (int stride = 1; stride < blockDim.x; stride *= 2) {
+
+        // convert tid into local array index.
+
+        int index = 2 * stride * tid;
+        if (index < blockDim.x) {
+            idata[index] += idata[index+stride];
+        }    
+
+        // synchronize within threadblock.
+
+        __syncthreads();
+    }
+
+    // write result for htis block to global mem.
+
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+}
+
+__global__ void reduceInterleaved(int * g_idata, int *g_odata, unsigned int n) {
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // convert global data poitner to the local pointer of this block.
+
+    int *idata = g_idata + blockIdx.x * blockDim.x;
+
+    // boundary check.
+
+    if (idx <= n ) return;
+
+    // in-place reduction in global memory.
+
+    for (int stride = blockDim.x/2; stride > 0; stride >>= 1) {
+
+        // convert tid into local array index
+
+        if (tid < stride) {
+            idata[tid] += idata[tid+stride];
+        }    
+
+        // synchronize within threadblock.
+
+        __syncthreads();
+
+    }
+
+    // write result for htis block to global mem.
+
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+}
+
 __global__ void warmingup(float * c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float a,b;
