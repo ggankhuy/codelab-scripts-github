0b770cf3133c3fda871498f75c0b7fa1bd0d7bdb
Your Name <you@example.com>
2023-05-10 21:28:21 -0700

    run-container.sh: enhancement

--- docker-scripts/create-container.sh
+++
@@ -1,7 +0,0 @@
-set +x
-if [[ -z $1 ]] || [[ -z $2 ]] ; then
-    echo "Usage: $0 <docker_image_name> <conainer_name>"
-    exit 1
-fi
-sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name=$2 -v $HOME/dockerx:/dockerx $1 
-
