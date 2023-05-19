41874a3ae9cd6943a6de11d1b4bcd445f31d139a
Your Name <you@example.com>
2023-05-19 18:43:24 +0000

    prebuild.sh: use rocm_source_dw api than script

--- build-install-scripts/rocm/helper/sh/common.sh
+++ build-install-scripts/rocm/helper/sh/common.sh
@@ -20,6 +20,7 @@ function set_os_type() {
       echo "Ubuntu is detected..."
       PKG_EXEC=apt
       PKG_EXT=deb
+      ln -s /usr/bin/python3  /usr/bin/python
       ;;
    "CentOS Stream")
       echo "CentOS is detected..."
@@ -126,3 +127,46 @@ function setup_root_rocm_softlink () {
     fi
 }
 
+function rocm_source_dw() {
+    p1=$1
+    SUDO=sudo
+    CONFIG_VERSION=4.1
+    DATE=`date +%Y%m%d-%H-%M-%S`
+    HOME_DIR=`pwd`
+    if [[ -z $p1 ]] ; then
+        echo "Version not specified."
+        return 200
+    else
+        CONFIG_VERSION=$p1
+    fi
+
+    set_os_type
+
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name"
+    git config --global color.ui false
+    DIR_NAME=$HOME_DIR/ROCm-$CONFIG_VERSION
+
+    if [[ -d $DIR_NAME ]] ; then
+        echo "Directory $DIR_NAME exists, moving to $DUR_NAME-$DATE "
+        $SUDO mv $DIR_NAME $DIR_NAME-$DATE
+    fi
+    mkdir $DIR_NAME
+
+    if [[ $? -ne 0 ]] ; then
+    	echo "Directory is already there. Verify it is deleted or renamed before continue." ; return 1
+    fi
+
+    pushd  $DIR_NAME
+    mkdir -p ~/bin/
+    echo "install repo..."
+    $SUDO $PKG_EXEC install curl -y && $SUDO curl https://storage.googleapis.com/git-repo-downloads/repo | $SUDO tee ~/bin/repo
+    $SUDO chmod a+x ~/bin/repo
+    echo "repo init..."
+    $SUDO ~/bin/repo init -u https://github.com/RadeonOpenCompute/ROCm.git -b roc-$CONFIG_VERSION.x
+    echo "repo sync..."
+    $SUDO ~/bin/repo sync
+    echo "ROCm source is downloaded to $DIR_NAME"
+    echo "push $DIR_NAME to get there..."
+    popd
+}
