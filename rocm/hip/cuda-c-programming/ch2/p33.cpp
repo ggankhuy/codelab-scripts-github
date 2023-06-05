cca29cce73f1d48398b6fdd8c3c7e68d04e1c817
Your Name <you@example.com>
2023-06-05 06:04:32 +0000

    p33.cpp in roc/hip: workaround for printf from kernel call causing compile error, compile but not printing correctly

--- rocm/hip/cuda-c-programming/ch2/p33.cpp
+++ rocm/hip/cuda-c-programming/ch2/p33.cpp
@@ -2,12 +2,27 @@
 #include <stdio.h>
 //#include "lib1/lib1.h"
 
-__global__ void checkIndex(void) {
-    printf("threadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d) blockDim (%d, %d, %d) gridDim: (%d, %d, %d)\n", \
-        threadIdx.x, threadIdx.y, threadIdx.z, \
-        blockIdx.x, blockIdx.y, blockIdx.z, \
-        blockDim.x, blockDim.y, blockDim.z, \
-        gridDim.x, gridDim.y, gridDim.z); 
+__global__ void checkIndex(int * pKernelResp) {
+    printf("threadIdx: (%d), %d, %d) blockIdx: (%d, %d, %d) blockDim (%d, %d, %d) gridDim: (%d, %d, %d)\n", \
+        hipThreadIdx_x, hipThreadIdx_y, hipThreadIdx_z, \
+        hipBlockIdx_x, hipBlockIdx_y, hipBlockIdx_z, \
+        hipBlockDim_x, hipBlockDim_y, hipBlockDim_z, \
+        hipGridDim_x, hipGridDim_y, hipGridDim_z); 
+        *(pKernelResp+0)=hipThreadIdx_x;
+        *(pKernelResp+1)=hipThreadIdx_y;
+        *(pKernelResp+2)=hipThreadIdx_z;
+
+        *(pKernelResp+3)=hipBlockIdx_x;
+        *(pKernelResp+3)=hipBlockIdx_y;
+        *(pKernelResp+3)=hipBlockIdx_z;
+
+        *(pKernelResp+6)=hipBlockDim_x;
+        *(pKernelResp+7)=hipBlockDim_y;
+        *(pKernelResp+8)=hipBlockDim_z;
+
+        *(pKernelResp+9)=hipGridDim_x;
+        *(pKernelResp+10)=hipGridDim_y;
+        *(pKernelResp+11)=hipGridDim_z;
 }
 
 int main(int argc, char **argv) {
@@ -15,7 +30,8 @@ int main(int argc, char **argv) {
     // define total data element.
 
     int nElem = 6;
-    
+    int kernelRes[9] = {0,0,0, 0,0,0, 0,0,0};
+
     // define grid and block structure.
 
     dim3 block(3, 2, 5);
@@ -28,7 +44,12 @@ int main(int argc, char **argv) {
 
     // check grid and block dimension from device side
 
-    checkIndex <<<grid,block>>>();
-    cudaDeviceReset();
+    checkIndex <<<grid,block>>>(kernelRes);
+    printf("threadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d) blockDim (%d, %d, %d) gridDim: (%d, %d, %d)\n", \
+        *(kernelRes+0), *(kernelRes+1), *(kernelRes+2), \
+        *(kernelRes+3), *(kernelRes+4), *(kernelRes+5), \
+        *(kernelRes+6), *(kernelRes+7), *(kernelRes+8), \
+        *(kernelRes+9), *(kernelRes+10), *(kernelRes+11));
+    hipDeviceReset();
     return 0;
 }
