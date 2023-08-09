1fbfb7f22f84fba827f415206e979f2243bb794c
Your Name <you@example.com>
2023-08-09 22:24:55 +0000

    hf: transformer files added

---
+++ ml/hf/transformers/train.py
@@ -0,0 +1,5 @@
+from transformers import AutoConfig
+
+#config = AutoConfig.from_pretrained("/root/.cache/huggingface/hub/models--bigscience--T0_3B/snapshots/8794c7177e3a67b8a0ec739d94eecfa6a591c974/config.json")
+config = AutoConfig.from_pretrained("/root/.cache/huggingface/hub/models--bigscience--T0_3B/snapshots/8794c7177e3a67b8a0ec739d94eecfa6a591c974/config.json")
+
