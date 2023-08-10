0b7ca1a1819be6e0461e934c8413345edc737e5c
Your Name <you@example.com>
2023-08-10 11:00:28 -0700

    run-container.sh: checkpoint, works for nvidia

--- docker-scripts/run-container.sh
+++ docker-scripts/run-container.sh
@@ -1,27 +1,106 @@
 set +x
+
+DEFAULT_GPU="amd"
+
 if [[ -z $1 ]] ; then
     echo "Usage: "
-    echo "To create container: $0 <docker_image_name> <container_name>"
-    echo "To start container: $0 <container_name>"
+    echo "To create container: $0 --name=<container_name> --image=<container_image> --gpu=<rocm,cuda,amd,nvidia>"
+    echo "To start container: $0 --name=<container_name>"   
+    exit 0
+fi
+
+for var in "$@"
+do
+    echo var: $var
+
+    if [[ $var == *"--name="* ]]  ; then
+        name=`echo $var | cut -d '=' -f2`
+    fi
+
+    if [[ $var == *"--image="* ]]  ; then
+        image=`echo "$var" | cut -d '=' -f2`
+    fi
+
+    if [[ $var == *"--gpu="* ]]  ; then
+        gpu=`echo $var | cut -d '=' -f2`
+    fi
+done 
+
+echo "name: $name"
+echo "image: $image"
+echo "gpu: $gpu"
+
+#   Must be specified: container name.
+
+if [[ -z $name ]] ; then
+    echo "Error: container name must be specified."
     exit 1
 fi
 
-if [[ $2 ]] ; then
-    sudo docker run -it \
-        --network=host \
-        --device=/dev/kfd \
-        --device=/dev/dri \
-        --group-add=video \
-        --ipc=host \
-        --cap-add=SYS_PTRACE \
-        --security-opt seccomp=unconfined \
-        --name=$2 -v $HOME/dockerx:/dockerx $1 
-    if [[ $? -ne 0 ]];  then
-        echo "It appears container already exists, starting and logging to container..."
-        sudo docker start $2 ; sudo docker exec -it $2 bash
+#   Attempt to start container.
+
+#ret=`sudo docker container list --all | grep $name | cut -d '=' -f5`
+
+if [[ ! -z $image ]] ; then
+    echo "Image is specified, therefore will attempt to create container..."
+
+    # create based on amd or nvidia:
+
+    if [[ -z $gpu ]] ; then
+        echo "Gpu has not been specified, will default to $DEFAULT_GPU..."
+        gpu=$DEFAULT_GPU
+    fi
+
+    if [[ $gpu == "amd" ]] || [[ $gpu == "rocm" ]] ; then
+        echo "amd selected, docker run command constructed: "
+        echo "sudo docker run -it \
+             --network=host \
+            --device=/dev/kfd \
+            --device=/dev/dri \
+            --group-add=video \
+            --ipc=host \
+            --cap-add=SYS_PTRACE \
+            --security-opt seccomp=unconfined \
+            -v vol-$name:/root/extdir \
+            -w /root/ \
+            --name=$name $image"
+        sudo docker run -it \
+             --network=host \
+            --device=/dev/kfd \
+            --device=/dev/dri \
+            --group-add=video \
+            --ipc=host \
+            --cap-add=SYS_PTRACE \
+            --security-opt seccomp=unconfined \
+            -v vol-$name:/root/extdir \
+            -w /root/ \
+            --name=$name $image 
+    elif [[ $gpu == "nvidia" ]] || [[ $gpu == "cuda" ]] ; then
+        echo "nvidia selected, docker run command constructed: "
+        echo sudo docker run -it --gpus=all --runtime=nvidia --network=host --name=$name  -v vol-$name:/root/hostdir -w /root/ $image
+        sudo docker run -it --gpus=all --runtime=nvidia --network=host --name=$name  -v vol-$name:/root/hostdir -w /root/ $image
+    else
+        echo "Error: Unknown gpu specified: $gpu"
+        exit 1
+    fi
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Create docker failed, it may already exist, will attempt starting..."
+        sudo docker start $name ; sudo docker exec -it $name bash
+        ret=$?
+        if [[ $ret -ne 0 ]] ; then
+            echo "Start docker failed, something wrong! return code: $ret"
+            exit 1
+        fi
     fi
 else
-    sudo docker start $1 ; sudo docker exec -it $1 bash
+    echo "Image not specified, will attempt starting container..."
+    sudo docker start $name ; sudo docker exec -it $name bash
+    ret=$? 
+    if [[ $ret -ne 0 ]] ; then
+        echo "Start docker failed, something wrong! return code: $ret"
+        exit 1
+    fi
 fi
 
 
