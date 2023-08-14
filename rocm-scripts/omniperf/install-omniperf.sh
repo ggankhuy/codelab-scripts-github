8c9b1e647f95f3ce6ed715dc5362b2abca8ef9f4
Your Name <you@example.com>
2023-08-14 20:24:37 +0000

    install-omniperf.sh: checkpoint: installed and build ok

--- rocm-scripts/omniperf/install-omniperf.sh
+++ rocm-scripts/omniperf/install-omniperf.sh
@@ -1,12 +1,17 @@
 set -x 
-git clone https://github.com/AMDResearch/omniperf.git omniperf
+
+if [[ ! -d  omniperf ]] ; then
+    git clone https://github.com/AMDResearch/omniperf.git omniperf
+else
+    echo "omniperf directory already exist. Assuming checkedout ..."
+fi
 cd omniperf
 export INSTALL_DIR=/opt/omniperf
+echo "INSTALL_DIR: $INSTALL_DIR"
 python3 -m pip install -t ${INSTALL_DIR}/python-libs -r requirements.txt
 mkdir build
 cd build
-#cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DPYTHON_DEPS=${INSTALL_DIR}/python-libs   -DMOD_INSTALL_PATH=${INSTALL_DIR}/modulefiles
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DMOD_INSTALL_PATH=${INSTALL_DIR}/modulefiles ..
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DPYTHON_DEPS=${INSTALL_DIR}/python-libs   -DMOD_INSTALL_PATH=${INSTALL_DIR}/modulefiles ..
 if [[ $? -ne  0 ]] ; then
     echo "Error during cmake stage..."
     exit 1
