ae6862312613f28307c0f74804cdc594664d6dc0
Your Name <you@example.com>
2023-08-15 04:28:54 +0000

    omniperf: run.sh: improvement, analyze command added

--- rocm/rocprofv2-mpi-omniperf/omniperf/run.sh
+++ rocm/rocprofv2-mpi-omniperf/omniperf/run.sh
@@ -1,3 +1,14 @@
 FILENAME=p61
+if [[ -z `which omniperf` ]] ; then
+    echo "Error: omniperf is not found. Make sure it is installed and its path is in PATH env var"
+    exit 1
+fi
 hipcc $FILENAME.cpp -o $FILENAME.out
 omniperf profile -n vcopy_all --  ./$FILENAME.out
+output_dir=workloads/vcopy_all/mi100/
+
+if [[ ! -d $output_dir ]] ; then
+    echo "Error: $output_dir is not found. THis script has been tested on mi100 platform. Adjust the variables as necessary for other GPUs."
+    exit 1
+fi
+omniperf analyze -p $output_dir
