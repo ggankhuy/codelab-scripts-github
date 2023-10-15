02dea20ca2d5f5d030532e2d6cc25301de4a3914
Your Name <you@example.com>
2023-10-15 04:06:54 +0000

    bench.sh: improvement, bf_16r not working for rocblas

--- rocm-scripts/blas/bench.sh
+++ rocm-scripts/blas/bench.sh
@@ -3,16 +3,17 @@ ROCBLAS_BENCH=./release/clients/staging/rocblas-bench
 HIPBLASLT_BENCH=./release/hipblaslt-install/bin/hipblaslt-bench
 
 BENCH=$ROCBLAS_BENCH
-BENCH=$HIPBLASLT_BENCH
+#BENCH=$HIPBLASLT_BENCH
 bench="rocblas"
-bench="hipblas-lt"
-GPU=MI100
+#bench="hipblas-lt"
+
+#GPU=MI100
 GPU=MI250
 
 LOG_DIR=./log/$GPU
 mkdir -p $LOG_DIR
 
-for type in f32_r, f16_r, bf16_r ; do
+for type in f32_r f16_r bf16_r ; do
     for size in 4096 8192 ; do
         case "$BENCH" in
             $ROCBLAS_BENCH)
@@ -31,9 +32,13 @@ for type in f32_r, f16_r, bf16_r ; do
                 ;;
         esac
 
-        $BENCH $calc_params_val $size_params | tee $LOG_DIR/$bench-bench.$type.$size.NN.log
-        $BENCH $calc_params_val $size_params  $transposeA_param T | tee $LOG_DIR/$bench-bench.$type.$size.TN.log
-        $BENCH $calc_params_val $size_params  $transposeA_param T $transposeB_param T | tee $LOG_DIR/$bench-bench.$type.$size.TT.log
-        $BENCH $calc_params_val $size_params  $transposeB_param T | tee $LOG_DIR/$bench-bench.$type.$size.NT.log
+#--precision |-r <value>    
+#Precision. Options: h,s,d,c,z,f8_r, bf8_r, f16_r,f32_r,f64_r,bf16_r,f32_c,f64_c,i8_r,i32_r  (Default value is: f32_r)
+
+        $BENCH $calc_params_val -r $type $size_params_val                       | tee $LOG_DIR/$bench-bench.$type.$size.NN.log
+        $BENCH $calc_params_val -r $type $size_params_val $transposeA_param T   | tee $LOG_DIR/$bench-bench.$type.$size.TN.log
+        $BENCH $calc_params_val -r $type $size_params_val $transposeA_param T $transposeB_param T \
+                                                                                | tee $LOG_DIR/$bench-bench.$type.$size.TT.log
+        $BENCH $calc_params_val -r $type $size_params_val $transposeB_param T   | tee $LOG_DIR/$bench-bench.$type.$size.NT.log
     done
 done
