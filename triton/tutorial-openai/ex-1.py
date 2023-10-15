5f02956ed810fe7b61277faec7f7895cddb42494
Your Name <you@example.com>
2023-10-15 07:21:42 +0000

    triton-openai: ex-1.py added not working

---
+++ triton/tutorial-openai/ex-1.py
@@ -0,0 +1,29 @@
+import torch
+import triton
+import triton.language as tl
+
+BLOCK = 512
+
+# This is a GPU kernel in Triton.
+# Different instances of this
+# function may run in parallel.
+@triton.jit
+def add(X, Y, Z, N):
+   # In Triton, each kernel instance
+   # executes block operations on a
+   # single thread: there is no construct
+   # analogous to threadIdx
+   pid = program_id(0)
+   # block of indices
+   idx = pid * BLOCK + arange(BLOCK)
+   mask = idx < N
+   # Triton uses pointer arithmetics  
+   # rather than indexing operators
+   x = load(X + idx, mask=mask)
+   y = load(Y + idx, mask=mask)
+   store(Z + idx, x + y, mask=mask)
+
+#grid = (ceil_div(N, BLOCK),)
+grid = (BLOCK,)
+# no thread-block
+add[grid](x, y, z, x.shape[0])
