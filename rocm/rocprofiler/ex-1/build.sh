9fc606c0a9b0ff97268cf77fc7241f4ab87fa395
Your Name <you@example.com>
2023-11-02 18:48:20 +0000

    roctracer example files added

--- rocm/rocprofiler/ex-1/build.sh
+++ rocm/rocprofiler/ex-1/build.sh
@@ -1,1 +1,3 @@
 g++ -I /opt/rocm-5.7.0/roctracer/include/ main.c /opt/rocm-5.7.0/lib/libroctracer64.so
+#hipcc -I /opt/rocm-5.7.0/roctracer/include/ p61.cpp /opt/rocm-5.7.0/lib/libroctracer64.so
+#hipcc p61.cpp
