e668a4e93807161f63cae59c4915533f669c8354
Your Name <you@example.com>
2023-11-02 19:11:32 +0000

    roctracer: version agnositc

--- rocm/rocprofiler/ex-1/build.sh
+++ rocm/rocprofiler/ex-1/build.sh
@@ -1,13 +1,13 @@
-g++ -I /opt/rocm-5.7.0/roctracer/include/ main.c /opt/rocm-5.7.0/lib/libroctracer64.so
+g++ -I /opt/rocm/roctracer/include/ main.c /opt/rocm/lib/libroctracer64.so
 
 # build with tracer support
-#hipcc -I /opt/rocm-5.7.0/roctracer/include/ p61.cpp /opt/rocm-5.7.0/lib/libroctracer64.so
+#hipcc -I /opt/rocm/roctracer/include/ p61.cpp /opt/rocm/lib/libroctracer64.so
 
 # build with both roctx + rocracer
-#hipcc -I /opt/rocm-5.7.0/roctracer/include/ p61.cpp /opt/rocm-5.7.0/lib/libroctracer64.so  /opt/rocm-5.7.0/lib/libroctx64.so
+#hipcc -I /opt/rocm/roctracer/include/ p61.cpp /opt/rocm/lib/libroctracer64.so  /opt/rocm/lib/libroctx64.so
 
 # build with roctx only
-hipcc -I /opt/rocm-5.7.0/roctracer/include/ p61.cpp /opt/rocm-5.7.0/lib/libroctx64.so
+hipcc -I /opt/rocm/roctracer/include/ p61.cpp /opt/rocm/lib/libroctx64.so
 
 # build without any tracer (working)
 #hipcc p61.cpp
