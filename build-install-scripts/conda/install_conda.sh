bd20cd67581632417274f30d1d39e53d311f3b0b
Your Name <you@example.com>
2023-12-02 16:25:34 -0800

    conda_install.sh: set exec permission

--- build-install-scripts/conda/install_conda.sh
+++ build-install-scripts/conda/install_conda.sh
@@ -1,5 +1,6 @@
 mkdir -p /miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+chmod 755 ./miniconda.sh
 bash ./miniconda.sh -b -u -p /miniconda3
 rm -rf ./miniconda.sh
 echo "export PATH=$PATH:/miniconda3/bin" >> ~/.bashrc
