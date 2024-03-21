79e71eed72cb4ad5866e9e928496ce9fe70b2a66
Your Name <you@example.com>
2024-03-21 03:42:32 +0000

    setup-docker-rocm.sh: rocm install code, untested

--- docker-scripts/setup-docker-rocm.sh
+++ docker-scripts/setup-docker-rocm.sh
@@ -1,29 +1,80 @@
 yum update -y ; yum install cmake git tree nano wget g++ python3-pip 
 sudo dnf install epel-release epel-next-release -y ; sudo dnf config-manager --set-enabled crb ; sudo dnf install epel-release epel-next-release -y
-cd ~/exdir ; mkdir gg; cd gg ; mkdir git log wget back transit ; cd git ; echo "cd `pwd`" >> ~/.bashrc
+cd ~/extdir ; mkdir gg; cd gg ; mkdir git log wget back transit ; cd git ; echo "cd `pwd`" >> ~/.bashrc
 
 if [[ -z $1 ]] ; then
     echo "Usage: "
     echo "$0 --ga --version=6.0 / use ga version of rocm version 6.0"
     echo "$0 --int --mainline --build=13435 --amdgpu=1720120 / use internal version of rocm"
-    echo "$0 --int --release --build=91 --version --amdgpu=1720120 / use internal version of rocm"
+    echo "$0 --int --branch --build=91 --version --amdgpu=1720120 / use internal version of rocm"
 
     exit 0
 fi
+
+set -x 
 
 for var in "$@"
 do
     echo var: $var
 
-    if [[ $var == *"--name="* ]]  ; then
-        name=`echo $var | cut -d '=' -f2`
+    if [[ $var == *"--ga="* ]]  ; then
+        p_ga=1
     fi
-
-    if [[ $var == *"--image="* ]]  ; then
-        image=`echo "$var" | cut -d '=' -f2`
+    if [[ $var == *"--int="* ]]  ; then
+        p_int=1
     fi
-
-    if [[ $var == *"--gpu="* ]]  ; then
-        gpu=`echo $var | cut -d '=' -f2`
+    if [[ $var == *"--version="* ]]  ; then
+        p_version=`echo $var | cut -d '=' -f2`
+    fi
+    if [[ $var == *"--mainline="* ]]  ; then
+        p_mainline=1
+    fi
+    if [[ $var == *"--branch="* ]]  ; then
+        p_branch=1
+    fi
+    if [[ $var == *"--build="* ]]  ; then
+        p_build=`echo $var | cut -d '=' -f2`
+    fi
+    if [[ $var == *"--amdgpu="* ]]  ; then
+        p_amdgpu=`echo $var | cut -d '=' -f2`
     fi
 done
+
+rocm_ver=$p_version
+rocm_build_no=$p_build
+
+if [[ ! -z $p_ga ]] ; then
+    url="http://repo.radeon.com/amdgpu-install/$rocm_ver/rhel/9.2/"
+fi
+
+pushd ~/extdir/gg/wget 
+wget --mirror -L -np -nH -c -nv --cut-dirs=6 -A "*.rpm" -P ./ $url
+
+yum install ./amdgpu-install*.rpm
+popd
+
+if [[ ! -z $p_int ]] ; then
+    url="http://mkmartifactory.amd.com/artifactory/amdgpu-rpm-local/rhel/9.2/builds/1720129/x86_64/a/amdgpu-install*"
+    if [[ ! -z $p_mainline ]] then ; 
+        sudo amdgpu-repo --amdgpu-build=$p_amdgpu --rocm-build=compute-rocm-dkms-no-npi-hipclang/$rocm_build_no
+        #url="http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-rhel-9.x/compute-rocm-dkms-no-npi-hipclang-$rocm_build_no/"
+    fi
+    if [[ ! -z $p_branch ]] then ; 
+        sudo amdgpu-repo --amdgpu-build=1739896 --rocm-build=compute-rocm-rel-$rocm_ver/$rocm_build_no
+        #url="http://compute-cdn.amd.com/artifactory/list/rocm-osdb-rhel-9.x/compute-rocm-rel-$rocm_ver-$rocm_build_no/
+    fi
+fi
+
+exit 0
+amdgpu-install --usecase=rocm --no-dkms -y
+
+
+
+
+
+
+
+
+
+
+
