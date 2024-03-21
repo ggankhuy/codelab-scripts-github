fedf5e3b201b2840ebf6b6dbc07f3130de4b4958
Your Name <you@example.com>
2024-03-21 03:17:32 +0000

    setup-docker-rocm.sh: startup dir

--- docker-scripts/setup-docker-rocm.sh
+++ docker-scripts/setup-docker-rocm.sh
@@ -1,4 +1,6 @@
 yum update -y ; yum install cmake git tree nano wget g++ python3-pip 
 sudo dnf install epel-release epel-next-release -y ; sudo dnf config-manager --set-enabled crb ; sudo dnf install epel-release epel-next-release -y
+cd ~/exdir ; mkdir gg; cd gg ; mkdir git log wget back transit ; cd git ; echo "cd `pwd`" >> ~/.bashrc
+
 
 
