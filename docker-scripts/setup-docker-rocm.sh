74234d36016222e10a14e935ae240fc371930502
Your Name <you@example.com>
2024-03-21 03:14:39 +0000

    setup-docker-rocm.sh

---
+++ docker-scripts/setup-docker-rocm.sh
@@ -0,0 +1,4 @@
+yum update -y ; yum install cmake git tree nano wget g++ python3-pip 
+sudo dnf install epel-release epel-next-release -y ; sudo dnf config-manager --set-enabled crb ; sudo dnf install epel-release epel-next-release -y
+
+
