4d879173b71ea6a5a9f3ce2ec17958a25eae25b4
Your Name <you@example.com>
2024-03-22 01:32:37 +0000

    rdc build scripts broke into grpc and rdc

--- rocm-scripts/rdc/build-grpc.sh
+++ rocm-scripts/rdc/build-grpc.sh
@@ -2,13 +2,18 @@ set -x
 #centos : gcrp part working.
 #ubuntu: in test.
 apt install doxygen  libcap-dev
+build_grpc=0
+build_rdc=1
 
-#git clone -b v1.61.0 https://github.com/grpc/grpc --depth=1 --shallow-submodules --recurse-submodules
+if [[ ! -d grpc ]] ; then 
+    git clone -b v1.61.0 https://github.com/grpc/grpc --depth=1 --shallow-submodules --recurse-submodules
+fi
+
 pushd  grpc
 export GRPC_ROOT=/opt/grpc
 cmake -B build \
     -DgRPC_INSTALL=ON \
-    -DgRPC_BUILD_TESTS=OFF \
+    -DgRPC_BUILD_TESTS=ON \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_INSTALL_PREFIX="$GRPC_ROOT" \
     -DCMAKE_INSTALL_LIBDIR=lib \
@@ -17,11 +22,4 @@ make -C build -j $(nproc)
 sudo make -C build install
 echo "$GRPC_ROOT" | sudo tee /etc/ld.so.conf.d/grpc.conf
 popd
-git clone https://github.com/ROCm/rdc
-pushd rdc
-mkdir -p build
-# default installation location is /opt/rocm, specify with -DROCM_DIR or -DCMAKE_INSTALL_PREFIX
-cmake -B build -DGRPC_ROOT="$GRPC_ROOT"
-make -C build -j $(nproc)
-make -C build install
-popd
+
