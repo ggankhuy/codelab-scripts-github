6af4f5689d16ae2115e783daad8f83607bab7f7e
Your Name <you@example.com>
2024-03-22 01:32:37 +0000

    rdc build scripts broke into grpc and rdc

--- rocm-scripts/rdc/build-rdc.sh
+++ rocm-scripts/rdc/build-rdc.sh
@@ -1,8 +1,9 @@
-set -x
-#git clone https://github.com/ROCm/rdc
-cd rdc
+set -x 
+pushd rdc
 mkdir -p build
+rm -rf ./*
 # default installation location is /opt/rocm, specify with -DROCM_DIR or -DCMAKE_INSTALL_PREFIX
-cmake -B build -DGRPC_ROOT="$GRPC_ROOT"
+cmake -B build -DGRPC_ROOT="$GRPC_ROOT" -DBUILD_RVS=ON
 make -C build -j $(nproc)
 make -C build install
+popd
