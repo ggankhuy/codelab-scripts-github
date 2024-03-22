c9b76cbc40994ac50b551232b7b59ecf4a66a094
Your Name <you@example.com>
2024-03-22 01:08:40 +0000

    rdc build script: build.sh: uncommented git clone lines

--- rocm-scripts/rdc/build.sh
+++ rocm-scripts/rdc/build.sh
@@ -1,5 +1,5 @@
 
-#git clone -b v1.61.0 https://github.com/grpc/grpc --depth=1 --shallow-submodules --recurse-submodules
+git clone -b v1.61.0 https://github.com/grpc/grpc --depth=1 --shallow-submodules --recurse-submodules
 cd grpc
 export GRPC_ROOT=/opt/grpc
 cmake -B build \
@@ -13,7 +13,7 @@ make -C build -j $(nproc)
 sudo make -C build install
 echo "$GRPC_ROOT" | sudo tee /etc/ld.so.conf.d/grpc.conf
 
-#git clone https://github.com/ROCm/rdc
+git clone https://github.com/ROCm/rdc
 cd rdc
 mkdir -p build
 # default installation location is /opt/rocm, specify with -DROCM_DIR or -DCMAKE_INSTALL_PREFIX
