b22c9c8742acbbae592e375e139c6db356943223
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2024-03-28 04:07:14 +0000

    build-rdc.sh: support for /opt/rocm for install path and debian package generation by default added, working ok

--- rocm-scripts/rdc/build-rdc.sh
+++ rocm-scripts/rdc/build-rdc.sh
@@ -2,17 +2,16 @@ set -x
 
 OPTION_BUILD_RVS_SUPPORT=1
 OPTION_CLEAN_BUILD_RDC=1
+OPTION_CREATE_PKG=1
 GRPC_PROTOC_ROOT=/opt/grpc
 
 apt install -y automake make g++ unzip build-essential autoconf libtool pkg-config libgflags-dev libgtest-dev clang-5.0 libc++-dev curl
 apt install -y libyaml-cpp-dev libabsl-dev
 
-CMAKE_PARAMS="-DROCM_DIR=/opt/rocm -DGRPC_ROOT=$GRPC_PROTOC_ROOT " 
+CMAKE_PARAMS="-DROCM_DIR=/opt/rocm -DGRPC_ROOT=$GRPC_PROTOC_ROOT -DCMAKE_INSTALL_PREFIX=/opt/rocm" 
 
-if [[ $OPTION_BUILD_RVS_SUPPORT == 1 ]] ; then 
-    apt install rocm-validation-suite -y
-    CMAKE_PARAMS="$CMAKE_PARAMS -DBUILD_RVS=ON"
-fi
+if [[ $OPTION_CREATE_PKG == 1 ]] ;          then CMAKE_PARAMS="$CMAKE_PARAMS -DCPACK_GENERATOR=DEB" ; fi
+if [[ $OPTION_BUILD_RVS_SUPPORT == 1 ]] ;   then apt install rocm-validation-suite -y ; CMAKE_PARAMS="$CMAKE_PARAMS -DBUILD_RVS=ON" ; fi
 
 if [[ ! -z rdc ]] ; then
     git clone https://github.com/ROCm/rdc
@@ -32,7 +31,7 @@ cmake $CMAKE_PARAMS ..
 #cmake  -DGRPC_ROOT="$GRPC_ROOT" -DBUILD_STANDALONE=off -DBUILD_RVS=Off .. 
 
 #if [[ -z $? ]] ; then make -j $(nproc) install ; fi
-make -j$(nproc)
+make -j$(nproc) install package
 popd
 
 RDC_LIB_DIR=/opt/rocm/rdc/lib
