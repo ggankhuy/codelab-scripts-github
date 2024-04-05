bea02cc99f30b50aba4336279845ed29c3a9f5e4
Your Name <you@example.com>
2024-04-05 15:56:06 +0000

    log/ex1.llama.log: added

--- ml/vllm/1/ex1.py
+++ ml/vllm/1/ex1.py
@@ -7,7 +7,8 @@ prompts = [
     "The future of AI is",
 ]
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-llm = LLM(model="facebook/opt-125m")
+#llm = LLM(model="facebook/opt-125m")
+llm=LLM(model="meta-llama/Llama-2-70b-hf")
 outputs = llm.generate(prompts, sampling_params)
 
 # Print the outputs.
