600801ca423dd7b6a9cddb635b5bef14ee827272
Your Name <you@example.com>
2024-04-06 05:32:52 +0000

    docker-scripts/setup-docker-rocm.sh: major improvements for internal build install

--- docker-scripts/setup-docker-rocm.sh
+++ docker-scripts/setup-docker-rocm.sh
@@ -1,29 +1,45 @@
-# Use this script to install ROCm onto Centos 9 stream docker container.
+    # Use this script to install ROCm onto Centos 9 stream docker container.
 # Currently only RHEL9.2 version is supported.
 # All other configuration will likely to cause fail or unpredictable result.
 # Parameter conflicts are not checked. Usage menu has most 3 commont usage: ga, internal mainline, internal release along with 
 # combination of parameters needed. Any other combination of parameters are currently untested and could fail or cause unpredictable
 # result.
-if [[ -z $1 ]] ; then
-    echo "Usage: "
+
+printHelp() {
+    echo "Usage: parameters:"
+    echo "Note that currently there is not compatility check between different parameters. It is up to user to provide"
+    echo "right combination of parameters. Using wrong combination or incompatible parameters are untested and will have "
+    echo "untested or unpredictable output and could SUT in uncertain state."
+    echo "--ga: use GA general availability repo.radeon.com."
+    echo "--int: use internal version, artifactory."
+    echo "--rocm-build: rocm build number."
+    echo "--rocm-ver: rocm version."
+    echo "--mainline: use mainline branch for internal build."
+    echo "--release: use release branch for internal build." 
+    echo "--amdgpu-build: amdgpu build number."
+    echo "--no-dkms: bypass dkms installation."
+    echo "Usage: examples"
     echo "$0 --ga --rocm-ver=6.0 / use ga version of rocm version 6.0"
     echo "$0 --int --mainline --rocm-build=13435 --rocm-ver=6.1 --amdgpu-build=1720120 / use internal version of rocm"
-    echo "$0 --int --branch --rocm-build=91 --rocm-ver=6.0 / use internal version of rocm"
-
+    echo "$0 --int --release --rocm-build=91 --rocm-ver=6.0 / use internal version of rocm"
     exit 0
+
+}
+
+if [[ -z $1 ]] ; then
+    printHelp
 fi
 
-yum update -y ; yum install cmake git tree nano wget g++ python3-pip sudo -y
-dnf install epel-release epel-next-release -y ; dnf config-manager --set-enabled crb ; dnf install epel-release epel-next-release -y
-cd /$USER/extdir ; mkdir gg; cd gg ; mkdir git log wget back transit ; cd git ; echo "cd `pwd`" >> /$USER/.bashrc
-
 set -x 
-DKMS=1
 
 for var in "$@"
 do
     echo var: $var
 
+    if [[ $var == "--help" ]]  ; then
+        printHelp
+        exit 0 
+    fi
     if [[ $var == *"--ga"* ]]  ; then
         p_ga=1
     fi
@@ -31,12 +47,12 @@ do
         p_int=1
     fi
     if [[ $var == *"--rocm-ver="* ]]  ; then
-        p_rocm_version=`echo $var | cut -d '=' -f2`
+        rocmversion=`echo $var | cut -d '=' -f2`
     fi
     if [[ $var == *"--mainline"* ]]  ; then
         p_mainline=1
     fi
-    if [[ $var == *"--branch"* ]]  ; then
+    if [[ $var == *"--release"* ]]  ; then
         p_branch=1
     fi
     if [[ $var == *"--rocm-build="* ]]  ; then
@@ -45,27 +61,27 @@ do
     if [[ $var == *"--amdgpu-build="* ]]  ; then
         p_amdgpu_build=`echo $var | cut -d '=' -f2`
     fi
+    if [[ $var == *"--dkms-no="* ]]  ; then
+        p_dkms="--dkms-no"
+    fi
 done
 
-rocm_ver=$p_rocm_version
-rocm_build_no=$p_rocm_build
+set -x 
+
 rhel_ver=9.2
 
-if [[ ! -z $p_ga ]] ; then
-    url="http://repo.radeon.com/amdgpu-install/$rocm_ver/rhel/$rhel_ver/"
-fi
-
-if [[ ! -z $p_int ]] ; then
-    #url="http://mkmartifactory.amd.com/artifactory/amdgpu-rpm-local/rhel/$rhel_ver/builds/$p_amdgpu_build/x86_64/a/"
-    url='http://artifactory-cdn.amd.com/artifactory/list/amdgpu-rpm/rhel/amdgpu-install-internal-$rocm_ver_9-1.noarch.rpm'
-fi
+if [[ -z $USER ]] ; then USER=root ; fi
+yum update -y ; yum install cmake git tree nano wget g++ python3-pip sudo -y
+dnf install epel-release epel-next-release -y ; dnf config-manager --set-enabled crb ; dnf install epel-release epel-next-release -y
+cd /$USER/extdir ; mkdir gg; cd gg ; mkdir git log wget back transit ; cd git ; echo "cd `pwd`" >> /$USER/.bashrc
 
 for i in rocm amdgpu ; do 
-    yum repository-packages $i remove -y ; 
+    yum repository-packages $i remove -y ; #if installed through online repository.
+    yum remove $i -y # if installed through offline bundle script.
     rm -rf /etc/yum.repos.d/$i*.repo
 done
+
 # this apparently not working, try using above.
-#yum remove rocm amdgpu -y   
 amdgpu-install --uninstall -y
 
 for i in amdgpu-install amdgpu-install-internal ; do 
@@ -74,32 +90,41 @@ done
 
 mkdir -p /$USER/extdir/gg/wget
 pushd /$USER/extdir/gg/wget 
-wget --mirror -L -np -nH -c -nv --cut-dirs=6 -A "*.rpm" -P ./ $url
-#yum install ./amdgpu-install*.rpm -y
+rm -rf amdgpu-install*
+
+amdgpu_file_name="amdgpu-install-internal-$rocmversion"
+amdgpu_file_name=$amdgpu_file_name"_9-1.noarch.rpm"
+
+echo "rocmversion: $rocmversion"
+if [[ ! -z $p_ga ]] ; then
+    url_amdgpu="http://repo.radeon.com/amdgpu-install/$((rocmversion))/rhel/$rhel_ver/"
+    wget --mirror -L -np -nH -c -nv --cut-dirs=6 -A "*.rpm" -P ./ $url_amdgpu
+fi
+if [[ ! -z $p_int ]] ; then
+    url_amdgpu="http://artifactory-cdn.amd.com/artifactory/list/amdgpu-rpm/rhel/$amdgpu_file_name"
+    wget $url_amdgpu
+fi
+
+echo "url_amdgpu: $url_amdgpu"
+
+#if [[ -z $url_amdgpu ]] ; then echo "url_amdgpu is not set." ; exit 1 ; fi
+#if [[ ! -z $? ]] ; then "echo wget unsuccessful..."  ; exit 1 ; fi
 amdgpu_installer_rpm_path=`find . -name "amdgpu-install*" | head -1`
-#if [[ ! -z $? ]] ; then exit 1; fi
 yum install $amdgpu_installer_rpm_path  -y
 popd
 echo "$rhel_ver" > /etc/dnf/vars/amdgpudistro
 
 if [[ ! -z $p_int ]] ; then
     if [[ ! -z $p_mainline ]] ;  then
-	echo ""
-        #amdgpu-repo --amdgpu-build=$p_amdgpu_build --rocm-build=compute-rocm-dkms-no-npi-hipclang/$rocm_build_no
-        #url="http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-rhel-9.x/compute-rocm-dkms-no-npi-hipclang-$rocm_build_no/"
+        url="http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-rhel-9.x/compute-rocm-dkms-no-npi-hipclang-$p_rocm_build/"
+        amdgpu-repo --amdgpu-build=$p_amdgpu_build --rocm-build=compute-rocm-dkms-no-npi-hipclang/$p_rocm_build
     fi
     if [[ ! -z $p_branch ]] ; then 
-	echo ""
-        #amdgpu-repo --amdgpu-build=1739896 --rocm-build=compute-rocm-rel-$rocm_ver/$rocm_build_no
-        #url="http://compute-cdn.amd.com/artifactory/list/rocm-osdb-rhel-9.x/compute-rocm-rel-$rocm_ver-$rocm_build_no/
+        amdgpu-repo --amdgpu-build=$p_amdgpu_build --rocm-build=compute-rocm-rel-$rocm_ver/$p_rocm_build
     fi
-fi
+fi 
 
-if [[ $DKMS == 1 ]] ; then 
-    amdgpu-install --usecase=rocm -y
-else
-    amdgpu-install --usecase=rocm --no-dkms -y
-fi
+amdgpu-install --usecase=rocm $p_dkms -y
 
 
 
