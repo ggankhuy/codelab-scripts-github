6be3d4d11bc749cb29dff07427a1967729dbb2bc
Your Name <you@example.com>
2024-04-07 06:58:13 +0000

    tf/3: got seq-to-seq.py with proper import

--- ml/tf/tf-from-scratch/3/code-exercises/ch9/seq-to-seq.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch9/seq-to-seq.py
@@ -7,5 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset,  random_split, TensorDataset
 
+import sys
+sys.path.append('..')
 from data_generation.square_sequences import generate_sequences
 #from step
