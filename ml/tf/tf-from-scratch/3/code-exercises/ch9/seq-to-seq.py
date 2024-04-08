c2434f690571428ba3b10688a37ee980dd77cf0c
Your Name <you@example.com>
2024-04-08 05:28:16 +0000

    tf/3: ch9 implemented encoder

--- ml/tf/tf-from-scratch/3/code-exercises/ch9/seq-to-seq.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch9/seq-to-seq.py
@@ -13,3 +13,25 @@ from stepbystep.v4 import StepByStep
 from plots.chapter8 import plot_data
 points, directions = generate_sequences(256,  seed=13)
 fig = plot_data(points, directions, n_rows=1)
+
+class Encoder(nn.Module):
+    def __init__(self, n_features, hidden_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.n_features = n_features
+        self.hidden = None
+        self.basic_rnn =  nn.GRU(self.hidden_dim, self.n_features, batch_first = True)
+        
+    def forward(self, X):
+        rnn_out, self.hidden = self.basic_rnn(X)
+        return rnn_out # N, L, F.
+
+full_seq = torch.tensor([[-1, -1], [-1, 1], [1, 1], [1, -1]]).float().view(1, 4, 2)
+source_seq = full_seq[:, :2] # first two corners
+target_seq = full_seq[:, 2:] # last two corners
+
+torch.manual_seed(21)
+encoder = Encoder(n_features=2, hidden_dim=2)
+hidden_seq = encoder(source_seq) # output is N, L, F
+hidden_final = hidden_seq[:, -1:]   # takes last hidden state
+hidden_final
