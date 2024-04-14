f326e45fd859f5e5c24ec3f60b8795bb974d6eeb
Your Name <you@example.com>
2024-04-14 19:13:15 +0000

    ch8: comparison ok between RNN cell created manually and created by library

--- ml/tf/tf-from-scratch/3/code-exercises/ch8/seq-to-seq.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch8/seq-to-seq.py
@@ -19,14 +19,55 @@ from stepbystep.v4 import StepByStep
 from plots.chapter8 import plot_data
 points, directions = generate_sequences(256,  seed=13)
 
-n_features=5
-hidden_dim=2
 
+# create rnn_cell.
+# rnn_state: 
+# - weight_ih: [n_features, hidden_dim], bias_ih: [hidden_dim]
+# - weight_hh: [hidden_dim, hidden_dim], bias_hh: [hidden_dim]
+
+n_features=2
+hidden_dim=2
 torch.manual_seed(19)
 rnn_cell=nn.RNNCell(input_size=n_features, hidden_size=hidden_dim)
 rnn_state=rnn_cell.state_dict()
 print(rnn_state)
 
-# rnn_state: 
-# - weight_ih: [n_features, hidden_dim], bias_ih: [hidden_dim]
-# - weight_hh: [hidden_dim, hidden_dim], bias_hh: [hidden_dim]
+# Lets do above RNNcell manually. However we are copying the values of weigts, bias initialized values
+# so that output can be compared for sanity check!
+
+linear_input=nn.Linear(n_features, hidden_dim)
+linear_hidden=nn.Linear(hidden_dim, hidden_dim)
+
+with torch.no_grad():
+    linear_input.weight=nn.Parameter(rnn_state['weight_ih'])
+    linear_input.bias=nn.Parameter(rnn_state['bias_ih'])
+    linear_hidden.weight=nn.Parameter(rnn_state['weight_hh'])
+    linear_hidden.bias=nn.Parameter(rnn_state['bias_hh'])
+
+initial_hidden=torch.zeros(1, hidden_dim)
+print(initial_hidden)
+th=linear_hidden(initial_hidden)
+print(th)
+
+X=torch.as_tensor(points[0]).float()
+print(X)
+
+tx=linear_input(X[0:1])
+print(tx)
+
+adding=th+tx
+print(adding)
+
+print("Output of our manually written RNN cell:")
+print(torch.tanh(adding)) 
+
+print("Output of RNN cell from library:")
+print(rnn_cell(X[0:1]))
+
+    
+    
+
+
+
+
+
