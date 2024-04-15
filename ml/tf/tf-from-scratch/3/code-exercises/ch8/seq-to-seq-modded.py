e0a7f590d2acd44800ebb8e54e5315c55ad0e9b4
Your Name <you@example.com>
2024-04-15 03:30:21 +0000

    classes.py: added

--- ml/tf/tf-from-scratch/3/code-exercises/ch8/seq-to-seq-modded.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch8/seq-to-seq-modded.py
@@ -14,7 +14,9 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset,  random_split, TensorDataset
 import sys
 sys.path.append('..')
+
 from common.settings import *
+from common.classes import *
 from data_generation.square_sequences import generate_sequences
 from stepbystep.v4 import StepByStep
 from plots.chapter8 import plot_data
@@ -35,37 +37,6 @@ printDbg(rnn_state)
 
 # Lets do above RNNcell manually. However we are copying the values of weigts, bias initialized values
 # so that output can be compared for sanity check!
-
-class Shark:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-class RNNCell:
-    def __init__(self, rnn_cell_src: nn.RNNCell, input_size:int, hidden_size:int):
-        self.linear_input=nn.Linear(input_size, hidden_size)
-        self.linear_hidden=nn.Linear(hidden_size, hidden_size)
-        rnn_state=rnn_cell_src.state_dict()
-
-        with torch.no_grad():
-            self.linear_input.weight=nn.Parameter(rnn_state['weight_ih'])
-            self.linear_input.bias=nn.Parameter(rnn_state['bias_ih'])
-            self.linear_hidden.weight=nn.Parameter(rnn_state['weight_hh'])
-            self.linear_hidden.bias=nn.Parameter(rnn_state['bias_hh'])
-
-        self.initial_hidden=torch.zeros(1, hidden_size)
-        printDbg(self.initial_hidden)
-        self.th=self.linear_hidden(self.initial_hidden)
-        printDbg(self.th)
-        self.tx=0
-        self.th=0
-        self.final_hidden=0
-
-    def forward(self, x):
-        self.tx=self.linear_input(x)
-        adding=self.th+self.tx
-        printDbg(adding)
-        self.final_hidden=torch.tanh(adding)
 
 X=torch.as_tensor(points[0]).float()
 print(X)
---
+++ ml/tf/tf-from-scratch/3/code-exercises/common/classes.py
@@ -0,0 +1,26 @@
+class RNNCell:
+    def __init__(self, rnn_cell_src: nn.RNNCell, input_size:int, hidden_size:int):
+        self.linear_input=nn.Linear(input_size, hidden_size)
+        self.linear_hidden=nn.Linear(hidden_size, hidden_size)
+        rnn_state=rnn_cell_src.state_dict()
+
+        with torch.no_grad():
+            self.linear_input.weight=nn.Parameter(rnn_state['weight_ih'])
+            self.linear_input.bias=nn.Parameter(rnn_state['bias_ih'])
+            self.linear_hidden.weight=nn.Parameter(rnn_state['weight_hh'])
+            self.linear_hidden.bias=nn.Parameter(rnn_state['bias_hh'])
+
+        self.initial_hidden=torch.zeros(1, hidden_size)
+        printDbg(self.initial_hidden)
+        self.th=self.linear_hidden(self.initial_hidden)
+        printDbg(self.th)
+        self.tx=0
+        self.th=0
+        self.final_hidden=0
+
+    def forward(self, x):
+        self.tx=self.linear_input(x)
+        adding=self.th+self.tx
+        printDbg(adding)
+        self.final_hidden=torch.tanh(adding)
+
