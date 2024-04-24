0a5360ddbe96ffdce8e35321b0c013580f8fc8d0
Your Name <you@example.com>
2024-04-24 22:22:31 +0000

    ch8-p131-bidir-rnn.py: added

---
+++ ml/tf/tf-from-scratch/3/code-exercises/ch8/ch8-p131-bidir-rnn.py
@@ -0,0 +1,26 @@
+import copy 
+import numpy as np
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset,  random_split, TensorDataset
+import sys
+sys.path.append('..')
+
+from common.settings import *
+from common.classes import *
+from data_generation.square_sequences import generate_sequences
+from stepbystep.v4 import StepByStep
+from plots.chapter8 import plot_data
+points, directions = generate_sequences(256,  seed=13)
+
+torch.manual_seed(19)
+rnn_bidirect=nn.RNN(input_size=n_features, hidden_size=hidden_dim, bidirectional=True, batch_first=True)
+state=rnn_bidirect.state_dict()
+
+for k, v in state.items():
+    printDbg("k/v:", k, v)
+
+
