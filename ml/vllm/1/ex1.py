4ea4e59d4fa9324119d645018ecf2b35319c8676
Your Name <you@example.com>
2024-04-27 08:06:29 +0000

    vllm: ex1.py/ex1.sh running code

--- ml/vllm/1/ex1.py
+++ ml/vllm/1/ex1.py
@@ -1,18 +1,17 @@
 from vllm import LLM, SamplingParams
-
+ 
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
+   "Hello, my name is",
+   "The president of the United States is",
+   "The capital of France is",
+   "The future of AI is",
 ]
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-#llm = LLM(model="facebook/opt-125m")
-llm=LLM(model="meta-llama/Llama-2-70b-hf")
+llm=LLM(model="/data/Llama-2-70b-chat-hf", gpu_memory_utilization=0.95)
 outputs = llm.generate(prompts, sampling_params)
-
+ 
 # Print the outputs.
 for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+   prompt = output.prompt
+   generated_text = output.outputs[0].text
+   print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
---
+++ ml/vllm/1/ex1.sh
@@ -0,0 +1,2 @@
+
+torchrun --standalone --nnodes=1 --nproc-per-node=1 ex1.py --tensor-parallel-size 1
