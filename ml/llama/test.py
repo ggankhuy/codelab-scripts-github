76a90766c065878b3b7b2c321a1a6a54c015f5e4
Your Name <you@example.com>
2024-04-29 22:38:10 +0000

    llama: added:

---
+++ ml/llama/test.py
@@ -0,0 +1,35 @@
+from vllm import LLM, SamplingParams
+
+prompts = [
+   "Hello, my name is",
+   "The president of the United States is",
+   "The capital of France is",
+   "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+llm=LLM(model="/data/Llama-2-70b-chat-hf", gpu_memory_utilization=0.95)
+outputs = llm.generate(prompts, sampling_params)
+
+# Print the outputs.
+for output in outputs:
+   prompt = output.prompt
+   generated_text = output.outputs[0].text
+   print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+root@gt-pla-u25-08:/workspace# cat test.py
+from vllm import LLM, SamplingParams
+
+prompts = [
+   "Hello, my name is",
+   "The president of the United States is",
+   "The capital of France is",
+   "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+llm=LLM(model="/data/Llama-2-70b-chat-hf", gpu_memory_utilization=0.95)
+outputs = llm.generate(prompts, sampling_params)
+
+# Print the outputs.
+for output in outputs:
+   prompt = output.prompt
+   generated_text = output.outputs[0].text
+   print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
