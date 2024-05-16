acc8021444572529c1d553542d828c3f4a19fb28
Your Name <you@example.com>
2024-05-16 05:38:25 +0000

    install-conda.sh: run fix and enabnle for nonroot user

--- build-install-scripts/conda/install_conda.sh
+++ build-install-scripts/conda/install_conda.sh
@@ -1,6 +1,9 @@
-mkdir -p /miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
-chmod 755 ./miniconda.sh
-bash ./miniconda.sh -b -u -p /miniconda3
-rm -rf ./miniconda.sh
-echo "export PATH=$PATH:/miniconda3/bin" >> ~/.bashrc
+SUDO=sudo
+$SUDO mkdir -p /miniconda3
+$SUDO wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+$SUDO chmod 755 ./miniconda.sh
+$SUDO bash ./miniconda.sh -b -u -p /miniconda3
+$SUDO rm -rf ./miniconda.sh
+if [[ -z `cat ~/.bashrc | grep export | grep miniconda3` ]] ; then
+    echo "export PATH=$PATH:/miniconda3/bin" >> ~/.bashrc
+fi
