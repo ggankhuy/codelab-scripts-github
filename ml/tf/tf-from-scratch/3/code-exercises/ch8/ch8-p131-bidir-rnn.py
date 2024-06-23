6240d54686a09b5cf2e5a1ccc2f461f264ebd6c2
Your Name <you@example.com>
2024-06-23 10:35:22 -0700

    ch8-p131-bidir-rnn.py: improvised code

--- ml/tf/tf-from-scratch/3/code-exercises/ch8/ch8-p131-bidir-rnn.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch8/ch8-p131-bidir-rnn.py
@@ -35,15 +35,21 @@ rnn_reverse.load_state_dict(dict([(k[:-8], v) \
 x=torch.as_tensor(points[0:1]).float()
 x_rev=torch.flip(x,dims=[1])
 printTensor(x_rev, globals())
-quit(0)
 
 out, h = rnn_forward(x)
 out_rev, h_rev = rnn_reverse(x_rev)
 out_rev_back = torch.flip(out_rev, dims=[1])
 
-printDbg("cat([out, out_rev_back]): \n", torch.cat([out, out_rev_back], dim=2))
-printDbg("cat([out,out_rev_back]: \n", torch.cat([h, h_rev]))
+printTensor(out, globals())
+printTensor(out_rev, globals())
+printTensor(out_rev_back, globals())
+
+out_rev_sum = torch.cat([out, out_rev_back], dim=2)
+printTensor(out_rev_sum, globals())
 
 out, hidden = rnn_bidirect(x)
+printTensor(out, globals())
+printTensor(hidden, globals())
+
 printDbg(out[:, -1] == hidden.permute(1,0,2).view(1, -1))
 
