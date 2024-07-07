4a3b5c7b5903a351e74bcaf694e1562064bdd27a
Your Name <you@example.com>
2024-07-07 05:00:05 +0000

    setup-llama2-stg1.sh: fix in stage again

--- build-install-scripts/llama/setup-llama2-stg1.sh
+++ build-install-scripts/llama/setup-llama2-stg1.sh
@@ -4,8 +4,16 @@
 # changing the actual instalaltion folder to /home/miniconda3 because centos by default alloc-s 
 # only 70gb during installation.
 set -x 
+
+#setup  MINICONDA_SRC_DIR
+
 MINICONDA_SRC_DIR=/home/miniconda3
-MINICONDA_DIR=/$HOME/miniconda3
+export MINICONDA_SRC_DIR=$MINICONDA_SRC_DIR
+
+if [[ -z `cat ~/.bashrc | egrep "export.*MINICONDA_SRC_DIR"` ]] ; then
+    echo "export MINICONDA_SRC_DIR=$MINICONDA_SRC_DIR" | sudo tee -a ~/.bashrc
+fi
+
 LLAMA_PREREQ_PKGS=20240502_quanta_llamav2
 CONDA=/$HOME/miniconda3/bin/conda
 
@@ -15,12 +23,18 @@ chmod 755 ./miniconda.sh
 bash ./miniconda.sh -b -u -p /$MINICONDA_SRC_DIR
 rm -rf ./miniconda.sh
 
-if [[ -z `cat ~/.bashrc | egrep "export.*$MINICONDA_SRC_DIR/bin"` ]] ; then
-    echo "export PATH=$PATH:/$MINICONDA_SRC_DIR/bin" | sudo tee -a ~/.bashrc
-fi
 ln -s $MINICONDA_SRC_DIR /$HOME/
 
+# setup CONDA_ENV_NAME
+
 CONDA_ENV_NAME="llama2"
+CONDA_ENV_NAME="llama2-test-1"
+export CONDA_ENV_NAME=$CONDA_ENV_NAME
+
+if [[ -z `cat ~/.bashrc | egrep "export.*CONDA_ENV_NAME"` ]] ; then
+    echo "export CONDA_ENV_NAME=$CONDA_ENV_NAME" | sudo tee -a ~/.bashrc
+fi
+
 $CONDA create --name  $CONDA_ENV_NAME python==3.9 -y
 $CONDA init
 echo "conda activate $CONDA_ENV_NAME" >> ~/.bashrc
