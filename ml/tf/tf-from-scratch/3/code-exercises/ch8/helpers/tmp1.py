6f76556b786c643920bda5c24425b26a6b828071
Your Name <you@example.com>
2024-07-20 23:07:12 -0700

    checkpoint: able to print class member variable name in printTensor, needs code cleanup!

--- ml/tf/tf-from-scratch/3/code-exercises/ch8/helpers/tmp1.py
+++ ml/tf/tf-from-scratch/3/code-exercises/ch8/helpers/tmp1.py
@@ -6,7 +6,29 @@ sys.path.append('../..')
 
 from common.settings import *
 from common.classes import *
+class tmp1:
+    tmp1_x = 10
+    tmp1_y = [0,1,2,3]
 
+    def tmp1_fn1(self):
+
+        print("printing namespaces from within a class...")
+        #print(self.__dict__)
+
+        printTensor(self.tmp1_x, getGlobalsClass(self))
+        printTensor(self.tmp1_y, getGlobalsClass(self))
+
+def getGlobalsClass(pObj):
+    print("getGlobalClass entered...")
+    d1={}
+    for i in dir(pObj):
+        d1[i] = getattr(pObj,i)
+    print("returning: ", type(d1))
+    for i in d1.keys():
+        print (" -- ", i, d1[i])
+
+    return d1
+    
 def namestrLocal(obj, namespace):
     return [name for name in namespace if namespace[name] is obj]
 
@@ -18,10 +40,13 @@ def printTensorLocal(pVarName):
     print(namestr(pVarName, globals()))
     print('--------------------------------')
 
+'''
 printTensorLocal(x)
 printTensor(x)
 printTensor(x, globals())
-
+'''
+tmp1_inst = tmp1()
+tmp1_inst.tmp1_fn1()
 
 # printTensorLocal(x)->nameStr() ok
 # printTensorLocal(x)->nameStrLocal() ok
--- ml/tf/tf-from-scratch/3/code-exercises/common/settings.py
+++ ml/tf/tf-from-scratch/3/code-exercises/common/settings.py
@@ -33,9 +33,23 @@ def get_variable_name(obj, namespace):
 
 @printFnc
 def namestr(obj, namespace):
-    return [name for name in namespace if namespace[name] is obj]
+    print("namestr entered...")
+    print("obj: ", obj, type(obj))
+    print("namespace: ", type(namespace))
 
-def printTensor(pVarName, pGlobals=None, pOverride=None):
+    if type(namespace) == dict:
+        for key in namespace.keys():
+            print(" - ", key, ": ", namespace[key])
+        return [name for name in namespace if namespace[name] is obj]
+    else:
+        for i in namespace:
+            print("id(obj): ", id(obj), obj)
+            print(" - ", i, id(i))
+            if  getattr(i) == obj:
+                return i
+
+def printTensor(pVar, pGlobals=None, pOverride=None):
+    print("printTensor entered...")
     CONFIG_PRINT_TENSOR_SHAPE_ONLY=1
     CONFIG_PRINT_TENSOR_BRIEF=0
     CONFIG_PRINT_TENSOR_BRIEF_DIM1=4
@@ -45,22 +59,26 @@ def printTensor(pVarName, pGlobals=None, pOverride=None):
 
     g=None
     if pGlobals:
+        print("globals: OK")
         g=pGlobals
     else:
+        print("globals: None.")
         g=globals()
-
     # for debug purpose only.
     #for i in g:
     #    print(i)
 
-    varName=pVarName
+    lVar=pVar
+    lVarName=None
 
+    if type(pVar)==list:
+        lVar=numpy.array(pVar)
 
-    if type(pVarName)==list:
-        varName=numpy.array(pVarName)
+    if type(pVar)==int:
+        lVar=numpy.array([pVar])
 
-    if type(pVarName)==int:
-        varName=numpy.array([pVarName])
+    if lVarName==None:
+        lVarName=namestr(pVar,g)
 
     print('--------------------------------')
     if pOverride == "full":
@@ -79,20 +97,20 @@ def printTensor(pVarName, pGlobals=None, pOverride=None):
         pass
 
     if CONFIG_PRINT_TENSOR_SHAPE_ONLY: 
-        print(namestr(pVarName,g), ": ", type(varName), varName.shape)
+        print(lVarName, ": ", type(lVar), lVar.shape)
     elif CONFIG_PRINT_TENSOR_BRIEF:
-        print(namestr(pVarName,g), ": ", type(varName), varName.shape)
-        pLenDim1 = min(len(varName), CONFIG_PRINT_TENSOR_BRIEF_DIM1)
+        print(lVarName, ": ", type(lVar), lVar.shape)
+        pLenDim1 = min(len(lVar), CONFIG_PRINT_TENSOR_BRIEF_DIM1)
         pLenDim2 = None
         pLenDim3 = None
 
         try:
-            pLenDim2 = min(len(varName[0]), CONFIG_PRINT_TENSOR_BRIEF_DIM2)
+            pLenDim2 = min(len(lVar[0]), CONFIG_PRINT_TENSOR_BRIEF_DIM2)
         except Exception as msg:
             pass
 
         try:
-            pLenDim3 = min(len(varName[0][0]), CONFIG_PRINT_TENSOR_BRIEF_DIM3)
+            pLenDim3 = min(len(lVar[0][0]), CONFIG_PRINT_TENSOR_BRIEF_DIM3)
         except Exception as msg:
             pass
 
@@ -100,20 +118,20 @@ def printTensor(pVarName, pGlobals=None, pOverride=None):
             print("pLenDims:", pLenDim1, pLenDim2, pLenDim3)
 
         if pLenDim3:
-            print([d2[0:pLenDim2] for d2 in varName[0:pLenDim1]])
+            print([d2[0:pLenDim2] for d2 in lVar[0:pLenDim1]])
         elif pLenDim2:
             # does not crash but does not appears working..
             # test well.
             # with array in ch8-p96-rnn-cell.py: p254,4,2 seems to print last dimension as 2 instead of 1 when pLenDim1 is calculated to be 1.
-            print( [d3[0:pLenDim3] for d3 in [d2[0:pLenDim2] for d2 in varName[0:pLenDim1]]  ])
+            print( [d3[0:pLenDim3] for d3 in [d2[0:pLenDim2] for d2 in lVar[0:pLenDim1]]  ])
         else:
             # all other cases, print only 1st dimension.
-            print(varName[0:pLenDim1])
+            print(lVar[0:pLenDim1])
 
     else:
-        print(namestr(pVarName,g), ": ", type(varName))
-        print(varName.shape)
-        print(varName)
+        print(lVarName, ": ", type(lVar))
+        print(lVar.shape)
+        print(lVar)
 
     print('--------------------------------')
 
