8aadaef24ba6a3b54079175bb992e526ed8106de
Your Name <you@example.com>
2024-08-07 03:08:33 +0000

    run-llama2.sh: will create full-fledged log files: environment, runtime, dmesg in log/token directory

--- build-install-scripts/llama/run-llama2.sh
+++ build-install-scripts/llama/run-llama2.sh
@@ -1,10 +1,23 @@
 set -x
-cat /opt/rocm/.info/version
-sudo dkms status
-ls -l $LLAMA_PREREQ_PKGS.tar
-tree -fs $LLAMA_PREREQ_PKGS
-pip3 list | egrep "torch|triton|gradlib|vllm"
+p1=$1
+TOKEN_DATE=`date +%Y%m%d-%H-%M-%S`
+[[ -z $p1 ]] && p1=$TOKEN_DATE
+LOG_DIR=`pwd`/log/$TOKEN_DATE/
+mkdir -p $LOG_DIR
+LOG_RUN=$LOG_DIR/$p1.run.log
+LOG_DMESG_BEFORE=$LOG_DIR/$p1.dmesg_before.log
+LOG_DMESG_AFTER=$LOG_DIR/$p1.dmesg_after.log
+LOG_ENV=$LOG_DIR/$p1.environ.log
+
+cat /opt/rocm/.info/version | tee $LOG_ENV
+sudo dkms status | tee -a $LOG_ENV
+ls -l $LLAMA_PREREQ_PKGS.tar | tee -a $LOG_ENV
+pip3 list | egrep "torch|triton|gradlib|vllm" | tee -a $LOG_ENV
+tree -fs $LLAMA_PREREQ_PKGS | tee -a $LOG_ENV
+
 pushd $LLAMA_PREREQ_PKGS
-./run_llama2_70b_bf16.sh && echo "---- done ----"   
+dmesg | tee $LOG_DMESG_BEFORE
+./run_llama2_70b_bf16.sh 2>&1 | tee $LOG_RUN && echo "---- done ----"   
+dmesg | tee $LOG_DMESG_AFTER
 popd
 
