fce9ef50534f55f70f95e7af5505c6a9452da4c7
Your Name <you@example.com>
2024-08-08 16:28:54 +0000

    run-llama2.sh: more info into logs

--- build-install-scripts/llama/readme.md
+++ build-install-scripts/llama/readme.md
@@ -1,4 +1,10 @@
 1) Run as either root or nonroot account. Do not run as sudo, running as sudo is untested.
+1a) If running as non root account, change parent directory to be owned by non root account's user:
+cd .. (go up one level to parent dir)
+for i in chgrp chown ; do sudo $i <USER> <child_dir> ; done
+done
+cd <child_dir>
+
 2) ./setup-llama2-stg1.sh --env_name=<conda_env_name> --pkg_name=<pkg_name without extension>
 example: ././setup-llama2-stg1.sh --env_name=llama2-test --pkg_name=20240719_quanta_llama2_rocm_6.2.0-8
 3) re-login (so that conda environment will take effect)
--- build-install-scripts/llama/run-llama2.sh
+++ build-install-scripts/llama/run-llama2.sh
@@ -9,10 +9,17 @@ LOG_DMESG_BEFORE=$LOG_DIR/$p1.dmesg_before.log
 LOG_DMESG_AFTER=$LOG_DIR/$p1.dmesg_after.log
 LOG_ENV=$LOG_DIR/$p1.environ.log
 
-cat /opt/rocm/.info/version | tee $LOG_ENV
+echo " ----- ROCm ------" | tee $LOG_ENV
+cat /opt/rocm/.info/version | tee -a $LOG_ENV
 sudo dkms status | tee -a $LOG_ENV
 ls -l $LLAMA_PREREQ_PKGS.tar | tee -a $LOG_ENV
+echo " ----- pip3 list  ------" | tee -a $LOG_ENV
 pip3 list | egrep "torch|triton|gradlib|vllm" | tee -a $LOG_ENV
+echo " ----- env variable list ------" | tee -a $LOG_ENV
+env | tee -a $LOG_ENV
+echo " ----- .bashrc ------" | tee -a $LOG_ENV
+cat ~/.bashrc | tee -a $LOG_ENV
+echo " ---- $LLAMA_PREREQ_PKGS files ----- " | tee -a $LOG_ENV
 tree -fs $LLAMA_PREREQ_PKGS | tee -a $LOG_ENV
 
 pushd $LLAMA_PREREQ_PKGS
