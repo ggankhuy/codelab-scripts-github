d153276dc35ae8b1ac55e00a86fe53d0107e43e3
Guyen Gankhuyag <guyen900@gmail.com>
2024-08-21 01:32:06 +0000

    Update build-rccl_tracing.sh: make to cmake for log

--- rocm-scripts/rccl/build-rccl_tracing.sh
+++ rocm-scripts/rccl/build-rccl_tracing.sh
@@ -9,7 +9,7 @@ mkdir build
 pushd build || exit 1
 
 t1=$SECONDS
-CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_PREFIX_PATH=/opt/rocm/ -GNinja .. | tee $LOG_DIR/rccl.1.make.log
+CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_PREFIX_PATH=/opt/rocm/ -GNinja .. | tee $LOG_DIR/rccl.1.cmake.log
 ninja -j32 2>&1 | tee $LOG_DIR/rccl.2.make.log
 t2=$SECONDS
 echo build time: $((t2-t1)) | tee $LOG_DIR/rccl.3.buildtime.log
