01e69c7bd67763cef6191bebe0b32715a5a3db17
Your Name <you@example.com>
2024-09-03 19:34:21 +0000

    build-kfd.sh: added

---
+++ rocm-scripts/roct-thunk/build-kfd.sh
@@ -0,0 +1,32 @@
+
+set -x
+rm -rf log/*
+rm -rf build
+mkdir build
+mkdir log
+
+LOG_DIR=`pwd`/log
+
+for i in ghc-terminfo-devel libzstd-devel.x86_64 rocm-llvm-devel rpm-build dpkg libdrm-devel numactl-devel
+do
+    sudo yum install $i -y
+done
+
+pushd build
+cmake ..  2>&1 | tee $LOG_DIR/1.roct.cmake.log
+#make -j16  ..  2>&1 | tee $LOG_DIR/2.roct.make.log
+make -j16 package  2>&1 | tee $LOG_DIR/2.roct.make.log
+popd
+yum install --allowerasing --nobest `find . -name *rpm | head -1` -y
+
+pushd tests/kfdtest
+rm -rf build
+mkdir build
+cd build
+
+#export LIBHSAKMT_PATH=/opt/rocm/lib64/libhsakmt.so
+CMAKE_PREFIX_PATH=/opt/rocm-6.1.0/lib/llvm/lib/cmake/llvm/ cmake .. 2>&1  | tee $LOG_DIR/3.kfdtest.cmake.log &&
+#LD_LIBRARY_PATH=/opt/rocm/lib64/libhsakmt.so 
+make -j16  2>&1  | tee $LOG_DIR/4.kfdtest.make.log
+popd
+
