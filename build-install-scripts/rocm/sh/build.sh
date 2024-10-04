339335c9c94a90d6e9da8e02843baa7372d3b431
Your Name <you@example.com>
2024-10-04 21:08:19 +0000

    build-rocm.py: sh/build.sh: function f2a use hipcc without path as no longer valid

--- build-install-scripts/rocm/sh/build.sh
+++ build-install-scripts/rocm/sh/build.sh
@@ -486,7 +486,7 @@ function f2a() {
     pushd $ROCM_SRC_FOLDER/$i
     mkdir build; cd build
     rm -rf ./*
-    CXX=/opt/rocm/hip/bin/hipcc cmake .. | tee $LOG_DIR/$CURR_BUILD.log
+    CXX=hipcc cmake .. | tee $LOG_DIR/$CURR_BUILD.log
     if [[ $? -ne 0 ]] ; then echo "$CURR_BUILD fail" >> $LOG_SUMMARY ; fi
     make -j$NPROC $BUILD_TARGET 2>&1 | tee -a $LOG_DIR/$CURR_BUILD.log
     if [[ $? -ne 0 ]] ; then echo "$CURR_BUILD fail" >> $LOG_SUMMARY ; fi
